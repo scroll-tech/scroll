5bd38eed4aafffb8eaf5ff8a3ae6e3f366fe8eea
HAOYUatHZ <haoyu@protonmail.com>
2022-10-08 11:21:44 +0800

    fix

--- roller/cmd/roller/main.go
+++ roller/cmd/roller/main.go
@@ -65,12 +65,12 @@ func action(ctx *cli.Context) error {
 	}
 
 	// Create roller
-	r, err := roller.NewRoller(cfg)
+	r, err := core.NewRoller(cfg)
 	if err != nil {
 		return err
 	}
 	defer r.Close()
-	log.Info("go-roller start successful", "name", cfg.RollerName)
+	log.Info("go-roller start successfully", "name", cfg.RollerName)
 
 	return r.Run()
 }
