3eb1454a477cea90786eae24b29334ea7d5e18be
Lawliet-Chan <1576710154@qq.com>
2022-11-14 16:08:50 +0800

    rewrite verifier_test

--- coordinator/verifier/verifier_test.go
+++ coordinator/verifier/verifier_test.go
@@ -1,65 +1,40 @@
 package verifier_test
 
 import (
+	"encoding/json"
+	"io"
+	"os"
+	"scroll-tech/coordinator/config"
+	"scroll-tech/coordinator/verifier"
 	"testing"
 
-	"github.com/scroll-tech/go-ethereum/common"
-	"github.com/scroll-tech/go-ethereum/crypto"
 	"github.com/stretchr/testify/assert"
 
 	"scroll-tech/common/message"
 )
 
-// skipped due to verifier upgrade
-/*
-// This test assumes the IPC Verifier is running.
-func TestIPCComm(t *testing.T) {
-	if os.Getenv("TEST_IPC") != "true" {
+func TestFFI(t *testing.T) {
+	if os.Getenv("TEST_FFI") != "true" {
 		return
 	}
 
-	assert := assert.New(t)
-	verifier, err := verifier.NewVerifier("/tmp/Verifier.sock")
-	assert.NoError(err)
-
-	// Retrieve pre-generated proofs
-	evmProof, err := ioutil.ReadFile("../assets/evm_proof")
-	assert.NoError(err)
-	stateProof, err := ioutil.ReadFile("../assets/state_proof")
-	assert.NoError(err)
-
-	proof := &message.ZkProof{
-		ID:         1,
-		EvmProof:   evmProof,
-		StateProof: stateProof,
+	as := assert.New(t)
+	cfg := &config.VerifierConfig{
+		MockMode:   false,
+		ParamsPath: "../../assets/test_params",
+		AggVkPath:  "../../assets/agg_vk",
 	}
+	v, err := verifier.NewVerifier(cfg)
+	as.NoError(err)
 
-	traces := &types.BlockResult{}
+	f, err := os.Open("../../assets/agg_proof")
+	as.NoError(err)
+	byt, err := io.ReadAll(f)
+	as.NoError(err)
+	aggProof := &message.AggProof{}
+	as.NoError(json.Unmarshal(byt, aggProof))
 
-	verified, err := verifier.VerifyProof(traces, proof)
-	assert.NoError(err)
-	assert.True(verified)
-}
-*/
-
-func TestVerifier(t *testing.T) {
-	privkey, err := crypto.HexToECDSA("dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7")
-	assert.NoError(t, err)
-
-	pubkey := crypto.CompressPubkey(&privkey.PublicKey)
-
-	msg := &message.Identity{
-		Name:      "scroll_roller",
-		Timestamp: 1649663001,
-		PublicKey: common.Bytes2Hex(pubkey),
-		Version:   "some_version",
-	}
-	hash, err := msg.Hash()
-	assert.NoError(t, err)
-
-	sig, err := crypto.Sign(hash, privkey)
-	assert.NoError(t, err)
-
-	ok := crypto.VerifySignature(pubkey, hash, sig[:64])
-	assert.Equal(t, true, ok)
+	ok, err := v.VerifyProof(aggProof)
+	as.NoError(err)
+	as.True(ok)
 }
