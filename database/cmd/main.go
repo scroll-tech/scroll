9d7419074a5b3e166be025e1f208089ca3cdfe8b
ChuhanJin <cj1436@nyu.edu>
2022-11-15 18:18:03 +0800

    naming (+3 squashed commits) Squashed commits: [f95b5bf] name [8c32511] name to db_cli [24f8758] add dockerfiles

--- database/cmd/main.go
+++ database/cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/signal"
 
 	"github.com/urfave/cli/v2"
 
@@ -14,8 +13,7 @@ import (
 func main() {
 	// Set up database app info.
 	app := cli.NewApp()
-	app.Action = action
-	app.Name = "database"
+	app.Name = "db_cli"
 	app.Usage = "The Scroll Database CLI"
 	app.Version = version.Version
 	app.Flags = append(app.Flags, commonFlags...)
@@ -82,14 +80,4 @@ func main() {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-func action(ctx *cli.Context) error {
-	// Catch CTRL-C to ensure a graceful shutdown.
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	// Wait until the interrupt signal is received from an OS signal.
-	<-interrupt
-
-	return nil
 }
