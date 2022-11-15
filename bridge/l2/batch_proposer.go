e5ef76f178bfce7da5bef586f8cb249e116f03d2
HAOYUatHZ <haoyu@protonmail.com>
2022-11-15 12:20:56 +0800

    3_000_000

--- bridge/l2/batch_proposer.go
+++ bridge/l2/batch_proposer.go
@@ -12,8 +12,8 @@ import (
 
 // batch-related config
 const (
-	batchTimeSec      = uint64(5 * 60)  // 5min
-	batchGasThreshold = uint64(3000000) // 3M
+	batchTimeSec      = uint64(5 * 60) // 5min
+	batchGasThreshold = uint64(3_000_000)
 	batchBlocksLimit  = uint64(100)
 )
 
