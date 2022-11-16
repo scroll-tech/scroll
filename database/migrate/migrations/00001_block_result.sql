2e392940bb16a00fcba6ad5a23c600c92a5c4f80
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2022-11-16 11:01:20 +0800

    add `parent_hash` (#92)

--- database/migrate/migrations/00001_block_result.sql
+++ database/migrate/migrations/00001_block_result.sql
@@ -7,6 +7,7 @@ create table block_result
 (
     number                  BIGINT          not null,
     hash                    VARCHAR         not null,
+    parent_hash             VARCHAR         default null,
     content                 json            not null,
     batch_id                VARCHAR         default null,
     tx_num                  INTEGER         NOT NULL DEFAULT 0,
@@ -19,6 +20,12 @@ create unique index block_result_hash_uindex
 
 create unique index block_result_number_uindex
     on block_result (number);
+
+create unique index block_result_parent_uindex
+    on block_result (number, parent_hash);
+
+create unique index block_result_parent_hash_uindex
+    on block_result (hash, parent_hash);
 
 -- +goose StatementEnd
 
