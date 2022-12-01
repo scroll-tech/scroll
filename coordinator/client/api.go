6b21c468a2c0a7e297cd0f93c1f744ea4fffb37e
maskpp <maskpp266@gmail.com>
2022-12-01 15:45:59 +0800

    fix comments

--- coordinator/client/api.go
+++ coordinator/client/api.go
@@ -10,11 +10,11 @@ import (
 
 // RegisterAndSubscribe subscribe roller and register, verified by sign data.
 func (c *Client) RegisterAndSubscribe(ctx context.Context, traceChan chan *message.TaskMsg, authMsg *message.AuthMsg) (ethereum.Subscription, error) {
-	return c.Subscribe(ctx, "roller", traceChan, "register", authMsg)
+	return c.client.Subscribe(ctx, "roller", traceChan, "register", authMsg)
 }
 
 // SubmitProof get proof from roller.
 func (c *Client) SubmitProof(ctx context.Context, proof *message.ProofMsg) (bool, error) {
 	var ok bool
-	return ok, c.CallContext(ctx, &ok, "roller_submitProof", proof)
+	return ok, c.client.CallContext(ctx, &ok, "roller_submitProof", proof)
 }
