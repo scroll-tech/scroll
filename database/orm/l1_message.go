cc2f5b5b0aa61ea660195746c82cf731d7243cb6
Xi Lin <zimpha@gmail.com>
2022-12-06 13:32:57 +0800

    feat(bridge): fix relay tx status based on fetched events (#56)

--- database/orm/l1_message.go
+++ database/orm/l1_message.go
@@ -20,13 +20,13 @@ func NewL1MessageOrm(db *sqlx.DB) L1MessageOrm {
 	return &l1MessageOrm{db: db}
 }
 
-// GetL1MessageByLayer1Hash fetch message by nonce
-func (m *l1MessageOrm) GetL1MessageByLayer1Hash(layer1Hash string) (*L1Message, error) {
+// GetL1MessageByMsgHash fetch message by nonce
+func (m *l1MessageOrm) GetL1MessageByMsgHash(msgHash string) (*L1Message, error) {
 	msg := L1Message{}
 
-	row := m.db.QueryRow(`SELECT nonce, height, sender, target, value, fee, gas_limit, deadline, calldata, layer1_hash, status FROM l1_message WHERE layer1_hash = $1`, layer1Hash)
+	row := m.db.QueryRowx(`SELECT nonce, msg_hash, height, sender, target, value, fee, gas_limit, deadline, calldata, layer1_hash, status FROM l1_message WHERE msg_hash = $1`, msgHash)
 
-	if err := row.Scan(&msg.Nonce, &msg.Height, &msg.Sender, &msg.Target, &msg.Value, &msg.Fee, &msg.GasLimit, &msg.Deadline, &msg.Calldata, &msg.Layer1Hash, &msg.Status); err != nil {
+	if err := row.StructScan(&msg); err != nil {
 		return nil, err
 	}
 	return &msg, nil
@@ -36,17 +36,17 @@ func (m *l1MessageOrm) GetL1MessageByLayer1Hash(layer1Hash string) (*L1Message, 
 func (m *l1MessageOrm) GetL1MessageByNonce(nonce uint64) (*L1Message, error) {
 	msg := L1Message{}
 
-	row := m.db.QueryRow(`SELECT nonce, height, sender, target, value, fee, gas_limit, deadline, calldata, layer1_hash, status FROM l1_message WHERE nonce = $1`, nonce)
+	row := m.db.QueryRowx(`SELECT nonce, msg_hash, height, sender, target, value, fee, gas_limit, deadline, calldata, layer1_hash, status FROM l1_message WHERE nonce = $1`, nonce)
 
-	if err := row.Scan(&msg.Nonce, &msg.Height, &msg.Sender, &msg.Target, &msg.Value, &msg.Fee, &msg.GasLimit, &msg.Deadline, &msg.Calldata, &msg.Layer1Hash, &msg.Status); err != nil {
+	if err := row.StructScan(&msg); err != nil {
 		return nil, err
 	}
 	return &msg, nil
 }
 
-// GetL1UnprocessedMessages fetch list of unprocessed messages
-func (m *l1MessageOrm) GetL1UnprocessedMessages() ([]*L1Message, error) {
-	rows, err := m.db.Queryx(`SELECT nonce, height, sender, target, value, fee, gas_limit, deadline, calldata, layer1_hash, status FROM l1_message WHERE status = $1 ORDER BY nonce ASC;`, MsgPending)
+// GetL1MessagesByStatus fetch list of unprocessed messages given msg status
+func (m *l1MessageOrm) GetL1MessagesByStatus(status MsgStatus) ([]*L1Message, error) {
+	rows, err := m.db.Queryx(`SELECT nonce, msg_hash, height, sender, target, value, fee, gas_limit, deadline, calldata, layer1_hash, status FROM l1_message WHERE status = $1 ORDER BY nonce ASC;`, status)
 	if err != nil {
 		return nil, err
 	}
@@ -72,17 +72,19 @@ func (m *l1MessageOrm) GetL1UnprocessedMessages() ([]*L1Message, error) {
 func (m *l1MessageOrm) GetL1ProcessedNonce() (int64, error) {
 	row := m.db.QueryRow(`SELECT MAX(nonce) FROM l1_message WHERE status = $1;`, MsgConfirmed)
 
-	var nonce int64
-	err := row.Scan(&nonce)
-	if err != nil {
-		if err == sql.ErrNoRows {
+	var nonce sql.NullInt64
+	if err := row.Scan(&nonce); err != nil {
+		if err == sql.ErrNoRows || !nonce.Valid {
 			// no row means no message
 			// since nonce starts with 0, return -1 as the processed nonce
 			return -1, nil
 		}
 		return 0, err
 	}
-	return nonce, nil
+	if nonce.Valid {
+		return nonce.Int64, nil
+	}
+	return -1, nil
 }
 
 // SaveL1Messages batch save a list of layer1 messages
@@ -91,6 +93,7 @@ func (m *l1MessageOrm) SaveL1Messages(ctx context.Context, messages []*L1Message
 	for i, msg := range messages {
 		messageMaps[i] = map[string]interface{}{
 			"nonce":       msg.Nonce,
+			"msg_hash":    msg.MsgHash,
 			"height":      msg.Height,
 			"sender":      msg.Sender,
 			"target":      msg.Target,
@@ -102,7 +105,7 @@ func (m *l1MessageOrm) SaveL1Messages(ctx context.Context, messages []*L1Message
 			"layer1_hash": msg.Layer1Hash,
 		}
 	}
-	_, err := m.db.NamedExec(`INSERT INTO public.l1_message (nonce, height, sender, target, value, fee, gas_limit, deadline, calldata, layer1_hash) VALUES (:nonce, :height, :sender, :target, :value, :fee, :gas_limit, :deadline, :calldata, :layer1_hash);`, messageMaps)
+	_, err := m.db.NamedExec(`INSERT INTO public.l1_message (nonce, msg_hash, height, sender, target, value, fee, gas_limit, deadline, calldata, layer1_hash) VALUES (:nonce, :msg_hash, :height, :sender, :target, :value, :fee, :gas_limit, :deadline, :calldata, :layer1_hash);`, messageMaps)
 	if err != nil {
 		nonces := make([]uint64, 0, len(messages))
 		heights := make([]uint64, 0, len(messages))
@@ -115,27 +118,27 @@ func (m *l1MessageOrm) SaveL1Messages(ctx context.Context, messages []*L1Message
 	return err
 }
 
-// UpdateLayer2Hash update corresponding layer2 hash given message nonce
-func (m *l1MessageOrm) UpdateLayer2Hash(ctx context.Context, layer1Hash string, layer2Hash string) error {
-	if _, err := m.db.ExecContext(ctx, m.db.Rebind("update l1_message set layer2_hash = ? where layer1_hash = ?;"), layer2Hash, layer1Hash); err != nil {
+// UpdateLayer2Hash update corresponding layer2 hash, given message hash
+func (m *l1MessageOrm) UpdateLayer2Hash(ctx context.Context, msgHash, layer2Hash string) error {
+	if _, err := m.db.ExecContext(ctx, m.db.Rebind("update l1_message set layer2_hash = ? where msg_hash = ?;"), layer2Hash, msgHash); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// UpdateLayer1Status updates message stauts
-func (m *l1MessageOrm) UpdateLayer1Status(ctx context.Context, layer1Hash string, status MsgStatus) error {
-	if _, err := m.db.ExecContext(ctx, m.db.Rebind("update l1_message set status = ? where layer1_hash = ?;"), status, layer1Hash); err != nil {
+// UpdateLayer1Status updates message stauts, given message hash
+func (m *l1MessageOrm) UpdateLayer1Status(ctx context.Context, msgHash string, status MsgStatus) error {
+	if _, err := m.db.ExecContext(ctx, m.db.Rebind("update l1_message set status = ? where msg_hash = ?;"), status, msgHash); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// UpdateLayer1StatusAndLayer2Hash updates message status and layer2 transaction hash
-func (m *l1MessageOrm) UpdateLayer1StatusAndLayer2Hash(ctx context.Context, layer1Hash, layer2Hash string, status MsgStatus) error {
-	if _, err := m.db.ExecContext(ctx, m.db.Rebind("update l1_message set status = ?, layer2_hash = ? where layer1_hash = ?;"), status, layer2Hash, layer1Hash); err != nil {
+// UpdateLayer1StatusAndLayer2Hash updates message status and layer2 transaction hash, given message hash
+func (m *l1MessageOrm) UpdateLayer1StatusAndLayer2Hash(ctx context.Context, msgHash string, status MsgStatus, layer2Hash string) error {
+	if _, err := m.db.ExecContext(ctx, m.db.Rebind("update l1_message set status = ?, layer2_hash = ? where msg_hash = ?;"), status, layer2Hash, msgHash); err != nil {
 		return err
 	}
 
@@ -148,12 +151,15 @@ func (m *l1MessageOrm) GetLayer1LatestWatchedHeight() (int64, error) {
 	// But it will only be called at start, some redundancy is acceptable.
 	row := m.db.QueryRow("SELECT MAX(height) FROM l1_message;")
 
-	var height int64
+	var height sql.NullInt64
 	if err := row.Scan(&height); err != nil {
-		if err == sql.ErrNoRows {
+		if err == sql.ErrNoRows || !height.Valid {
 			return -1, nil
 		}
 		return 0, err
 	}
-	return height, nil
+	if height.Valid {
+		return height.Int64, nil
+	}
+	return -1, nil
 }
