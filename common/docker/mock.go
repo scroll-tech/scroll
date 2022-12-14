91d8fcbad9656b39866f238939ab0d3ebfb90700
HAOYUatHZ <haoyu@protonmail.com>
2022-12-14 15:22:45 +0800

    update

--- common/docker/mock.go
+++ common/docker/mock.go
@@ -16,7 +16,7 @@ var (
 
 // NewTestL1Docker starts and returns l1geth docker
 func NewTestL1Docker(t *testing.T) ImgInstance {
-	id, _ := rand.Int(rand.Reader, big.NewInt(2000))
+	id, _ := rand.Int(rand.Reader, big.NewInt(9999))
 	imgL1geth := NewImgGeth(t, "scroll_l1geth", "", "", 0, l1StartPort+int(id.Int64()))
 	assert.NoError(t, imgL1geth.Start())
 	return imgL1geth
@@ -24,7 +24,7 @@ func NewTestL1Docker(t *testing.T) ImgInstance {
 
 // NewTestL2Docker starts and returns l2geth docker
 func NewTestL2Docker(t *testing.T) ImgInstance {
-	id, _ := rand.Int(rand.Reader, big.NewInt(2000))
+	id, _ := rand.Int(rand.Reader, big.NewInt(9999))
 	imgL2geth := NewImgGeth(t, "scroll_l2geth", "", "", 0, l2StartPort+int(id.Int64()))
 	assert.NoError(t, imgL2geth.Start())
 	return imgL2geth
@@ -32,7 +32,7 @@ func NewTestL2Docker(t *testing.T) ImgInstance {
 
 // NewTestDBDocker starts and returns database docker
 func NewTestDBDocker(t *testing.T, driverName string) ImgInstance {
-	id, _ := rand.Int(rand.Reader, big.NewInt(2000))
+	id, _ := rand.Int(rand.Reader, big.NewInt(9999))
 	imgDB := NewImgDB(t, driverName, "123456", "test_db", dbStartPort+int(id.Int64()))
 	assert.NoError(t, imgDB.Start())
 	return imgDB
