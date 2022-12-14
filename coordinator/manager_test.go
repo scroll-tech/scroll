b10bf91072886b094759fa300eda8ae383cc6d07
HAOYUatHZ <haoyu@protonmail.com>
2022-12-14 15:54:55 +0800

    fix

--- coordinator/manager_test.go
+++ coordinator/manager_test.go
@@ -36,6 +36,7 @@ var newManagerURL string
 
 func init() {
 	i1, _ := rand.Int(rand.Reader, big.NewInt(9998))
+	i1 = i1.Add(i1, big.NewInt(40000))
 	i2 := i1.Add(i1, big.NewInt(1))
 	managerURL = "localhost:" + strconv.Itoa(int(i1.Int64()))
 	newManagerURL = "localhost:" + strconv.Itoa(int(i2.Int64()))
