6e6a14dc31842053bfa14ecf1376560e31dd6fd0
maskpp <maskpp266@gmail.com>
2022-12-21 15:50:49 +0800

    fix weak test

--- common/cmd/cmd_test.go
+++ common/cmd/cmd_test.go
@@ -2,8 +2,11 @@ package cmd_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 
 	"scroll-tech/common/cmd"
 )
@@ -13,6 +16,27 @@ func TestCmd(t *testing.T) {
 
 	tm := time.Now()
 	curTime := fmt.Sprintf("%d-%d-%d %d:%d", tm.Year(), tm.Month(), tm.Day(), tm.Hour(), tm.Minute())
+
+	okCh := make(chan struct{}, 1)
+	app.RegistFunc(curTime, func(buf string) {
+		if strings.Contains(buf, curTime) {
+			select {
+			case okCh <- struct{}{}:
+			default:
+				return
+			}
+		}
+	})
+	defer app.UnRegistFunc(curTime)
+
+	// Run cmd.
 	app.RunCmd(true)
-	app.ExpectWithTimeout(false, time.Second, curTime)
+
+	// Wait result.
+	select {
+	case <-okCh:
+		return
+	case <-time.After(time.Second):
+		assert.Fail(t, fmt.Sprintf("didn't get the desired result before timeout, keyword: %s", curTime))
+	}
 }
