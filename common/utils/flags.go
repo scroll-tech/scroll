20a9a430704381d97cfcb1ed3da7301d906ec06a
maskpp <maskpp266@gmail.com>
2022-12-22 15:36:31 +0800

    fix comments

--- common/utils/flags.go
+++ common/utils/flags.go
@@ -1,10 +1,6 @@
 package utils
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/docker/docker/pkg/reexec"
 	"github.com/urfave/cli/v2"
 )
 
@@ -47,16 +43,3 @@ var (
 		Usage: "Prepends log messages with call-site location (file and line number)",
 	}
 )
-
-// RegisterSimulation register initializer function for integration-test.
-func RegisterSimulation(app *cli.App, name string) {
-	// Run the app for integration-test
-	reexec.Register(name, func() {
-		if err := app.Run(os.Args); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		os.Exit(0)
-	})
-	reexec.Init()
-}
