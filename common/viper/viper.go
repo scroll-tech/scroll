6765ab0972fe99152c342bf61d3c264f4a946339
maskpp <maskpp266@gmail.com>
2022-12-27 17:48:02 +0800

    feat(viper): wrap viper (#186)

---
+++ common/viper/viper.go
@@ -0,0 +1,101 @@
+package viper
+
+import (
+	"github.com/scroll-tech/go-ethereum/log"
+	"github.com/spf13/viper"
+	"strings"
+	"sync"
+)
+
+var (
+	root *Viper
+)
+
+func init() {
+	root = &Viper{
+		path: "",
+		// Get the root viper.
+		Viper:  viper.GetViper(),
+		subVps: make(map[string]*Viper),
+	}
+}
+
+type Viper struct {
+	path string
+
+	subVps map[string]*Viper
+
+	mu sync.RWMutex
+	*viper.Viper
+}
+
+func (v *Viper) Sub(key string) *Viper {
+	var (
+		path = v.path
+		sub  = v
+	)
+	for _, s := range strings.Split(key, ".") {
+		path = absolutePath(path, s)
+		if vp := sub.subVps[path]; vp != nil {
+			sub = vp
+		} else {
+			vip := root.Viper.Sub(path)
+			if vip == nil {
+				return nil
+			}
+			sub.subVps[path] = &Viper{
+				path:   path,
+				Viper:  vip,
+				subVps: make(map[string]*Viper),
+			}
+			sub = sub.subVps[path]
+		}
+	}
+	return sub
+}
+
+func (v *Viper) getSub(key string) *Viper {
+	var (
+		path = v.path
+		sub  = v
+	)
+	for _, s := range strings.Split(key, ".") {
+		path = absolutePath(path, s)
+		if vp := sub.subVps[path]; vp != nil {
+			sub = vp
+		} else {
+			return nil
+		}
+	}
+	return sub
+}
+
+func (v *Viper) Set(key string, val interface{}) {
+	path := v.path
+	var sub = v
+	for _, s := range strings.Split(key, ".") {
+		path = absolutePath(path, s)
+		if vp := sub.subVps[path]; vp != nil {
+			sub = vp
+		} else {
+			log.Error("has no such path", "absolute path", path)
+			return
+		}
+	}
+	v.mu.Lock()
+	sub.Viper.Set(key, val)
+	v.mu.Unlock()
+}
+
+// Unmarshal unmarshals the config into a Struct. Make sure that the tags
+// on the fields of the structure are properly set.
+func (v *Viper) Unmarshal(rawVal interface{}, opts ...viper.DecoderConfigOption) error {
+	return v.Viper.Unmarshal(rawVal, opts...)
+}
+
+func absolutePath(path, key string) string {
+	if path == "" {
+		return key
+	}
+	return path + "." + key
+}
