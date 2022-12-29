079c180badd8b0c0cbca12dce1414c56faf12a0e
maskpp <maskpp266@gmail.com>
2022-12-29 11:32:21 +0800

    upgrade viper

--- common/viper/viper.go
+++ common/viper/viper.go
@@ -8,26 +8,30 @@ import (
 	"github.com/spf13/viper"
 )
 
-var (
-	root *Viper
-)
-
-func init() {
-	root = &Viper{
-		path: "",
-		// Get the root viper.
-		Viper:  viper.GetViper(),
-		subVps: make(map[string]*Viper),
-	}
-}
-
 type Viper struct {
 	path string
+	root *Viper
 
 	subVps map[string]*Viper
 
 	mu sync.RWMutex
-	*viper.Viper
+	vp *viper.Viper
+}
+
+func NewViper(file string) (*Viper, error) {
+	vp := viper.New()
+	vp.SetConfigFile(file)
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	root := &Viper{
+		// Get the root viper.
+		vp:     vp,
+		subVps: make(map[string]*Viper),
+	}
+	root.root = root
+	return root, nil
 }
 
 func (v *Viper) Sub(key string) *Viper {
@@ -40,13 +44,14 @@ func (v *Viper) Sub(key string) *Viper {
 		if vp := sub.subVps[path]; vp != nil {
 			sub = vp
 		} else {
-			vip := root.Viper.Sub(path)
+			vip := v.root.vp.Sub(path)
 			if vip == nil {
 				return nil
 			}
 			sub.subVps[path] = &Viper{
 				path:   path,
-				Viper:  vip,
+				vp:     vip,
+				root:   v.root,
 				subVps: make(map[string]*Viper),
 			}
 			sub = sub.subVps[path]
@@ -59,7 +64,7 @@ func (v *Viper) Set(key string, val interface{}) {
 	var sub = v
 	if idx := strings.LastIndex(key, "."); idx >= 0 {
 		path := absolutePath(v.path, key[:idx])
-		sub = root.Sub(path)
+		sub = v.root.Sub(path)
 		if sub == nil {
 			log.Error("don't exist the sub viper", "path", path)
 			return
@@ -68,13 +73,13 @@ func (v *Viper) Set(key string, val interface{}) {
 	}
 	sub.mu.Lock()
 	defer sub.mu.Unlock()
-	sub.Viper.Set(key, val)
+	sub.vp.Set(key, val)
 }
 
 // Unmarshal unmarshals the config into a Struct. Make sure that the tags
 // on the fields of the structure are properly set.
 func (v *Viper) Unmarshal(rawVal interface{}, opts ...viper.DecoderConfigOption) error {
-	return v.Viper.Unmarshal(rawVal, opts...)
+	return v.vp.Unmarshal(rawVal, opts...)
 }
 
 func absolutePath(path, key string) string {
