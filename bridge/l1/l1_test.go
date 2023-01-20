049cac345b2aaaaab2f454871b56a03048e6be12
maskpp <maskpp266@gmail.com>
2023-01-20 08:14:22 +0800

    Update bridge/l1/l1_test.go
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

--- bridge/l1/l1_test.go
+++ bridge/l1/l1_test.go
@@ -37,8 +37,8 @@ func setupEnv(t *testing.T) {
 	cfg.L2Config.Endpoint = l2gethImg.Endpoint()
 
 	// Create db container.
-	dbImg = docker.NewTestDBDocker(t, cfg.DBConfig.DB.DriverName)
-	cfg.DBConfig.DB.DSN = dbImg.Endpoint()
+	dbImg = docker.NewTestDBDocker(t, cfg.DBConfig.Persistence.DriverName)
+	cfg.DBConfig.Persistence.DSN = dbImg.Endpoint()
 }
 
 func free(t *testing.T) {
