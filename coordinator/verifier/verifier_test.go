0c7fa22c9bbe7c3534dc7a77c9670f2cf7b6cdb8
Lawliet-Chan <1576710154@qq.com>
2023-01-28 16:30:25 +0800

    fix path

--- coordinator/verifier/verifier_test.go
+++ coordinator/verifier/verifier_test.go
@@ -16,17 +16,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	ParamsPath = "./assets/test_params"
+	AggVkPath  = "./assets/agg_vk"
+	ProofPath  = "./assets/agg_proof"
+)
+
 func TestFFI(t *testing.T) {
 	as := assert.New(t)
 	cfg := &config.VerifierConfig{
 		MockMode:   false,
-		ParamsPath: "./assets/test_params",
-		AggVkPath:  "./assets/agg_vk",
+		ParamsPath: ParamsPath,
+		AggVkPath:  AggVkPath,
 	}
 	v, err := verifier.NewVerifier(cfg)
 	as.NoError(err)
 
-	f, err := os.Open("../assets/agg_proof")
+	f, err := os.Open(ProofPath)
 	as.NoError(err)
 	byt, err := io.ReadAll(f)
 	as.NoError(err)
