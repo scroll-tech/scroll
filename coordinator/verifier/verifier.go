71d31a7103e80642d8f3a67dd6a66897b183212a
Nazarii Denha <dengaaa2002@gmail.com>
2023-01-29 20:17:42 +0100

    fix

--- coordinator/verifier/verifier.go
+++ coordinator/verifier/verifier.go
@@ -63,5 +63,4 @@ func (v *Verifier) VerifyProof(proof *message.AggProof) (bool, error) {
 	log.Info("Start to verify proof ...")
 	verified := C.verify_agg_proof(aggProofStr)
 	return verified != 0, nil
-	return true, nil
 }
