ee15881a04ebc0b9705e1f655f9eec41ee589a3d
maskpp <maskpp266@gmail.com>
2023-02-10 16:35:55 +0800

    Update bridge/l2/relayer_finalize.go
    Co-authored-by: colin <102356659+colinlyguo@users.noreply.github.com>

--- bridge/l2/relayer_finalize.go
+++ bridge/l2/relayer_finalize.go
@@ -20,7 +20,7 @@ import (
 
 func (r *Layer2Relayer) checkFinalizingBatches() error {
 	var (
-		batch              = 10
+		batchLimit              = 10
 		blockNumber uint64 = math.MaxUint64
 	)
 BEGIN:
