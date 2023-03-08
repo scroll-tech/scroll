94effbd80a93c8b46b44513ff41c62e7ebfa8686
maskpp <maskpp266@gmail.com>
2023-03-08 16:22:22 +0800

    fix ci

--- database/cmd/app/app_test.go
+++ database/cmd/app/app_test.go
@@ -10,10 +10,10 @@ import (
 )
 
 func TestRunDatabase(t *testing.T) {
-	bridge := cmd.NewCmd(t, "db_cli-test", "--version")
+	bridge := cmd.NewCmd("db_cli-test", "--version")
 	defer bridge.WaitExit()
 
 	// wait result
-	bridge.ExpectWithTimeout(true, time.Second*3, fmt.Sprintf("db_cli version %s", version.Version))
+	bridge.ExpectWithTimeout(t, true, time.Second*3, fmt.Sprintf("db_cli version %s", version.Version))
 	bridge.RunApp(nil)
 }
