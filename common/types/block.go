139ddd5fb1afe7891a1d1487a5935804a359c785
colinlyguo <651734127@qq.com>
2023-03-24 23:56:02 +0800

    add a comment about TransactionData and bump version

--- common/types/block.go
+++ common/types/block.go
@@ -7,7 +7,8 @@ import (
 
 // BlockWithWithdrawTrieRoot contains the block's Header, Transactions and WithdrawTrieRoot hash.
 type BlockWithWithdrawTrieRoot struct {
-	Header           *types.Header            `json:"header"`
+	Header *types.Header `json:"header"`
+	// Transactions is only used for recover types.Transactions, the from of types.TransactionData field is missing.
 	Transactions     []*types.TransactionData `json:"transactions"`
 	WithdrawTrieRoot common.Hash              `json:"withdraw_trie_root,omitempty"`
 }
