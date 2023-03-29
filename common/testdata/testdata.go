a1476ab8db22f496b1a48ababf263bd714218384
maskpp <maskpp266@gmail.com>
2023-03-29 21:07:43 +0800

    fix bug when in testdata module

--- common/testdata/testdata.go
+++ common/testdata/testdata.go
@@ -3,36 +3,19 @@ package testdata
 import (
 	"encoding/json"
 	"os"
-	"strings"
 
 	"github.com/scroll-tech/go-ethereum/core/types"
 )
 
-var (
-	TraceList = map[string]*types.BlockTrace{
-		"blockTrace_02.json":       nil,
-		"blockTrace_03.json":       nil,
-		"blockTrace_delegate.json": nil,
+// GetTrace returns trace by file name.
+func GetTrace(file string) *types.BlockTrace {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
 	}
-)
-
-// Load Load trace list.
-func Load() {
-	dir, _ := os.Getwd()
-	index := strings.LastIndex(dir, "scroll-tech/scroll")
-	pwd := dir[:index] + "scroll-tech/scroll/common/testdata/"
-	for file, val := range TraceList {
-		if val != nil {
-			continue
-		}
-		data, err := os.ReadFile(pwd + file)
-		if err != nil {
-			panic(err)
-		}
-		trace := &types.BlockTrace{}
-		if err = json.Unmarshal(data, &trace); err != nil {
-			panic(err)
-		}
-		TraceList[file] = trace
+	trace := &types.BlockTrace{}
+	if err = json.Unmarshal(data, &trace); err != nil {
+		panic(err)
 	}
+	return trace
 }
