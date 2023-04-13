91a200f4ba19abf7402fa1247526fe486661cfba
Max Wolff <maxcwolff@gmail.com>
2023-04-12 18:01:20 -0700

    update test

--- contracts/integration-test/L1MessageQueue.spec.ts
+++ contracts/integration-test/L1MessageQueue.spec.ts
@@ -1,10 +1,10 @@
 /* eslint-disable node/no-unpublished-import */
 /* eslint-disable node/no-missing-import */
 import { expect } from "chai";
-import { BigNumber, constants } from "ethers";
-import { concat, hexlify, keccak256, randomBytes, RLP } from "ethers/lib/utils";
-import { ethers } from "hardhat";
+import { BigNumber, constants, utils } from "ethers";
+import { concat, hexlify, keccak256, randomBytes, RLP, UnsignedTransaction } from "ethers/lib/utils";
 import { L1MessageQueue } from "../typechain";
+import { ethers } from "hardhat";
 
 describe.only("L1MessageQueue", async () => {
   let queue: L1MessageQueue;
@@ -68,7 +68,7 @@ describe.only("L1MessageQueue", async () => {
     }
   });
 
-  it.only("should give example", async () => {
+  it("should give example transaction hash to match to geth", async () => {
     const sender = hexlify("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
     const target = hexlify("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
     const nonce = BigNumber.from(1);
@@ -76,23 +76,7 @@ describe.only("L1MessageQueue", async () => {
     const gasLimit = BigNumber.from(3);
     const data = Uint8Array.from([1, 2, 3, 4]);
 
-    const transactionType = "0x7E";
-    const transactionPayload = RLP.encode([
-      nonce.toHexString(),
-      gasLimit.toHexString(),
-      target,
-      value.toHexString(),
-      data,
-      sender,
-    ]);
-
-    const payload = concat([transactionType, transactionPayload]);
-
-    console.log({
-      enc: payload,
-    });
+    const computedHash = await queue.computeTransactionHash(sender, nonce, value, target, gasLimit, data);
+    expect(computedHash).to.eq("0x1cebed6d90ef618f60eec1b7edc0df36b298a237c219f0950081acfb72eac6be");
   });
 });
-
-// [     126, 242, 1, 3, 148, 112, 153, 121, 112, 197, 24, 18, 220, 58, 1, 12, 125, 1, 181, 14, 13, 23, 220, 121, 200,   2, 132, 1, 2, 3, 4, 148, 243, 159, 214, 229, 26, 173, 136, 246, 244, 206, 106, 184, 130, 114, 121, 207, 255, 185, 34, 102]
-// [180, 126, 242, 1, 3, 148, 112, 153, 121, 112, 197, 24, 18, 220, 58, 1, 12, 125, 1, 181, 14, 13, 23, 220, 121, 200, 128, 132, 1, 2, 3, 4, 148, 243, 159, 214, 229, 26, 173, 136, 246, 244, 206, 106, 184, 130, 114, 121, 207, 255, 185, 34, 102]
