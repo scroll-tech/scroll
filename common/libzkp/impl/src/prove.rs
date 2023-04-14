401ea6833293d9f0f42f3954f3b9a837ed9e262c
Lawliet-Chan <1576710154@qq.com>
2023-04-14 20:51:50 +0800

    feat(libzkp): recover rust zkp panic instead of crashing (#421)

--- common/libzkp/impl/src/prove.rs
+++ common/libzkp/impl/src/prove.rs
@@ -1,6 +1,8 @@
 use crate::utils::{c_char_to_str, c_char_to_vec, vec_to_c_char};
 use libc::c_char;
 use std::cell::OnceCell;
+use std::panic;
+use std::ptr::null;
 use types::eth::BlockTrace;
 use zkevm::circuit::AGG_DEGREE;
 use zkevm::utils::{load_params, load_seed};
@@ -27,13 +29,15 @@ pub unsafe extern "C" fn init_prover(params_path: *const c_char, seed_path: *con
 pub unsafe extern "C" fn create_agg_proof(trace_char: *const c_char) -> *const c_char {
     let trace_vec = c_char_to_vec(trace_char);
     let trace = serde_json::from_slice::<BlockTrace>(&trace_vec).unwrap();
-    let proof = PROVER
-        .get_mut()
-        .unwrap()
-        .create_agg_circuit_proof(&trace)
-        .unwrap();
-    let proof_bytes = serde_json::to_vec(&proof).unwrap();
-    vec_to_c_char(proof_bytes)
+    let proof_result = panic::catch_unwind(|| {
+        let proof = PROVER
+            .get_mut()
+            .unwrap()
+            .create_agg_circuit_proof(&trace)
+            .unwrap();
+        serde_json::to_vec(&proof).unwrap()
+    });
+    proof_result.map_or(null(), vec_to_c_char)
 }
 
 /// # Safety
@@ -41,11 +45,13 @@ pub unsafe extern "C" fn create_agg_proof(trace_char: *const c_char) -> *const c
 pub unsafe extern "C" fn create_agg_proof_multi(trace_char: *const c_char) -> *const c_char {
     let trace_vec = c_char_to_vec(trace_char);
     let traces = serde_json::from_slice::<Vec<BlockTrace>>(&trace_vec).unwrap();
-    let proof = PROVER
-        .get_mut()
-        .unwrap()
-        .create_agg_circuit_proof_batch(traces.as_slice())
-        .unwrap();
-    let proof_bytes = serde_json::to_vec(&proof).unwrap();
-    vec_to_c_char(proof_bytes)
+    let proof_result = panic::catch_unwind(|| {
+        let proof = PROVER
+            .get_mut()
+            .unwrap()
+            .create_agg_circuit_proof_batch(traces.as_slice())
+            .unwrap();
+        serde_json::to_vec(&proof).unwrap()
+    });
+    proof_result.map_or(null(), vec_to_c_char)
 }
