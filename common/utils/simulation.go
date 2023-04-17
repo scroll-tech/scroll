dd2ea5b182a1b248ec4073a6ed509d6ec7960b5d
maskpp <maskpp266@gmail.com>
2023-04-17 11:14:46 +0800

    Change integration-test in the new way.

--- common/utils/simulation.go
+++ common/utils/simulation.go
@@ -8,10 +8,23 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+type MockAppName string
+
+var (
+	EventWatcherApp   MockAppName = "event-watcher-test"
+	GasOracleApp      MockAppName = "gas-oracle-test"
+	MessageRelayerApp MockAppName = "message-relayer-test"
+	RollupRelayerApp  MockAppName = "rollup-relayer-test"
+
+	CoordinatorApp MockAppName = "coordinator-test"
+	DBCliApp       MockAppName = "db_cli-test"
+	RollerApp      MockAppName = "roller-test"
+)
+
 // RegisterSimulation register initializer function for integration-test.
-func RegisterSimulation(app *cli.App, name string) {
+func RegisterSimulation(app *cli.App, name MockAppName) {
 	// Run the app for integration-test
-	reexec.Register(name, func() {
+	reexec.Register(string(name), func() {
 		if err := app.Run(os.Args); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
