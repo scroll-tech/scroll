bfe29b41c178208092c53566ddc67c6d2fdfde23
zimpha <zimpha@gmail.com>
2023-04-20 00:48:06 +0800

    forward data to receiver after deposit/withdraw

---
+++ contracts/src/test/mocks/MockGatewayRecipient.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {IScrollGatewayCallback} from "../../libraries/callbacks/IScrollGatewayCallback.sol";
+
+contract MockGatewayRecipient is IScrollGatewayCallback {
+    event ReceiveCall(bytes data);
+
+    function onScrollGatewayCallback(bytes memory data) external {
+        emit ReceiveCall(data);
+    }
+
+    receive() external payable {}
+}
