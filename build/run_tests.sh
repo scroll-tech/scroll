7cd0f45eece7eb1e44093c1b3f79ffa6fb6334bb
colinlyguo <colinlyguo@scroll.io>
2023-04-21 02:52:22 +0800

    fix Jenkins test bridge package

---
+++ build/run_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -uex
+
+profile_name=$1
+
+exclude_dirs=("scroll-tech/bridge/cmd" "scroll-tech/bridge/tests" "scroll-tech/bridge/mock_bridge" "scroll-tech/coordinator/cmd")
+
+all_packages=$(go list ./...)
+coverpkg=""
+
+for pkg in $all_packages; do
+    exclude_pkg=false
+    for exclude_dir in "${exclude_dirs[@]}"; do
+        if [[ $pkg == $exclude_dir* ]]; then
+            exclude_pkg=true
+            break
+        fi
+    done
+
+    if [ "$exclude_pkg" = false ]; then
+        if [ -z "$coverpkg" ]; then
+            coverpkg="$pkg/..."
+        else
+            coverpkg="$coverpkg,$pkg/..."
+        fi
+    fi
+done
+
+echo "coverage.${profile_name}.txt"
+
+go test -v -race -coverpkg="$coverpkg" -coverprofile=../coverage.${profile_name}.txt -covermode=atomic ./...
