f98c9f7e48505887f51b5932cd0be481490152fa
Lawliet-Chan <1576710154@qq.com>
2023-04-28 16:16:49 +0800

    Update database/migrate/migrations/00007_agg_task.sql
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

--- database/migrate/migrations/00007_agg_task.sql
+++ database/migrate/migrations/00007_agg_task.sql
@@ -17,6 +17,19 @@ create table agg_task
 create unique index agg_task_hash_uindex
     on agg_task (id);
 
+
+CREATE OR REPLACE FUNCTION update_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_time = CURRENT_TIMESTAMP;
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_timestamp BEFORE UPDATE
+ON agg_task FOR EACH ROW EXECUTE PROCEDURE
+update_timestamp();
+
 -- +goose StatementEnd
 
 -- +goose Down
