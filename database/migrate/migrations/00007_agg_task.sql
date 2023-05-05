9ab552f4592654e5a23e6a55c9b129bcb34c2cff
xinran chen <lawliet@xinran-m1x.local>
2023-05-05 19:16:15 +0800

    fix sql

--- database/migrate/migrations/00007_agg_task.sql
+++ database/migrate/migrations/00007_agg_task.sql
@@ -10,8 +10,8 @@ create table agg_task
     end_batch_hash         VARCHAR  NOT NULL,
     proving_status         SMALLINT DEFAULT 1,
     proof                  BYTEA DEFAULT NULL,
-    created_time           TIMESTAMP(0)    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_time           TIMESTAMP(0)    DEFAULT CURRENT_TIMESTAMP
+    created_at             TIMESTAMP(0)    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at             TIMESTAMP(0)    DEFAULT CURRENT_TIMESTAMP
 );
 
 create unique index agg_task_hash_uindex
