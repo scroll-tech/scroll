efe868472503e1c5f497211db2cf3af3afb58376
xinran chen <lawliet@xinran-m1x.local>
2023-05-08 16:07:22 +0800

    rm blank line

--- bridge/relayer/l1_relayer_test.go
+++ bridge/relayer/l1_relayer_test.go
@@ -6,14 +6,13 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"scroll-tech/database"
 	"scroll-tech/database/migrate"
 
 	"scroll-tech/common/types"
 	"scroll-tech/common/utils"
 
 	"scroll-tech/bridge/sender"
-
-	"scroll-tech/database"
 )
 
 var (
