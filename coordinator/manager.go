e25e8b897edc4d2cfeda0fa18d66afa5f26358cf
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2023-05-08 22:01:08 +0800

    Update manager.go

--- coordinator/manager.go
+++ coordinator/manager.go
@@ -807,7 +807,7 @@ func (m *Manager) VerifyToken(authMsg *message.AuthMsg) (bool, error) {
 	pubkey, _ := authMsg.PublicKey()
 	// GetValue returns nil if value is expired
 	if token, ok := m.tokenCache.Get(pubkey); !ok || token != authMsg.Identity.Token {
-		return false, fmt.Errorf("failed to find corresponding token. roller name: %v. roller pk: %v.", roller.Name, roller.PublicKey)
+		return false, fmt.Errorf("failed to find corresponding token. roller name: %s. roller pk: %s.", authMsg.Identity.Name, pubkey)
 	}
 	return true, nil
 }
