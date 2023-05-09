01365fd6a4384670f09548cd5517671a771c04a7
xinran chen <lawliet@xinran-m1x.local>
2023-05-09 10:25:45 +0800

    upgrade zkevm-v0.3

--- scripts/download_params.sh
+++ scripts/download_params.sh
@@ -5,6 +5,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 PROJ_DIR=$DIR"/.."
 
 mkdir -p $PROJ_DIR/assets/params
-wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/release-1220/test_seed -O $PROJ_DIR/assets/seed
-wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/release-1220/test_params/params19 -O $PROJ_DIR/assets/params/params19
-wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/release-1220/test_params/params26 -O $PROJ_DIR/assets/params/params26
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/test_seed -O $PROJ_DIR/assets/seed
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/test_params/params20 -O $PROJ_DIR/assets/params/params20
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/test_params/params26 -O $PROJ_DIR/assets/params/params26
