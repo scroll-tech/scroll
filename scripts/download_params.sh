d50ef65d7e877f0a071fa5d52a44317b6344860d
xinran chen <lawliet@xinran-m1x.local>
2023-05-09 10:40:18 +0800

    fix

--- scripts/download_params.sh
+++ scripts/download_params.sh
@@ -5,6 +5,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 PROJ_DIR=$DIR"/.."
 
 mkdir -p $PROJ_DIR/assets/params
-wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/test_seed -O $PROJ_DIR/assets/seed
-wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/test_params/params20 -O $PROJ_DIR/assets/params/params20
-wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/test_params/params26 -O $PROJ_DIR/assets/params/params26
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/params20 -O $PROJ_DIR/assets/params/params20
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/params26 -O $PROJ_DIR/assets/params/params26
