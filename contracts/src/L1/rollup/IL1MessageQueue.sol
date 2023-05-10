ff800bee9d77994ec67a6f71e99468e10f404a31
Xi Lin <zimpha@gmail.com>
2023-05-11 03:01:44 +0800

    feat(contract): use rlp encoding instead of abi.encode for L1 message (#395)
    Co-authored-by: Max Wolff <maxcwolff@gmail.com>
    Co-authored-by: Péter Garamvölgyi <peter@scroll.io>
    Co-authored-by: Haichen Shen <shenhaichen@gmail.com>

--- contracts/src/L1/rollup/IL1MessageQueue.sol
+++ contracts/src/L1/rollup/IL1MessageQueue.sol
@@ -39,6 +39,22 @@ interface IL1MessageQueue {
     /// @param gasLimit Gas limit required to complete the message relay on L2.
     function estimateCrossDomainMessageFee(uint256 gasLimit) external view returns (uint256);
 
+    /// @notice Return the hash of a L1 message.
+    /// @param sender The address of sender.
+    /// @param queueIndex The queue index of this message.
+    /// @param value The amount of Ether transfer to target.
+    /// @param target The address of target.
+    /// @param gasLimit The gas limit provided.
+    /// @param data The calldata passed to target address.
+    function computeTransactionHash(
+        address sender,
+        uint256 queueIndex,
+        uint256 value,
+        address target,
+        uint256 gasLimit,
+        bytes calldata data
+    ) external view returns (bytes32);
+
     /*****************************
      * Public Mutating Functions *
      *****************************/
--- contracts/src/L1/rollup/L1MessageQueue.sol
+++ contracts/src/L1/rollup/L1MessageQueue.sol
@@ -11,7 +11,7 @@ import {AddressAliasHelper} from "../../libraries/common/AddressAliasHelper.sol"
 
 /// @title L1MessageQueue
 /// @notice This contract will hold all L1 to L2 messages.
-/// Each appended message is assigned with a unique and increasing `uint256` index denoting the message nonce.
+/// Each appended message is assigned with a unique and increasing `uint256` index.
 contract L1MessageQueue is OwnableUpgradeable, IL1MessageQueue {
     /**********
      * Events *
@@ -67,6 +67,131 @@ contract L1MessageQueue is OwnableUpgradeable, IL1MessageQueue {
         return IL2GasPriceOracle(_oracle).estimateCrossDomainMessageFee(_gasLimit);
     }
 
+    /// @inheritdoc IL1MessageQueue
+    function computeTransactionHash(
+        address _sender,
+        uint256 _queueIndex,
+        uint256 _value,
+        address _target,
+        uint256 _gasLimit,
+        bytes calldata _data
+    ) public pure override returns (bytes32) {
+        // We use EIP-2718 to encode the L1 message, and the encoding of the message is
+        //      `TransactionType || TransactionPayload`
+        // where
+        //  1. `TransactionType` is 0x7E
+        //  2. `TransactionPayload` is `rlp([queueIndex, gasLimit, to, value, data, sender])`
+        //
+        // The spec of rlp: https://ethereum.org/en/developers/docs/data-structures-and-encoding/rlp/
+        uint256 transactionType = 0x7E;
+        bytes32 hash;
+        assembly {
+            function get_uint_bytes(v) -> len {
+                if eq(v, 0) {
+                    len := 1
+                    leave
+                }
+                for {
+
+                } gt(v, 0) {
+
+                } {
+                    len := add(len, 1)
+                    v := shr(8, v)
+                }
+            }
+
+            function store_uint(_ptr, v) -> ptr {
+                ptr := _ptr
+                switch lt(v, 128)
+                case 1 {
+                    // single byte in the [0x00, 0x7f]
+                    mstore(ptr, shl(248, v))
+                    ptr := add(ptr, 1)
+                }
+                default {
+                    // 1-32 bytes long
+                    let len := get_uint_bytes(v)
+                    mstore(ptr, shl(248, add(len, 0x80)))
+                    ptr := add(ptr, 1)
+                    mstore(ptr, shl(mul(8, sub(32, len)), v))
+                    ptr := add(ptr, len)
+                }
+            }
+
+            function store_address(_ptr, v) -> ptr {
+                ptr := _ptr
+                // 20 bytes long
+                mstore(ptr, shl(248, 0x94)) // 0x80 + 0x14
+                ptr := add(ptr, 1)
+                mstore(ptr, shl(96, v))
+                ptr := add(ptr, 0x14)
+            }
+
+            // 1 byte for TransactionType
+            // 4 byte for list payload length
+            let start_ptr := add(mload(0x40), 5)
+            let ptr := start_ptr
+            ptr := store_uint(ptr, _queueIndex)
+            ptr := store_uint(ptr, _gasLimit)
+            ptr := store_address(ptr, _target)
+            ptr := store_uint(ptr, _value)
+
+            switch eq(_data.length, 1)
+            case 1 {
+                // single byte
+                ptr := store_uint(ptr, shr(248, calldataload(_data.offset)))
+            }
+            default {
+                switch lt(_data.length, 56)
+                case 1 {
+                    // a string is 0-55 bytes long
+                    mstore(ptr, shl(248, add(0x80, _data.length)))
+                    ptr := add(ptr, 1)
+                    calldatacopy(ptr, _data.offset, _data.length)
+                    ptr := add(ptr, _data.length)
+                }
+                default {
+                    // a string is more than 55 bytes long
+                    let len_bytes := get_uint_bytes(_data.length)
+                    mstore(ptr, shl(248, add(0xb7, len_bytes)))
+                    ptr := add(ptr, 1)
+                    mstore(ptr, shl(mul(8, sub(32, len_bytes)), _data.length))
+                    ptr := add(ptr, len_bytes)
+                    calldatacopy(ptr, _data.offset, _data.length)
+                    ptr := add(ptr, _data.length)
+                }
+            }
+            ptr := store_address(ptr, _sender)
+
+            let payload_len := sub(ptr, start_ptr)
+            let value
+            let value_bytes
+            switch lt(payload_len, 56)
+            case 1 {
+                // the total payload of a list is 0-55 bytes long
+                value := add(0xc0, payload_len)
+                value_bytes := 1
+            }
+            default {
+                // If the total payload of a list is more than 55 bytes long
+                let len_bytes := get_uint_bytes(payload_len)
+                value_bytes := add(len_bytes, 1)
+                value := add(0xf7, len_bytes)
+                value := shl(mul(len_bytes, 8), value)
+                value := or(value, payload_len)
+            }
+            value := or(value, shl(mul(8, value_bytes), transactionType))
+            value_bytes := add(value_bytes, 1)
+            let value_bits := mul(8, value_bytes)
+            value := or(shl(sub(256, value_bits), value), shr(value_bits, mload(start_ptr)))
+            start_ptr := sub(start_ptr, value_bytes)
+            mstore(start_ptr, value)
+            hash := keccak256(start_ptr, sub(ptr, start_ptr))
+        }
+        return hash;
+    }
+
     /*****************************
      * Public Mutating Functions *
      *****************************/
@@ -82,13 +207,12 @@ contract L1MessageQueue is OwnableUpgradeable, IL1MessageQueue {
         // do address alias to avoid replay attack in L2.
         address _sender = AddressAliasHelper.applyL1ToL2Alias(msg.sender);
 
-        // @todo Change it to rlp encoding later.
-        bytes32 _hash = keccak256(abi.encode(_sender, _target, 0, _gasLimit, _data));
-
+        // compute transaction hash
         uint256 _queueIndex = messageQueue.length;
-        emit QueueTransaction(_sender, _target, 0, _queueIndex, _gasLimit, _data);
-
+        bytes32 _hash = computeTransactionHash(_sender, _queueIndex, 0, _target, _gasLimit, _data);
         messageQueue.push(_hash);
+
+        emit QueueTransaction(_sender, _target, 0, _queueIndex, _gasLimit, _data);
     }
 
     /// @inheritdoc IL1MessageQueue
