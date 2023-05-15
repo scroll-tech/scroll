9384dd2576b0742dc76e13a0a6029d2fee83de6e
xinran chen <lawliet@xinran-m1x.local>
2023-05-15 10:31:19 +0800

    revert seed file

--- scripts/download_params.sh
+++ scripts/download_params.sh
@@ -7,3 +7,5 @@ PROJ_DIR=$DIR"/.."
 mkdir -p $PROJ_DIR/assets/params
 wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/params20 -O $PROJ_DIR/assets/params/params20
 wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/params-0320/params26 -O $PROJ_DIR/assets/params/params26
+wget https://circuit-release.s3.us-west-2.amazonaws.com/circuit-release/release-1220/test_seed -O $PROJ_DIR/assets/seed
+
