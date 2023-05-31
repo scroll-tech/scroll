b1857187a494289d64c6f9121fe70cbd86d418fc
vincent <419436363@qq.com>
2023-05-31 17:34:27 +0800

    add

--- build/post-test-report-coverage.sh
+++ build/post-test-report-coverage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -uex
-~/go/bin/gocover-cobertura < coverage.bridge.txt > coverage.bridge.xml
-~/go/bin/gocover-cobertura < coverage.db.txt > coverage.db.xml
-~/go/bin/gocover-cobertura < coverage.common.txt > coverage.common.xml
-~/go/bin/gocover-cobertura < coverage.coordinator.txt > coverage.coordinator.xml
+${GOROOT}/bin/gocover-cobertura < coverage.bridge.txt > coverage.bridge.xml
+${GOROOT}/bin/gocover-cobertura < coverage.db.txt > coverage.db.xml
+${GOROOT}/bin/gocover-cobertura < coverage.common.txt > coverage.common.xml
+${GOROOT}/bin/gocover-cobertura < coverage.coordinator.txt > coverage.coordinator.xml
 #${GOROOT}/bin/bin/gocover-cobertura < coverage.integration.txt > coverage.integration.xml
 
 npx cobertura-merge -o cobertura.xml \
