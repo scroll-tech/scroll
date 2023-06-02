7e15310d413a5cc5612bf0fb5c8c278742e02ff1
vincent <419436363@qq.com>
2023-06-02 21:15:44 +0800

    fix

--- bridge-history-api/cmd/cross_msg_fetcher/app/app.go
+++ bridge-history-api/cmd/cross_msg_fetcher/app/app.go
@@ -98,11 +98,11 @@ func action(ctx *cli.Context) error {
 	go l2crossMsgFetcher.Start()
 	defer l2crossMsgFetcher.Stop()
 
-	l1BlocktimeFetcher := cross_msg.NewBlocktimestampFetcher(subCtx, uint(cfg.L1.Confirmation), int(cfg.L1.BlockTime), l1client, db.UpdateL1Blocktimestamp, db.GetL1LatestNoBlocktimestampHeight)
+	l1BlocktimeFetcher := cross_msg.NewBlocktimestampFetcher(subCtx, uint(cfg.L1.Confirmation), int(cfg.L1.BlockTime), l1client, db.UpdateL1Blocktimestamp, db.GetL1EarliestNoBlocktimestampHeight)
 	go l1BlocktimeFetcher.Start()
 	defer l1BlocktimeFetcher.Stop()
 
-	l2BlocktimeFetcher := cross_msg.NewBlocktimestampFetcher(subCtx, uint(cfg.L2.Confirmation), int(cfg.L2.BlockTime), l2client, db.UpdateL2Blocktimestamp, db.GetL2LatestNoBlocktimestampHeight)
+	l2BlocktimeFetcher := cross_msg.NewBlocktimestampFetcher(subCtx, uint(cfg.L2.Confirmation), int(cfg.L2.BlockTime), l2client, db.UpdateL2Blocktimestamp, db.GetL2EarliestNoBlocktimestampHeight)
 	go l2BlocktimeFetcher.Start()
 	defer l2BlocktimeFetcher.Stop()
 
