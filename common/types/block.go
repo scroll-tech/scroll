86b67f6dad7cd8fbcd3df526c48664aca123f905
Richord <rzhang139@gmail.com>
2023-06-13 10:08:07 -0700

    feat(batch proposer): chunk encoding using L1MsgTxs (#557)
    Co-authored-by: Péter Garamvölgyi <peter@scroll.io>

--- common/types/block.go
+++ common/types/block.go
@@ -24,22 +24,25 @@ func (w *WrappedBlock) Encode() ([]byte, error) {
 	if !w.Header.Number.IsUint64() {
 		return nil, errors.New("block number is not uint64")
 	}
-
 	if len(w.Transactions) > math.MaxUint16 {
 		return nil, errors.New("number of transactions exceeds max uint16")
 	}
+	var numL1Messages uint16
+	for _, txData := range w.Transactions {
+		if txData.Type == 0x7E {
+			if numL1Messages == math.MaxUint16 {
+				return nil, errors.New("number of L1 messages exceeds max uint16")
+			}
+			numL1Messages++
+		}
+	}
 
 	binary.BigEndian.PutUint64(bytes[0:], w.Header.Number.Uint64())
-
 	binary.BigEndian.PutUint64(bytes[8:], w.Header.Time)
-
-	// TODO: Currently, baseFee is 0
-
+	// TODO: [16:47] Currently, baseFee is 0, because we disable EIP-1559.
 	binary.BigEndian.PutUint64(bytes[48:], w.Header.GasLimit)
-
 	binary.BigEndian.PutUint16(bytes[56:], uint16(len(w.Transactions)))
-
-	// TODO: set numL1Messages properly
+	binary.BigEndian.PutUint16(bytes[58:], numL1Messages)
 
 	return bytes, nil
 }
