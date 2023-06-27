399d8cf95d8aa0d0bf284328f131731c9fb6b79f
colinlyguo <colinlyguo@scroll.io>
2023-06-28 00:14:45 +0800

    fix event signature

--- bridge/abi/bridge_abi_test.go
+++ bridge/abi/bridge_abi_test.go
@@ -16,9 +16,9 @@ func TestEventSignature(t *testing.T) {
 	assert.Equal(L1FailedRelayedMessageEventSignature, common.HexToHash("99d0e048484baa1b1540b1367cb128acd7ab2946d1ed91ec10e3c85e4bf51b8f"))
 
 	assert.Equal(L1CommitBatchEventSignature, common.HexToHash("2cdc615c74452778c0fb6184735e014c13aad2b62774fe0b09bd1dcc2cc14a62"))
-	assert.Equal(L1FinalizeBatchEventSignature, common.HexToHash("0x9d3058a3cb9739a2527f22dd9a4138065844037d3004254952e2458d808cc364"))
+	assert.Equal(L1FinalizeBatchEventSignature, common.HexToHash("9d3058a3cb9739a2527f22dd9a4138065844037d3004254952e2458d808cc364"))
 
-	assert.Equal(L1QueueTransactionEventSignature, common.HexToHash("bdcc7517f8fe3db6506dfd910942d0bbecaf3d6a506dadea65b0d988e75b9439"))
+	assert.Equal(L1QueueTransactionEventSignature, common.HexToHash("69cfcb8e6d4192b8aba9902243912587f37e550d75c1fa801491fce26717f37e"))
 
 	assert.Equal(L2SentMessageEventSignature, common.HexToHash("104371f3b442861a2a7b82a070afbbaab748bb13757bf47769e170e37809ec1e"))
 	assert.Equal(L2RelayedMessageEventSignature, common.HexToHash("4641df4a962071e12719d8c8c8e5ac7fc4d97b927346a3d7a335b1f7517e133c"))
