0a577470856cf0211504317f2763824e1a367310
Max Wolff <maxcwolff@gmail.com>
2023-06-27 21:36:44 -0700

    add wip cli

--- contracts/scripts/foundry/DeployL1AdminContracts.s.sol
+++ contracts/scripts/foundry/DeployL1AdminContracts.s.sol
@@ -17,14 +17,14 @@ contract DeployL1AdminContracts is Script {
         address council_safe = deploySafe();
         // deploy timelock with no delay just to have flow between council and scroll admin
         address council_timelock = deployTimelockController(council_safe, 0);
-        
+
         logAddress("L1_COUNCIL_SAFE_ADDR", address(council_safe));
         logAddress("L1_COUNCIL_TIMELOCK_ADDR", address(council_timelock));
 
         address scroll_safe = deploySafe();
         // TODO: get timelock delay from env. for now just use 2 days
         address scroll_timelock = deployTimelockController(scroll_safe, 2 days);
-        
+
         logAddress("L1_SCROLL_SAFE_ADDR", address(scroll_safe));
         logAddress("L1_SCROLL_TIMELOCK_ADDR", address(scroll_timelock));
 
@@ -47,27 +47,20 @@ contract DeployL1AdminContracts is Script {
         address[] memory owners = new address[](1);
         owners[0] = owner;
         // deployer 1/1. no gas refunds for now
-        safe.setup(
-            owners,
-            1,
-            address(0),
-            new bytes(0),
-            address(0),
-            address(0),
-            0,
-            payable(address(0))
-        );
+        safe.setup(owners, 1, address(0), new bytes(0), address(0), address(0), 0, payable(address(0)));
         return address(safe);
     }
 
-    function deployTimelockController(address safe, uint delay) internal returns(address) {
+    function deployTimelockController(address safe, uint256 delay) internal returns (address) {
         address deployer = vm.addr(L1_DEPLOYER_PRIVATE_KEY);
 
         address[] memory proposers = new address[](1);
         proposers[0] = safe;
         // add SAFE as the only proposer, anyone can execute
-        TimelockController timelock = new TimelockController(delay, proposers, new address[](0));
-        
+        address[] memory executors = new address[](1);
+        executors[0] = deployer;
+        TimelockController timelock = new TimelockController(delay, proposers, executors);
+
         bytes32 TIMELOCK_ADMIN_ROLE = keccak256("TIMELOCK_ADMIN_ROLE");
 
         // make safe admin of timelock, then revoke deployer's rights
