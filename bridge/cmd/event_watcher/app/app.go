dc71a95439d6368d956eac43f48cb3a5673dcd1e
georgehao <haohongfan@gmail.com>
2023-07-04 15:26:01 +0800

    feat: add logger interface

--- bridge/cmd/event_watcher/app/app.go
+++ bridge/cmd/event_watcher/app/app.go
@@ -51,7 +51,7 @@ func action(ctx *cli.Context) error {
 
 	subCtx, cancel := context.WithCancel(ctx.Context)
 	// Init db connection
-	db, err := utils.InitDB(cfg.DBConfig)
+	db, err := utils.InitDB(cfg.DBConfig, logger)
 	if err != nil {
 		log.Crit("failed to init db connection", "err", err)
 	}
