81ae4d3e7bd01cc4fc3cca6cb7472b8b83e808df
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2023-07-10 12:28:51 +0800

    refactor(libzkp): suppress compilation warnings (#575)

--- common/libzkp/interface/libzkp.h
+++ common/libzkp/interface/libzkp.h
@@ -1,5 +1,5 @@
-init_prover(char *params_path, char *seed_path);
+void init_prover(char *params_path, char *seed_path);
 char* create_agg_proof(char *trace);
 char* create_agg_proof_multi(char *trace);
-init_verifier(char *params_path, char *agg_vk_path);
+void init_verifier(char *params_path, char *agg_vk_path);
 char verify_agg_proof(char *proof);
