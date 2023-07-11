6f3236c5d5d1d4b972000c3d61178e837e136e8f
georgehao <haohongfan@gmail.com>
2023-07-11 16:41:22 +0800

    feat: add uint256 uint test

--- coordinator/internal/orm/orm_test.go
+++ coordinator/internal/orm/orm_test.go
@@ -81,7 +81,7 @@ func TestProverTaskOrm(t *testing.T) {
 	assert.Equal(t, proverTask.ProverName, proverTasks[0].ProverName)
 
 	// // test decimal reward, get reward
-	resultReward := proverTask.Reward.BigInt()
+	resultReward := proverTasks[0].Reward.BigInt()
 	assert.Equal(t, resultReward, reward)
 	assert.Equal(t, resultReward.String(), "18446744073709551616")
 
@@ -92,16 +92,30 @@ func TestProverTaskOrm(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(proverTasks))
 	assert.Equal(t, proverTask.ProvingStatus, proverTasks[0].ProvingStatus)
+}
+
+func TestProverTaskOrmUint256(t *testing.T) {
+	sqlDB, err := db.DB()
+	assert.NoError(t, err)
+	assert.NoError(t, migrate.ResetDB(sqlDB))
 
 	// test reward for uint256 maximum 1 << 256 -1 :115792089237316195423570985008687907853269984665640564039457584007913129639935
 	rewardUint256 := big.NewInt(0)
 	rewardUint256.SetString("115792089237316195423570985008687907853269984665640564039457584007913129639935", 10)
-	proverTask.Reward = decimal.NewFromBigInt(rewardUint256, 0)
+	proverTask := ProverTask{
+		TaskID:          "test-hash",
+		ProverName:      "roller-0",
+		ProverPublicKey: "0",
+		ProvingStatus:   int16(types.RollerAssigned),
+		Reward:          decimal.NewFromBigInt(rewardUint256, 0),
+	}
+
 	err = proverTaskOrm.SetProverTask(context.Background(), &proverTask)
 	assert.NoError(t, err)
-	proverTasks, err = proverTaskOrm.GetProverTasksByHashes(context.Background(), []string{"test-hash"})
+	proverTasksUint256, err := proverTaskOrm.GetProverTasksByHashes(context.Background(), []string{"test-hash"})
 	assert.NoError(t, err)
-	resultRewardUint256 := proverTask.Reward.BigInt()
+	assert.Equal(t, 1, len(proverTasksUint256))
+	resultRewardUint256 := proverTasksUint256[0].Reward.BigInt()
 	assert.Equal(t, resultRewardUint256, rewardUint256)
 	assert.Equal(t, resultRewardUint256.String(), "115792089237316195423570985008687907853269984665640564039457584007913129639935")
 }
