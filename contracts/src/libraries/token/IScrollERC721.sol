03fcb4b2eeebbb46970aba53cd63c1c273611f53
turupawn <ahmed.hn.43@gmail.com>
2023-07-17 20:20:50 -0600

    added token interface extensions

--- contracts/src/libraries/token/IScrollERC721.sol
+++ contracts/src/libraries/token/IScrollERC721.sol
@@ -3,22 +3,6 @@
 pragma solidity ^0.8.0;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IScrollERC721Extension} from "./IScrollERC721Extension.sol";
 
-interface IScrollERC721 is IERC721 {
-    /// @notice Return the address of Gateway the token belongs to.
-    function gateway() external view returns (address);
-
-    /// @notice Return the address of counterpart token.
-    function counterpart() external view returns (address);
-
-    /// @notice Mint some token to recipient's account.
-    /// @dev Gateway Utilities, only gateway contract can call
-    /// @param _to The address of recipient.
-    /// @param _tokenId The token id to mint.
-    function mint(address _to, uint256 _tokenId) external;
-
-    /// @notice Burn some token from account.
-    /// @dev Gateway Utilities, only gateway contract can call
-    /// @param _tokenId The token id to burn.
-    function burn(uint256 _tokenId) external;
-}
+interface IScrollERC721 is IERC721, IScrollERC721Extension {}
