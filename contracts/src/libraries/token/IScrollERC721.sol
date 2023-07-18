9f6f6b9ccfd082f62d715598b3dce72496262ba0
turupawn <ahmed.hn.43@gmail.com>
2023-07-17 21:01:37 -0600

    added IScrollERC721 docs

--- contracts/src/libraries/token/IScrollERC721.sol
+++ contracts/src/libraries/token/IScrollERC721.sol
@@ -5,4 +5,9 @@ pragma solidity ^0.8.0;
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {IScrollERC721Extension} from "./IScrollERC721Extension.sol";
 
-interface IScrollERC721 is IERC721, IScrollERC721Extension {}
+// The recommended ERC721 implementation for bridge token.
+// deployed in L2 when original token is on L1
+// deployed in L1 when original token is on L2
+interface IScrollERC721 is IERC721, IScrollERC721Extension {
+
+}
