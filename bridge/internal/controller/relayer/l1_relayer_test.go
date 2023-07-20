076cfc1eae6745bf59d42393eb8845f7a1ce5ee6
colin <102356659+colinlyguo@users.noreply.github.com>
2023-07-20 17:26:43 +0800

    refactor(relayer): optimize l1 block schema (#648)
    Co-authored-by: georgehao <haohongfan@gmail.com>
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

--- bridge/internal/controller/relayer/l1_relayer_test.go
+++ bridge/internal/controller/relayer/l1_relayer_test.go
@@ -128,8 +128,8 @@ func testL1RelayerGasOracleConfirm(t *testing.T) {
 	l1BlockOrm := orm.NewL1Block(db)
 
 	l1Block := []orm.L1Block{
-		{Hash: "gas-oracle-1", Number: 0, GasOracleStatus: int16(types.GasOraclePending), BlockStatus: int16(types.L1BlockPending)},
-		{Hash: "gas-oracle-2", Number: 1, GasOracleStatus: int16(types.GasOraclePending), BlockStatus: int16(types.L1BlockPending)},
+		{Hash: "gas-oracle-1", Number: 0, GasOracleStatus: int16(types.GasOraclePending)},
+		{Hash: "gas-oracle-2", Number: 1, GasOracleStatus: int16(types.GasOraclePending)},
 	}
 	// Insert test data.
 	assert.NoError(t, l1BlockOrm.InsertL1Blocks(context.Background(), l1Block))
