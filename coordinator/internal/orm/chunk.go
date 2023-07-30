e115a39bb686869b74c2db1e0d560e2a2e35b8d9
Steven <asongala@163.com>
2023-07-30 15:37:41 +0800

    Update coordinator/internal/orm/chunk.go
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

--- coordinator/internal/orm/chunk.go
+++ coordinator/internal/orm/chunk.go
@@ -104,7 +104,7 @@ func (o *Chunk) GetProofsByBatchHash(ctx context.Context, batchHash string) ([]*
 
 	var proofs []*message.ChunkProof
 	for _, chunk := range chunks {
-		var proof message.BatchProof
+		var proof message.ChunkProof
 		if err := json.Unmarshal(chunk.Proof, &proof); err != nil {
 			return nil, fmt.Errorf("Chunk.GetProofsByBatchHash error: %w, batch hash: %v, chunk hash: %v", err, batchHash, chunk.Hash)
 		}
