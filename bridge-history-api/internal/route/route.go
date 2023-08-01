c5590e9434c5d07351c880f2ceddb5818b66fc4f
ChuhanJin <60994121+ChuhanJin@users.noreply.github.com>
2023-08-02 00:05:48 +0800

    refactor(bridge-history-api): server switch to gin framework (#672)
    Co-authored-by: colinlyguo <colinlyguo@scroll.io>
    Co-authored-by: colin <102356659+colinlyguo@users.noreply.github.com>
    Co-authored-by: georgehao <haohongfan@gmail.com>
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

---
+++ bridge-history-api/internal/route/route.go
@@ -0,0 +1,26 @@
+package route
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	"bridge-history-api/config"
+	"bridge-history-api/internal/controller"
+)
+
+// Route routes the APIs
+func Route(router *gin.Engine, conf *config.Config) {
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r := router.Group("api/")
+	r.GET("/txs", controller.HistoryCtrler.GetAllTxsByAddr)
+	r.POST("/txsbyhashes", controller.HistoryCtrler.PostQueryTxsByHash)
+	r.GET("/claimable", controller.HistoryCtrler.GetAllClaimableTxsByAddr)
+}
