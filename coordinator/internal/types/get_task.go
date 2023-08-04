602895daa0f50fa4e7dc09be7b078b2536c73e85
georgehao <haohongfan@gmail.com>
2023-08-04 16:35:24 +0800

    feat: remove prover_height required tag

--- coordinator/internal/types/get_task.go
+++ coordinator/internal/types/get_task.go
@@ -3,7 +3,7 @@ package types
 // GetTaskParameter for ProverTasks request parameter
 type GetTaskParameter struct {
 	ProverVersion string `form:"prover_version" json:"prover_version" binding:"required"`
-	ProverHeight  int    `form:"prover_height" json:"prover_height" binding:"required"`
+	ProverHeight  int    `form:"prover_height" json:"prover_height"`
 	TaskType      int    `form:"task_type" json:"task_type"`
 }
 
