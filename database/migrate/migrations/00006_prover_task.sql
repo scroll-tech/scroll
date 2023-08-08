3a80500ffdc40bb36db39eec09a6d1fb0e4efea5
colinlyguo <colinlyguo@scroll.io>
2023-08-09 06:47:11 +0800

    change not null to default null for compability

--- database/migrate/migrations/00006_prover_task.sql
+++ database/migrate/migrations/00006_prover_task.sql
@@ -8,7 +8,7 @@ create table prover_task
 -- prover
     prover_public_key   VARCHAR        NOT NULL,
     prover_name         VARCHAR        NOT NULL,
-    real_ip             VARCHAR        NOT NULL,
+    real_ip             VARCHAR        DEFAULT NULL,
 
 -- task
     task_id             VARCHAR        NOT NULL,
