437e0c3ec30f0625977c536235065827bd130b25
HAOYUatHZ <haoyu@protonmail.com>
2023-08-09 15:31:57 +0800

    fix

--- database/migrate/migrations/00006_prover_task.sql
+++ database/migrate/migrations/00006_prover_task.sql
@@ -8,8 +8,7 @@ create table prover_task
 -- prover
     prover_public_key   VARCHAR        NOT NULL,
     prover_name         VARCHAR        NOT NULL,
-    -- added in 20230809_1153.sql
-    -- prover_version         VARCHAR        NOT NULL,
+    prover_version      VARCHAR        NOT NULL,
 
 -- task
     task_id             VARCHAR        NOT NULL,
--- database/migrate/migrations/20230809_1153.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE prover_task
-ADD COLUMN
-prover_version         VARCHAR        NOT NULL;
