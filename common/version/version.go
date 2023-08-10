21269f1770156abd00229d6c10e0deefc18ea1ee
maskpp <maskpp266@gmail.com>
2023-08-10 17:01:50 +0800

    Update common/version/version.go
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

--- common/version/version.go
+++ common/version/version.go
@@ -25,8 +25,8 @@ var commit = func() string {
 }()
 
 // ZkVersion is commit-id of common/libzkp/impl/cargo.lock/scroll-prover and halo2, contacted by a "-"
-// The default `zk-000000` is set for integration test.
-var ZkVersion = "zk-000000"
+// The default `000000-000000` is set for integration test, and will be overwritten by coordinator's & prover's actual compilations (see their Makefiles).
+var ZkVersion = "000000-000000"
 
 // Version denote the version of scroll protocol, including the l2geth, relayer, coordinator, prover, contracts and etc.
 var Version = fmt.Sprintf("%s-%s-%s", tag, commit, ZkVersion)
