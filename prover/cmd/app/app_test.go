14aa1adb9d47ad3eab07ac5da6c7c884835d725a
maskpp <maskpp266@gmail.com>
2023-08-10 14:57:38 +0800

    fix bug at TestCoordinatorProverInteraction

--- prover/cmd/app/app_test.go
+++ prover/cmd/app/app_test.go
@@ -2,19 +2,28 @@ package app
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
 	"scroll-tech/common/cmd"
+	"scroll-tech/common/utils"
 	"scroll-tech/common/version"
 )
 
-func TestRunProver(t *testing.T) {
-	prover := cmd.NewCmd("prover-test", "--version")
+func TestRunChunkProver(t *testing.T) {
+	prover := cmd.NewCmd(string(utils.ChunkProverApp), "--version")
 	defer prover.WaitExit()
 
 	// wait result
-	prover.ExpectWithTimeout(t, true, time.Second*3, fmt.Sprintf("prover version %s-", strings.Split(version.Version, "-")[0]))
+	prover.ExpectWithTimeout(t, true, time.Second*3, fmt.Sprintf("prover version %s", version.Version))
+	prover.RunApp(nil)
+}
+
+func TestRunBatchProver(t *testing.T) {
+	prover := cmd.NewCmd(string(utils.BatchProverApp), "--version")
+	defer prover.WaitExit()
+
+	// wait result
+	prover.ExpectWithTimeout(t, true, time.Second*3, fmt.Sprintf("prover version %s", version.Version))
 	prover.RunApp(nil)
 }
