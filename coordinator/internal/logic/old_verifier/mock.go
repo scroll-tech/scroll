5955329cff4d8660c9727149fb97f5a194d3f6a5
Lawliet-Chan <1576710154@qq.com>
2023-08-15 11:34:14 +0800

    fix lint

--- coordinator/internal/logic/old_verifier/mock.go
+++ coordinator/internal/logic/old_verifier/mock.go
@@ -1,25 +1,24 @@
 //go:build mock_verifier
 
-package verifier
+package old_verifier
 
 import (
 	"scroll-tech/common/types/message"
-
 	"scroll-tech/coordinator/internal/config"
 )
 
 const InvalidTestProof = "this is a invalid proof"
 
-// Verifier represents a mock halo2 verifier.
-type Verifier struct{}
+// OldVerifier represents a mock halo2 verifier.
+type OldVerifier struct{}
 
 // NewVerifier Sets up a mock verifier.
-func NewVerifier(_ *config.VerifierConfig) (*Verifier, error) {
-	return &Verifier{}, nil
+func NewOldVerifier(_ *config.VerifierConfig) (*OldVerifier, error) {
+	return &OldVerifier{}, nil
 }
 
 // VerifyChunkProof return a mock verification result for a ChunkProof.
-func (v *Verifier) VerifyChunkProof(proof *message.ChunkProof) (bool, error) {
+func (v *OldVerifier) VerifyChunkProof(proof *message.ChunkProof) (bool, error) {
 	if string(proof.Proof) == InvalidTestProof {
 		return false, nil
 	}
@@ -27,7 +26,7 @@ func (v *Verifier) VerifyChunkProof(proof *message.ChunkProof) (bool, error) {
 }
 
 // VerifyBatchProof return a mock verification result for a BatchProof.
-func (v *Verifier) VerifyBatchProof(proof *message.BatchProof) (bool, error) {
+func (v *OldVerifier) VerifyBatchProof(proof *message.BatchProof) (bool, error) {
 	if string(proof.Proof) == InvalidTestProof {
 		return false, nil
 	}
