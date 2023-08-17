b85a109fd37ee312c020b4c86f7bce56f6670f57
georgehao <haohongfan@gmail.com>
2023-08-17 21:15:27 +0800

    feat(coordinator): when prover failure, recover status (#814)
    Co-authored-by: georgehao <georgehao@users.noreply.github.com>

--- coordinator/internal/types/submit_proof.go
+++ coordinator/internal/types/submit_proof.go
@@ -2,8 +2,10 @@ package types
 
 // SubmitProofParameter the SubmitProof api request parameter
 type SubmitProofParameter struct {
-	TaskID   string `form:"task_id" json:"task_id" binding:"required"`
-	TaskType int    `form:"task_type" json:"task_type" binding:"required"`
-	Status   int    `form:"status" json:"status"`
-	Proof    string `form:"proof" json:"proof"`
+	TaskID      string `form:"task_id" json:"task_id" binding:"required"`
+	TaskType    int    `form:"task_type" json:"task_type" binding:"required"`
+	Status      int    `form:"status" json:"status"`
+	Proof       string `form:"proof" json:"proof"`
+	FailureType int    `form:"failure_type" json:"failure_type"`
+	FailureMsg  string `form:"failure_msg" json:"failure_msg"`
 }
