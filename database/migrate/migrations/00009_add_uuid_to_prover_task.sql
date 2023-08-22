d0d4b6e4f3be6f8fb3e319762c28e2a70caedfe4
georgehao <haohongfan@gmail.com>
2023-08-22 15:49:46 +0800

    feat: update

--- database/migrate/migrations/00009_add_uuid_to_prover_task.sql
+++ database/migrate/migrations/00009_add_uuid_to_prover_task.sql
@@ -2,16 +2,15 @@
 -- +goose StatementBegin
 ALTER TABLE prover_task ADD COLUMN uuid uuid DEFAULT gen_random_uuid() NOT NULL UNIQUE;
 
-DROP index uk_tasktype_taskid_publickey_version;
+DROP index if exists uk_tasktype_taskid_publickey_version;
 
-DROP index idx_uuid;
-create index idx_uuid on prover_task (uuid) where deleted_at IS NULL;
+create index if not exists idx_uuid on prover_task (uuid) where deleted_at IS NULL;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-drop index uk_tasktype_taskid_publickey_version;
+drop index if exists uk_tasktype_taskid_publickey_version;
 
-create unique index uk_tasktype_taskid_publickey_version
+create unique index if not exists uk_tasktype_taskid_publickey_version
 on prover_task (task_type, task_id, prover_public_key, prover_version)) where deleted_at IS NULL;
 -- +goose StatementEnd
