12eb3c510d7c01cd592f153ea6271571746dce0a
georgehao <haohongfan@gmail.com>
2023-08-27 00:31:44 +0800

    feat: update

--- database/migrate/migrations/00009_add_index_chunk_prover_task.sql
+++ database/migrate/migrations/00009_add_index_chunk_prover_task.sql
@@ -3,9 +3,7 @@
 
 create index if not exists idx_chunk_hash on chunk(hash, deleted_at) where deleted_at IS NULL;
 
-CREATE INDEX idx_chunk_index ON chunk(index) WHERE deleted_at IS NULL;
-
-create index if not exists idx_proving_status_end_block_number_index on chunk(proving_status, end_block_number,deleted_at, index) where deleted_at IS NULL;
+create index if not exists idx_proving_status_end_block_number_index on chunk(index, end_block_number, proving_status, deleted_at) where deleted_at IS NULL;
 
 create index if not exists  idx_publickey_proving_status on prover_task(prover_public_key, proving_status, deleted_at, id) where deleted_at is null;
 
