70189f8be0fb43d4d905c41087ae0ec11b5520bb
georgehao <haohongfan@gmail.com>
2023-08-28 16:46:15 +0800

    Update database/migrate/migrations/00010_add_index_chunk_prover_task.sql
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

--- database/migrate/migrations/00010_add_index_chunk_prover_task.sql
+++ database/migrate/migrations/00010_add_index_chunk_prover_task.sql
@@ -5,7 +5,7 @@ create index if not exists idx_chunk_hash on chunk(hash, deleted_at) where delet
 
 create index if not exists idx_proving_status_end_block_number_index on chunk(index, end_block_number, proving_status, deleted_at) where deleted_at IS NULL;
 
-create index if not exists  idx_publickey_proving_status on prover_task(prover_public_key, proving_status, deleted_at, id) where deleted_at is null;
+create index if not exists idx_publickey_proving_status on prover_task(prover_public_key, proving_status, deleted_at, id) where deleted_at is null;
 
 -- +goose StatementEnd
 
