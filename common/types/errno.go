d2527097e0f7b9afda7a1000ef04bdac2d5547be
georgehao <haohongfan@gmail.com>
2023-09-08 20:15:33 +0800

    feat: fix comments

--- common/types/errno.go
+++ common/types/errno.go
@@ -3,7 +3,7 @@ package types
 const (
 	// Success shows OK.
 	Success = 0
-	// InternalServerError show server fatal
+	// InternalServerError shows a fatal error in the server
 	InternalServerError = 500
 
 	// ErrJWTCommonErr jwt common error
