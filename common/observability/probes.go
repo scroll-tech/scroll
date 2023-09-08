98de7a5faf44c0599d53364c836680bc2a7a78ed
georgehao <haohongfan@gmail.com>
2023-09-08 14:52:45 +0800

    feat: format

--- common/observability/probes.go
+++ common/observability/probes.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
-	
+
 	"scroll-tech/common/database"
 	"scroll-tech/common/types"
 )
--- rollup/cmd/gas_oracle/app/app.go
+++ rollup/cmd/gas_oracle/app/app.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"scroll-tech/common/observability"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -15,6 +14,7 @@ import (
 	"github.com/urfave/cli/v2"
 
 	"scroll-tech/common/database"
+	"scroll-tech/common/observability"
 	"scroll-tech/common/utils"
 	"scroll-tech/common/version"
 
