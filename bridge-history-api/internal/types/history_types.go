7f5338ee45bbe886bdc32c31375e13e40645675c
georgehao <haohongfan@gmail.com>
2023-09-08 19:43:02 +0800

    Update bridge-history-api/internal/types/history_types.go
    Co-authored-by: Péter Garamvölgyi <peter@scroll.io>

--- bridge-history-api/internal/types/history_types.go
+++ bridge-history-api/internal/types/history_types.go
@@ -10,7 +10,7 @@ import (
 const (
 	// Success shows OK.
 	Success = 0
-	// InternalServerError show server fatal
+	// InternalServerError shows a fatal error in the server
 	InternalServerError = 500
 	// ErrParameterInvalidNo is invalid params
 	ErrParameterInvalidNo = 40001
