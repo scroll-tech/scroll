b784f5bafcd9ed590f65656423ca88d1bcab1959
zimpha <zimpha@gmail.com>
2023-09-20 19:41:50 +0800

    add InitializeL2RateLimiter for initialization

--- contracts/scripts/foundry/DeployL2RateLimiter.s.sol
+++ contracts/scripts/foundry/DeployL2RateLimiter.s.sol
@@ -15,11 +15,6 @@ contract DeployL2RateLimiter is Script {
 
     address L2_SCROLL_MESSENGER_PROXY_ADDR = vm.envAddress("L2_SCROLL_MESSENGER_PROXY_ADDR");
 
-    address L2_CUSTOM_ERC20_GATEWAY_PROXY_ADDR = vm.envAddress("L2_CUSTOM_ERC20_GATEWAY_PROXY_ADDR");
-    address L2_STANDARD_ERC20_GATEWAY_PROXY_ADDR = vm.envAddress("L2_STANDARD_ERC20_GATEWAY_PROXY_ADDR");
-    address L2_DAI_GATEWAY_PROXY_ADDR = vm.envAddress("L2_DAI_GATEWAY_PROXY_ADDR");
-    // address L2_USDC_GATEWAY_PROXY_ADDR = vm.envAddress("L2_USDC_GATEWAY_PROXY_ADDR");
-
     uint256 RATE_LIMITER_PERIOD_LENGTH = vm.envUint("RATE_LIMITER_PERIOD_LENGTH");
     uint104 ETH_TOTAL_LIMIT = uint104(vm.envUint("ETH_TOTAL_LIMIT"));
 
@@ -40,28 +35,12 @@ contract DeployL2RateLimiter is Script {
         );
 
         logAddress("L2_ETH_RATE_LIMITER_ADDR", address(limiter));
-
-        ScrollMessengerBase(payable(L2_SCROLL_MESSENGER_PROXY_ADDR)).updateRateLimiter(address(limiter));
     }
 
     function deployTokenRateLimiter() internal {
         TokenRateLimiter limiter = new TokenRateLimiter(RATE_LIMITER_PERIOD_LENGTH);
 
         logAddress("L2_TOKEN_RATE_LIMITER_ADDR", address(limiter));
-
-        bytes32 TOKEN_SPENDER_ROLE = limiter.TOKEN_SPENDER_ROLE();
-
-        limiter.grantRole(TOKEN_SPENDER_ROLE, L2_CUSTOM_ERC20_GATEWAY_PROXY_ADDR);
-        limiter.grantRole(TOKEN_SPENDER_ROLE, L2_STANDARD_ERC20_GATEWAY_PROXY_ADDR);
-        limiter.grantRole(TOKEN_SPENDER_ROLE, L2_DAI_GATEWAY_PROXY_ADDR);
-
-        ScrollGatewayBase(payable(L2_CUSTOM_ERC20_GATEWAY_PROXY_ADDR)).updateRateLimiter(address(limiter));
-        ScrollGatewayBase(payable(L2_STANDARD_ERC20_GATEWAY_PROXY_ADDR)).updateRateLimiter(address(limiter));
-        ScrollGatewayBase(payable(L2_DAI_GATEWAY_PROXY_ADDR)).updateRateLimiter(address(limiter));
-
-        // @note comments out for now
-        // limiter.grantRole(TOKEN_SPENDER_ROLE, L2_USDC_GATEWAY_PROXY_ADDR);
-        // ScrollGatewayBase(payable(L2_USDC_GATEWAY_PROXY_ADDR)).updateRateLimiter(address(limiter));
     }
 
     function logAddress(string memory name, address addr) internal view {
