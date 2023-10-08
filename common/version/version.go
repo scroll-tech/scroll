badde3cba559d35c0bff434c5b916e42ec91e0a1
colin <102356659+colinlyguo@users.noreply.github.com>
2023-10-08 19:39:17 +0800

    fix(rollup-relayer): better handle commit batch revert (#981)
    Co-authored-by: colinlyguo <colinlyguo@users.noreply.github.com>
    Co-authored-by: HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>

--- common/version/version.go
+++ common/version/version.go
@@ -5,7 +5,7 @@ import (
 	"runtime/debug"
 )
 
-var tag = "v4.3.30"
+var tag = "v4.3.31"
 
 var commit = func() string {
 	if info, ok := debug.ReadBuildInfo(); ok {
--- rollup/internal/orm/batch.go
+++ rollup/internal/orm/batch.go
@@ -192,22 +192,22 @@ func (o *Batch) GetRollupStatusByHashList(ctx context.Context, hashes []string) 
 	return statuses, nil
 }
 
-// GetPendingBatches retrieves pending batches up to the specified limit.
+// GetFailedAndPendingBatches retrieves batches with failed or pending status up to the specified limit.
 // The returned batches are sorted in ascending order by their index.
-func (o *Batch) GetPendingBatches(ctx context.Context, limit int) ([]*Batch, error) {
+func (o *Batch) GetFailedAndPendingBatches(ctx context.Context, limit int) ([]*Batch, error) {
 	if limit <= 0 {
 		return nil, errors.New("limit must be greater than zero")
 	}
 
 	db := o.db.WithContext(ctx)
 	db = db.Model(&Batch{})
-	db = db.Where("rollup_status = ?", types.RollupPending)
+	db = db.Where("rollup_status = ? OR rollup_status = ?", types.RollupCommitFailed, types.RollupPending)
 	db = db.Order("index ASC")
 	db = db.Limit(limit)
 
 	var batches []*Batch
 	if err := db.Find(&batches).Error; err != nil {
-		return nil, fmt.Errorf("Batch.GetPendingBatches error: %w", err)
+		return nil, fmt.Errorf("Batch.GetFailedAndPendingBatches error: %w", err)
 	}
 	return batches, nil
 }
