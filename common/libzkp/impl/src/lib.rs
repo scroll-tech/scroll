535c3a61161888401d13cbdec4b0c3dc17a7fba3
kunxian xia <xiakunxian130@gmail.com>
2023-10-14 00:37:40 +0800

    use jemalloc for libzkp

--- common/libzkp/impl/src/lib.rs
+++ common/libzkp/impl/src/lib.rs
@@ -1,5 +1,12 @@
 #![feature(once_cell)]
 
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static ALLOCATOR: Jemalloc = Jemalloc;
+
 mod batch;
 mod chunk;
 mod types;
