dc9086251bf392cc4424b8512d6854e0c3f04ced
Vladimir Trifonov <trifonov.vp@gmail.com>
2023-11-13 16:53:51 +0200

    test: fix database test

--- database/migrate/migrate_test.go
+++ database/migrate/migrate_test.go
@@ -63,7 +63,7 @@ func testResetDB(t *testing.T) {
 	cur, err := Current(pgDB.DB)
 	assert.NoError(t, err)
 	// total number of tables.
-	assert.Equal(t, 14, int(cur))
+	assert.Equal(t, 16, int(cur))
 }
 
 func testMigrate(t *testing.T) {
