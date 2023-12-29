5ba06456f9b5a01875f00cddfbc11a566021d4a1
colinlyguo <colinlyguo@scroll.io>
2023-12-29 15:36:54 +0800

    change table names

--- bridge-history-api/internal/orm/migrate/migrations/00002_batch_event_v2.sql
+++ bridge-history-api/internal/orm/migrate/migrations/00002_batch_event_v2.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE batch_event
+CREATE TABLE batch_event_v2
 (
     id                  BIGSERIAL     PRIMARY KEY,
     l1_block_number     BIGINT        NOT NULL,
@@ -15,14 +15,14 @@ CREATE TABLE batch_event
     deleted_at          TIMESTAMP(0)  DEFAULT NULL
 );
 
-CREATE INDEX IF NOT EXISTS idx_be_l1_block_number ON batch_event (l1_block_number);
-CREATE INDEX IF NOT EXISTS idx_be_batch_index ON batch_event (batch_index);
-CREATE INDEX IF NOT EXISTS idx_be_batch_index_batch_hash ON batch_event (batch_index, batch_hash);
-CREATE INDEX IF NOT EXISTS idx_be_end_block_number_update_status_batch_status_batch_index ON batch_event (end_block_number, update_status, batch_status, batch_index);
+CREATE INDEX IF NOT EXISTS idx_be_l1_block_number ON batch_event_v2 (l1_block_number);
+CREATE INDEX IF NOT EXISTS idx_be_batch_index ON batch_event_v2 (batch_index);
+CREATE INDEX IF NOT EXISTS idx_be_batch_index_batch_hash ON batch_event_v2 (batch_index, batch_hash);
+CREATE INDEX IF NOT EXISTS idx_be_end_block_number_update_status_batch_status_batch_index ON batch_event_v2 (end_block_number, update_status, batch_status, batch_index);
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS batch_event;
+DROP TABLE IF EXISTS batch_event_v2;
 -- +goose StatementEnd
