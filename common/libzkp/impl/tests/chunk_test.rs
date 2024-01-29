5d901bd8335a755cda0b4501f0ecd2ffdefe367f
georgehao <haohongfan@gmail.com>
2024-01-29 16:19:20 +0800

    feat: update

--- common/libzkp/impl/tests/chunk_test.rs
+++ common/libzkp/impl/tests/chunk_test.rs
@@ -10,15 +10,18 @@ fn chunk_test() {
     let trace_path = "/assets/traces/1_transfer.json".to_string();
     let chunk_trace = get_block_trace_from_file(Path::new(&trace_path));
     let json_str = serde_json::to_string(&chunk_trace).expect("Serialization failed");
+    println!("String: {}", json_str);
+
     let c_string = CString::new(json_str).expect("CString conversion failed");
     let c_str_ptr = c_string.as_ptr();
 
     unsafe {
         chunk::init_chunk_prover(params.as_ptr(), assets.as_ptr());
+        let mut count = 1;
         loop {
+            count += 1;
+            println!("count: {}", count);
             chunk::gen_chunk_proof(c_str_ptr);
-            let duration = Duration::from_millis(100);
-            sleep(duration);
         }
     }
 }
