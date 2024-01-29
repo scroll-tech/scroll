c93bf627373c125059999f24f117767575df5d4e
georgehao <haohongfan@gmail.com>
2024-01-29 17:19:42 +0800

    feat: update

--- common/libzkp/impl/tests/chunk_test.rs
+++ common/libzkp/impl/tests/chunk_test.rs
@@ -4,18 +4,18 @@ use zkp::chunk;
 
 #[test]
 fn chunk_test() {
-    let params = CString::new("/assets/test_params").expect("test_params conversion failed");
-    let assets = CString::new("/assets/test_assets").expect("test_assets conversion failed");
-
-    let trace_path = "/assets/traces/1_transfer.json".to_string();
-    let chunk_trace = get_block_trace_from_file(Path::new(&trace_path));
-    let json_str = serde_json::to_string(&chunk_trace).expect("Serialization failed");
-    log::info!("json str {:?}", json_str);
-
-    let c_string = CString::new(json_str).expect("CString conversion failed");
-    let c_str_ptr = c_string.as_ptr();
-
     unsafe {
+        let params = CString::new("/assets/test_params").expect("test_params conversion failed");
+        let assets = CString::new("/assets/test_assets").expect("test_assets conversion failed");
+
+        let trace_path = "/assets/traces/1_transfer.json".to_string();
+        let chunk_trace = get_block_trace_from_file(Path::new(&trace_path));
+        let json_str = serde_json::to_string(&chunk_trace).expect("Serialization failed");
+        log::info!("json str {:?}", json_str);
+
+        let c_string = CString::new(json_str).expect("CString conversion failed");
+        let c_str_ptr = c_string.as_ptr();
+
         chunk::init_chunk_prover(params.as_ptr(), assets.as_ptr());
         let mut count = 1;
         loop {
