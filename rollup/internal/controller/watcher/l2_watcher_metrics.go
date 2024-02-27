e26b65c9d0c7f37086c57957dd7271949d9c3581
bzarka <111681909+bzarka@users.noreply.github.com>
2024-02-27 17:51:26 +0100

    Update l2_watcher_metrics.go
    code with some improvements

--- rollup/internal/controller/watcher/l2_watcher_metrics.go
+++ rollup/internal/controller/watcher/l2_watcher_metrics.go
@@ -1,59 +1,52 @@
 package watcher
 
 import (
-	"sync"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+// l2WatcherMetrics contient les métriques de surveillance pour le watcher de la couche 2 (L2)
 type l2WatcherMetrics struct {
-	fetchRunningMissingBlocksTotal    prometheus.Counter
-	fetchRunningMissingBlocksHeight   prometheus.Gauge
-	fetchContractEventTotal           prometheus.Counter
-	fetchContractEventHeight          prometheus.Gauge
-	rollupL2MsgsRelayedEventsTotal    prometheus.Counter
-	rollupL2BlocksFetchedGap          prometheus.Gauge
-	rollupL2BlockL1CommitCalldataSize prometheus.Gauge
+    fetchRunningMissingBlocksTotal    prometheus.Counter
+    fetchRunningMissingBlocksHeight   prometheus.Gauge
+    fetchContractEventTotal           prometheus.Counter
+    fetchContractEventHeight          prometheus.Gauge
+    rollupL2MsgsRelayedEventsTotal    prometheus.Counter
+    rollupL2BlocksFetchedGap          prometheus.Gauge
+    rollupL2BlockL1CommitCalldataSize prometheus.Gauge
 }
 
-var (
-	initL2WatcherMetricOnce sync.Once
-	l2WatcherMetric         *l2WatcherMetrics
-)
-
+// initL2WatcherMetrics initialise les métriques de surveillance pour le watcher de la couche 2 (L2)
 func initL2WatcherMetrics(reg prometheus.Registerer) *l2WatcherMetrics {
-	initL2WatcherMetricOnce.Do(func() {
-		l2WatcherMetric = &l2WatcherMetrics{
-			fetchRunningMissingBlocksTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
-				Name: "rollup_l2_watcher_fetch_running_missing_blocks_total",
-				Help: "The total number of l2 watcher fetch running missing blocks",
-			}),
-			fetchRunningMissingBlocksHeight: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-				Name: "rollup_l2_watcher_fetch_running_missing_blocks_height",
-				Help: "The total number of l2 watcher fetch running missing blocks height",
-			}),
-			fetchContractEventTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
-				Name: "rollup_l2_watcher_fetch_contract_events_total",
-				Help: "The total number of l2 watcher fetch contract events",
-			}),
-			fetchContractEventHeight: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-				Name: "rollup_l2_watcher_fetch_contract_height",
-				Help: "The total number of l2 watcher fetch contract height",
-			}),
-			rollupL2MsgsRelayedEventsTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
-				Name: "rollup_l2_watcher_msg_relayed_events_total",
-				Help: "The total number of l2 watcher msg relayed event",
-			}),
-			rollupL2BlocksFetchedGap: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-				Name: "rollup_l2_watcher_blocks_fetched_gap",
-				Help: "The gap of l2 fetch",
-			}),
-			rollupL2BlockL1CommitCalldataSize: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-				Name: "rollup_l2_block_l1_commit_calldata_size",
-				Help: "The l1 commitBatch calldata size of the l2 block",
-			}),
-		}
-	})
-	return l2WatcherMetric
+    metrics := &l2WatcherMetrics{
+        fetchRunningMissingBlocksTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+            Name: "rollup_l2_watcher_fetch_running_missing_blocks_total",
+            Help: "The total number of L2 watcher fetch running missing blocks",
+        }),
+        fetchRunningMissingBlocksHeight: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+            Name: "rollup_l2_watcher_fetch_running_missing_blocks_height",
+            Help: "The height of L2 watcher fetch running missing blocks",
+        }),
+        fetchContractEventTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+            Name: "rollup_l2_watcher_fetch_contract_events_total",
+            Help: "The total number of L2 watcher fetch contract events",
+        }),
+        fetchContractEventHeight: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+            Name: "rollup_l2_watcher_fetch_contract_height",
+            Help: "The height of L2 watcher fetch contract",
+        }),
+        rollupL2MsgsRelayedEventsTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+            Name: "rollup_l2_watcher_msg_relayed_events_total",
+            Help: "The total number of L2 watcher relayed events",
+        }),
+        rollupL2BlocksFetchedGap: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+            Name: "rollup_l2_watcher_blocks_fetched_gap",
+            Help: "The gap of L2 fetch",
+        }),
+        rollupL2BlockL1CommitCalldataSize: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+            Name: "rollup_l2_block_l1_commit_calldata_size",
+            Help: "The size of L1 commitBatch calldata in the L2 block",
+        }),
+    }
+    return metrics
 }
