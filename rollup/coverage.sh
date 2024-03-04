3fc61b03269373f20cc3b24eb87b060b998e68d8
Sourab <64914231+sourabupadhya123@users.noreply.github.com>
2024-03-04 16:05:27 +0530

    Update coverage.sh

--- rollup/coverage.sh
+++ rollup/coverage.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-#
+
 # Code coverage generation
 
 COVERAGE_DIR="${COVERAGE_DIR:-coverage}"
 PKG_LIST=$(go list ./... | grep -v /vendor/)
 
 # Create the coverage files directory
-mkdir -p "$COVERAGE_DIR";
+mkdir -p "$COVERAGE_DIR" || exit 1
 
 # Create a coverage file for each package
 for package in ${PKG_LIST}; do
-    go test -covermode=count -coverprofile "${COVERAGE_DIR}/${package##*/}.cov" "$package" ;
-done ;
+  go test -covermode=count -coverprofile="${COVERAGE_DIR}/${package##*/}.cov" "$package" || exit 1
+done
 
 # Merge the coverage profile files
-echo 'mode: count' > "${COVERAGE_DIR}"/coverage.cov ;
-tail -q -n +2 "${COVERAGE_DIR}"/*.cov >> "${COVERAGE_DIR}"/coverage.cov ;
+echo 'mode: count' > "${COVERAGE_DIR}"/coverage.cov
+tail -q -n +2 "${COVERAGE_DIR}"/*.cov >> "${COVERAGE_DIR}"/coverage.cov
 
 # Display the global code coverage
-go tool cover -func="${COVERAGE_DIR}"/coverage.cov ;
+go tool cover -func="${COVERAGE_DIR}"/coverage.cov
 
 # If needed, generate HTML report
 if [ "$1" == "html" ]; then
-    go tool cover -html="${COVERAGE_DIR}"/coverage.cov -o coverage.html ;
+  go tool cover -html="${COVERAGE_DIR}"/coverage.cov -o coverage.html
 fi
 
 # Remove the coverage files directory
-rm -rf "$COVERAGE_DIR";
+rm -rf "$COVERAGE_DIR"
