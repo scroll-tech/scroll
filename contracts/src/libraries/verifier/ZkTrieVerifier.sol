d8bac3214574895d816ffde63e901ab64c2f3801
Xi Lin <zimpha@gmail.com>
2024-03-06 09:57:54 +0800

    fix(contracts): OZ-N02 Inconsistent Integer Base in Inline Assembly (#1139)

--- contracts/src/libraries/verifier/ZkTrieVerifier.sol
+++ contracts/src/libraries/verifier/ZkTrieVerifier.sol
@@ -90,7 +90,7 @@ library ZkTrieVerifier {
                 // the first byte is the number of nodes + 1
                 let nodes := sub(byte(0, calldataload(ptr)), 1)
                 require(lt(nodes, 249), "InvalidNodeDepth")
-                ptr := add(ptr, 1)
+                ptr := add(ptr, 0x01)
 
                 // treat the leaf node with different logic
                 for {
@@ -102,7 +102,7 @@ library ZkTrieVerifier {
                     let nodeType := byte(0, calldataload(ptr))
                     // 6 <= nodeType && nodeType < 10
                     require(lt(sub(nodeType, 6), 4), "InvalidBranchNodeType")
-                    ptr := add(ptr, 1)
+                    ptr := add(ptr, 0x01)
 
                     // load left/right child hash
                     let childHashL := calldataload(ptr)
