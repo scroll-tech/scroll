492b213462a7b521cfa3397d2c56376bf45a5e2c
georgehao <haohongfan@gmail.com>
2024-03-15 17:01:28 +0800

    feat: fix lint

--- coordinator/internal/logic/provertask/prover_task.go
+++ coordinator/internal/logic/provertask/prover_task.go
@@ -15,6 +15,8 @@ import (
 
 // ErrCoordinatorInternalFailure coordinator internal db failure
 var ErrCoordinatorInternalFailure = fmt.Errorf("coordinator internal error")
+
+// ErrHardForkName client request with the wrong hard fork name
 var ErrHardForkName = fmt.Errorf("wrong hard fork name")
 
 // ProverTask the interface of a collector who send data to prover
