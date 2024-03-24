816c403c6d9998a8a6b81e1471b2d1837d34b576
Jeswin Simon <jeswinsimon@gmail.com>
2024-03-24 17:04:43 +0530

    #1042 Allow configure db ConnMaxLifetime & ConnMaxIdleTime

--- common/database/config.go
+++ common/database/config.go
@@ -8,4 +8,6 @@ type Config struct {
 
 	MaxOpenNum int `json:"maxOpenNum"`
 	MaxIdleNum int `json:"maxIdleNum"`
+	MaxLifetime int `json:"maxLifetime,omitempty"`
+	MaxIdleTime int `json:"maxIdleTime,omitempty"`
 }
