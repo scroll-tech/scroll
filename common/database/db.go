ece6b6f86414427d7a1e981e334f93558edeeb60
Jeswin Simon <jeswinsimon@gmail.com>
2024-03-24 16:40:43 +0530

    Fix time

--- common/database/db.go
+++ common/database/db.go
@@ -71,13 +71,13 @@ func InitDB(config *Config) (*gorm.DB, error) {
 	}
 
 	if (config.MaxLifetime != 0) {
-		sqlDB.SetConnMaxLifetime(time.second * config.MaxLifetime)
+		sqlDB.SetConnMaxLifetime(time.Second * config.MaxLifetime)
 	} else {
 		sqlDB.SetConnMaxLifetime(time.Minute * 10)
 	}
 
 	if (config.MaxIdleTime != 0) {
-		sqlDB.SetConnMaxIdleTime(time.second * config.MaxIdleTime)
+		sqlDB.SetConnMaxIdleTime(time.Second * config.MaxIdleTime)
 	} else {
 		sqlDB.SetConnMaxIdleTime(time.Minute * 5)
 	}
