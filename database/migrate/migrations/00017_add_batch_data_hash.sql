2b49f1a8980edc2a4b0f8eda945080082b3fb713
HAOYUatHZ <haoyu@protonmail.com>
2024-04-01 15:04:16 +0800

    update sql

---
+++ database/migrate/migrations/00017_add_batch_data_hash.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+
+ALTER TABLE batch
+ADD COLUMN data_hash VARCHAR NOT NULL,
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+ALTER TABLE IF EXISTS batch
+DROP COLUMN data_hash,
+
+-- +goose StatementEnd
