8ac53276279c18730ae752508707ea99c4c9b669
HAOYUatHZ <haoyu@protonmail.com>
2024-04-02 14:47:38 +0800

    update sql

---
+++ database/migrate/migrations/00017_add_blob_meta_data.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+-- +goose StatementBegin
+
+ALTER TABLE chunk
+ADD COLUMN crc_max VARCHAR NOT NULL,
+ADD COLUMN blob_len INTEGER NOT NULL DEFAULT 0;
+
+ALTER TABLE batch
+ADD COLUMN data_hash VARCHAR NOT NULL,
+ADD COLUMN blob_data_proof BYTEA DEFAULT NULL,
+ADD COLUMN blob_len INTEGER NOT NULL DEFAULT 0;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+ALTER TABLE IF EXISTS chunk
+DROP COLUMN crc_max,
+DROP COLUMN blob_len;
+
+ALTER TABLE IF EXISTS batch
+DROP COLUMN data_hash,
+DROP COLUMN blob_data_proof,
+DROP COLUMN blob_len;
+
+-- +goose StatementEnd
