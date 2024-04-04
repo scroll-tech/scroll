e53e15034142b245b1933de40c3080d0074bc8d8
HAOYUatHZ <haoyu@protonmail.com>
2024-04-04 16:30:36 +0800

    add ecc.sol foundry deployment scripts

---
+++ contracts/scripts/foundry/DeployEcc.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+
+import {Ecc} from "../../src/misc/ecc.sol";
+
+contract DeployEcc is Script {
+    address L2_ECC_ADDR = vm.envAddress("L2_ECC_ADDR");
+
+    function run() external {
+        uint256 L2_DEPLOYER_PRIVATE_KEY = vm.envUint("L2_DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(L2_DEPLOYER_PRIVATE_KEY);
+        Ecc ecc = new Ecc();
+        L2_ECC_ADDR = address(ecc);
+        vm.stopBroadcast();
+
+        logAddress("L2_ECC_ADDR", L2_ECC_ADDR);
+    }
+
+    function logAddress(string memory name, address addr) internal view {
+        console.log(string(abi.encodePacked(name, "=", vm.toString(address(addr)))));
+    }
+}
