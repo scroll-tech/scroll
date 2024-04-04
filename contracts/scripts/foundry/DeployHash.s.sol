5b6b170db1a44f0a94b2283f071cb4e4ac9c4527
HAOYUatHZ <haoyu@protonmail.com>
2024-04-04 16:24:40 +0800

    add hash.sol foundry deployment scripts

---
+++ contracts/scripts/foundry/DeployHash.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+
+import {Hash} from "../../src/misc/Hash.sol";
+
+contract DeployHash is Script {
+    address L2_HASH_ADDR = vm.envAddress("L2_HASH_ADDR");
+
+    function run() external {
+        uint256 L2_DEPLOYER_PRIVATE_KEY = vm.envUint("L2_DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(L2_DEPLOYER_PRIVATE_KEY);
+        Hash hash = new Hash();
+        L2_HASH_ADDR = address(hash);
+        vm.stopBroadcast();
+
+        logAddress("L2_HASH_ADDR", L2_HASH_ADDR);
+    }
+
+    function logAddress(string memory name, address addr) internal view {
+        console.log(string(abi.encodePacked(name, "=", vm.toString(address(addr)))));
+    }
+}
