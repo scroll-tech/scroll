77028b7d72e2083c9465bef6ece8afc51d023d30
liuyuecai <liuyuecai@360.cn>
2024-04-04 14:12:30 +0800

    reformat code

--- common/go.mod
+++ common/go.mod
@@ -10,7 +10,6 @@ require (
 	github.com/gin-contrib/pprof v1.4.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/jmoiron/sqlx v1.3.5
-	github.com/lib/pq v1.10.9
 	github.com/mattn/go-colorable v0.1.13
 	github.com/mattn/go-isatty v0.0.20
 	github.com/modern-go/reflect2 v1.0.2
--- common/testcontainers/testcontainers.go
+++ common/testcontainers/testcontainers.go
@@ -6,13 +6,13 @@ import (
 	"log"
 	"time"
 
-	"scroll-tech/common/database"
-
 	"github.com/scroll-tech/go-ethereum/ethclient"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
 	"gorm.io/gorm"
+
+	"scroll-tech/common/database"
 )
 
 // TestcontainerApps testcontainers struct
