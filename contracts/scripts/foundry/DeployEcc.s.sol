c1059a3f514b4ce88bb0dcc6703775303c8831e7
colinlyguo <colinlyguo@scroll.io>
2024-04-05 00:20:01 +0800

    fix

--- contracts/scripts/foundry/DeployEcc.s.sol
+++ contracts/scripts/foundry/DeployEcc.s.sol
@@ -7,13 +7,11 @@ import {console} from "forge-std/console.sol";
 import {Ecc} from "../../src/misc/ecc.sol";
 
 contract DeployEcc is Script {
-    address L2_ECC_ADDR = vm.envAddress("L2_ECC_ADDR");
-
     function run() external {
         uint256 L2_DEPLOYER_PRIVATE_KEY = vm.envUint("L2_DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(L2_DEPLOYER_PRIVATE_KEY);
         Ecc ecc = new Ecc();
-        L2_ECC_ADDR = address(ecc);
+        address L2_ECC_ADDR = address(ecc);
         vm.stopBroadcast();
 
         logAddress("L2_ECC_ADDR", L2_ECC_ADDR);
--- contracts/scripts/foundry/DeployHash.s.sol
+++ contracts/scripts/foundry/DeployHash.s.sol
@@ -7,13 +7,11 @@ import {console} from "forge-std/console.sol";
 import {Hash} from "../../src/misc/Hash.sol";
 
 contract DeployHash is Script {
-    address L2_HASH_ADDR = vm.envAddress("L2_HASH_ADDR");
-
     function run() external {
         uint256 L2_DEPLOYER_PRIVATE_KEY = vm.envUint("L2_DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(L2_DEPLOYER_PRIVATE_KEY);
         Hash hash = new Hash();
-        L2_HASH_ADDR = address(hash);
+        address L2_HASH_ADDR = address(hash);
         vm.stopBroadcast();
 
         logAddress("L2_HASH_ADDR", L2_HASH_ADDR);
