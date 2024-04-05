65481212d569ada49a3fde14b7f3f13a98a3e9f5
colinlyguo <colinlyguo@scroll.io>
2024-04-06 03:13:22 +0800

    update script

--- common/types/constructing-target-payload-tx/main.go
+++ common/types/constructing-target-payload-tx/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"math/big"
-	"time"
 
 	"github.com/scroll-tech/go-ethereum/accounts/abi/bind"
 	"github.com/scroll-tech/go-ethereum/core/types"
@@ -14,7 +13,7 @@ import (
 	"github.com/scroll-tech/go-ethereum/rlp"
 )
 
-const targetTxSize = 126976
+const targetTxSize = 126914
 
 func main() {
 	privateKeyHex := "0000000000000000000000000000000000000000000000000000000000000042"
@@ -23,40 +22,74 @@ func main() {
 		log.Fatalf("Invalid private key: %v", err)
 	}
 
-	fromAddress := crypto.PubkeyToAddress(privateKey.PublicKey)
-
-	client, err := ethclient.Dial("http://localhost:8545")
+	client, err := ethclient.Dial("http://localhost:9999")
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
-
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		log.Fatalf("Failed to retrieve account nonce: %v", err)
-	}
-
-	fmt.Println("nonce", nonce)
 
 	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(222222))
 	if err != nil {
 		log.Fatalf("Failed to create transactor with chain ID 222222: %v", err)
 	}
 
-	gasLimit := uint64(3000000)
-	gasPrice := big.NewInt(2000000000)
+	nonce, err := client.PendingNonceAt(context.Background(), auth.From)
+	if err != nil {
+		log.Fatalf("Failed to retrieve account nonce: %v", err)
+	}
+	prepareAndSendTransactions(client, auth, nonce, 2)
+}
+
+func prepareAndSendTransactions(client *ethclient.Client, auth *bind.TransactOpts, initialNonce uint64, totalTxNum uint64) error {
+	gasLimit := uint64(5000000)
+	gasPrice := big.NewInt(1000000000)
+
+	var signedTxs []*types.Transaction
+	payloadSum := 0
+
+	dataPayload := make([]byte, targetTxSize/4)
+	for i := range dataPayload {
+		dataPayload[i] = 0xff
+	}
+
+	for i := uint64(0); i < totalTxNum-1; i++ {
+		txData := &types.LegacyTx{
+			Nonce:    initialNonce + i,
+			GasPrice: gasPrice,
+			Gas:      gasLimit,
+			To:       &auth.From,
+			Data:     dataPayload,
+		}
+
+		signedTx, err := auth.Signer(auth.From, types.NewTx(txData))
+		if err != nil {
+			log.Fatalf("Failed to sign tx: %v", err)
+		}
+
+		rlpTxData, err := rlp.EncodeToBytes(signedTx)
+		if err != nil {
+			log.Fatalf("Failed to RLP encode the tx: %v", err)
+		}
+
+		payloadSum += len(rlpTxData)
+		signedTxs = append(signedTxs, signedTx)
+	}
+
+	fmt.Println("payload sum", payloadSum)
 
 	lowerBound := 0
 	upperBound := targetTxSize
-
 	for lowerBound <= upperBound {
 		mid := (lowerBound + upperBound) / 2
 		data := make([]byte, mid)
+		for i := range data {
+			data[i] = 0xff
+		}
 
 		txData := &types.LegacyTx{
-			Nonce:    nonce,
+			Nonce:    initialNonce + totalTxNum - 1,
 			GasPrice: gasPrice,
 			Gas:      gasLimit,
-			To:       &fromAddress,
+			To:       &auth.From,
 			Data:     data,
 		}
 
@@ -69,40 +102,25 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to RLP encode the tx: %v", err)
 		}
-		txSize := len(rlpTxData)
+		txSize := len(rlpTxData) + payloadSum
 
-		if txSize < targetTxSize {
+		if payloadSum+txSize < targetTxSize {
 			lowerBound = mid + 1
-		} else if txSize > targetTxSize {
+		} else if payloadSum+txSize > targetTxSize {
 			upperBound = mid - 1
 		} else {
-			fmt.Printf("Found correct payload size: %d bytes\n", mid)
-			fmt.Printf("RLP encoded transaction size: %d bytes\n", len(rlpTxData))
-			fmt.Printf("Transaction hash: %s\n", signedTx.Hash().Hex())
-
-			err = client.SendTransaction(context.Background(), signedTx)
-			if err != nil {
-				log.Fatalf("Failed to send transaction: %v", err)
-			}
-			fmt.Printf("Transaction sent! Hash: %s\n", signedTx.Hash().Hex())
-
-			fmt.Println("Polling for transaction receipt...")
-			for {
-				receipt, err := client.TransactionReceipt(context.Background(), signedTx.Hash())
-				if err == nil {
-					fmt.Printf("Transaction receipt received: status %v\n", receipt.Status)
-					return
-				}
-				if err.Error() != "not found" {
-					log.Fatalf("Failed to get transaction receipt: %v", err)
-					break
-				}
-				fmt.Println("Transaction receipt not found yet. Waiting for mining...")
-				time.Sleep(2 * time.Second)
-			}
-			return
+			fmt.Println("payloadSum+txSize", payloadSum+txSize)
+			signedTxs = append(signedTxs, signedTx)
+			break
 		}
 	}
 
-	fmt.Println("Could not find the exact payload size for 128 KiB RLP encoded transaction.")
+	for i := len(signedTxs) - 1; i >= 0; i-- {
+		if err := client.SendTransaction(context.Background(), signedTxs[i]); err != nil {
+			return fmt.Errorf("failed to send transaction: %v", err)
+		}
+		fmt.Printf("Transaction with nonce %d sent\n", signedTxs[i].Nonce())
+	}
+
+	return nil
 }
