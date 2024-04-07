be011c59eadd063ab6d7c7aa29a38852b0c893c6
liuyuecai <luky116@126.com>
2024-04-07 21:47:20 +0800

    go format import

--- tests/integration-test/integration_test.go
+++ tests/integration-test/integration_test.go
@@ -16,12 +16,11 @@ import (
 	"scroll-tech/common/types/encoding"
 	"scroll-tech/common/utils"
 	"scroll-tech/common/version"
+	capp "scroll-tech/coordinator/cmd/api/app"
 	"scroll-tech/database/migrate"
 	"scroll-tech/integration-test/orm"
 	rapp "scroll-tech/prover/cmd/app"
 	bcmd "scroll-tech/rollup/cmd"
-
-	capp "scroll-tech/coordinator/cmd/api/app"
 )
 
 var (
