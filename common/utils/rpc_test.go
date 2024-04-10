dd8c6a0c6954f984b7c52f388b0c64a1481bc61c
Hadisamadbin <hadisamadbin@gmail.com>
2024-04-10 12:01:03 +0330

    refactor: separate test cases for RPC endpoint tests

--- common/utils/rpc_test.go
+++ common/utils/rpc_test.go
@@ -1,78 +1,92 @@
 package utils
 
 import (
-	"compress/flate"
-	"context"
-	"testing"
+    "compress/flate"
+    "context"
+    "testing"
 
-	"github.com/scroll-tech/go-ethereum/rpc"
-	"github.com/stretchr/testify/assert"
+    "github.com/scroll-tech/go-ethereum/rpc"
+    "github.com/stretchr/testify/assert"
 )
 
 type testService struct{}
 
 type echoArgs struct {
-	S string
+    S string
 }
 
 type echoResult struct {
-	Name string
-	ID   int
-	Args *echoArgs
+    Name string
+    ID   int
+    Args *echoArgs
 }
 
 func (s *testService) NoArgsRets() {}
 
 func (s *testService) Echo(str string, i int, args *echoArgs) echoResult {
-	return echoResult{str, i, args}
+    return echoResult{str, i, args}
 }
 
 func TestStartHTTPEndpoint(t *testing.T) {
-	endpoint := "localhost:18080"
-	handler, _, err := StartHTTPEndpoint(endpoint, []rpc.API{
-		{
-			Public:    true,
-			Namespace: "test",
-			Service:   new(testService),
-		},
-	})
-	assert.NoError(t, err)
-	defer handler.Shutdown(context.Background())
+    endpoint := "localhost:18080"
+    handler, _, err := StartHTTPEndpoint(endpoint, []rpc.API{
+        {
+            Public:     true,
+            Namespace:  "test",
+            Service:    new(testService),
+        },
+    })
+    assert.NoError(t, err)
+    defer handler.Shutdown(context.Background())
 
-	client, err := rpc.Dial("http://" + endpoint)
-	assert.NoError(t, err)
+    t.Run("StartEndpoint", func(t *testing.T) {
+        assert.NotNil(t, handler)
+    })
 
-	assert.NoError(t, client.Call(nil, "test_noArgsRets"))
+    client, err := rpc.Dial("http://" + endpoint)
+    assert.NoError(t, err)
+    defer client.Close()
 
-	result := echoResult{}
-	assert.NoError(t, client.Call(&result, "test_echo", "test", 0, &echoArgs{S: "test"}))
-	assert.Equal(t, 0, result.ID)
-	assert.Equal(t, "test", result.Name)
+    t.Run("NoArgsRets", func(t *testing.T) {
+        assert.NoError(t, client.Call(nil, "test_noArgsRets"))
+    })
 
-	defer client.Close()
+    t.Run("Echo", func(t *testing.T) {
+        result := echoResult{}
+        assert.NoError(t, client.Call(&result, "test_echo", "test", 0, &echoArgs{S: "test"}))
+        assert.Equal(t, 0, result.ID)
+        assert.Equal(t, "test", result.Name)
+    })
 }
 
 func TestStartWSEndpoint(t *testing.T) {
-	endpoint := "localhost:18081"
-	handler, _, err := StartWSEndpoint(endpoint, []rpc.API{
-		{
-			Public:    true,
-			Namespace: "test",
-			Service:   new(testService),
-		},
-	}, flate.NoCompression)
-	assert.NoError(t, err)
-	defer handler.Shutdown(context.Background())
+    endpoint := "localhost:18081"
+    handler, _, err := StartWSEndpoint(endpoint, []rpc.API{
+        {
+            Public:     true,
+            Namespace:  "test",
+            Service:    new(testService),
+        },
+    }, flate.NoCompression)
+    assert.NoError(t, err)
+    defer handler.Shutdown(context.Background())
 
-	client, err := rpc.Dial("ws://" + endpoint)
-	assert.NoError(t, err)
+    t.Run("StartEndpoint", func(t *testing.T) {
+        assert.NotNil(t, handler)
+    })
 
-	assert.NoError(t, client.Call(nil, "test_noArgsRets"))
+    client, err := rpc.Dial("ws://" + endpoint)
+    assert.NoError(t, err)
+    defer client.Close()
 
-	result := echoResult{}
-	assert.NoError(t, client.Call(&result, "test_echo", "test", 0, &echoArgs{S: "test"}))
-	assert.Equal(t, 0, result.ID)
-	assert.Equal(t, "test", result.Name)
+    t.Run("NoArgsRets", func(t *testing.T) {
+        assert.NoError(t, client.Call(nil, "test_noArgsRets"))
+    })
 
-	defer client.Close()
+    t.Run("Echo", func(t *testing.T) {
+        result := echoResult{}
+        assert.NoError(t, client.Call(&result, "test_echo", "test", 0, &echoArgs{S: "test"}))
+        assert.Equal(t, 0, result.ID)
+        assert.Equal(t, "test", result.Name)
+    })
 }
