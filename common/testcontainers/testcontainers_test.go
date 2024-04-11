2498559af5ea7ab13070becaf2ce5b96fb3414d8
liuyuecai <liuyuecai1995@gmail.com>
2024-04-12 00:33:58 +0800

    optimise docker-compose

--- common/testcontainers/testcontainers_test.go
+++ common/testcontainers/testcontainers_test.go
@@ -8,6 +8,29 @@ import (
 	"gorm.io/gorm"
 )
 
+func TestNewTestcontainerApps2(t *testing.T) {
+	var (
+		err       error
+		endpoint  string
+		ethclient *ethclient.Client
+	)
+
+	testApps := NewTestcontainerApps()
+
+	assert.NoError(t, testApps.StartPoSL1Container())
+	endpoint, err = testApps.GetPoSL1EndPoint()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, endpoint)
+	ethclient, err = testApps.GetPoSL1Client()
+	assert.NoError(t, err)
+	assert.NotNil(t, ethclient)
+
+	testApps.Free()
+	endpoint, err = testApps.GetPoSL1EndPoint()
+	assert.EqualError(t, err, "PoS L1 container is not running")
+	assert.NotEmpty(t, endpoint)
+}
+
 // TestNewTestcontainerApps tests NewTestcontainerApps
 func TestNewTestcontainerApps(t *testing.T) {
 	var (
@@ -17,8 +40,9 @@ func TestNewTestcontainerApps(t *testing.T) {
 		ethclient    *ethclient.Client
 	)
 
-	// test start testcontainers
 	testApps := NewTestcontainerApps()
+
+	// test start testcontainers
 	assert.NoError(t, testApps.StartPostgresContainer())
 	endpoint, err = testApps.GetDBEndPoint()
 	assert.NoError(t, err)
@@ -43,6 +67,14 @@ func TestNewTestcontainerApps(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, ethclient)
 
+	assert.NoError(t, testApps.StartPoSL1Container())
+	endpoint, err = testApps.GetPoSL1EndPoint()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, endpoint)
+	ethclient, err = testApps.GetPoSL1Client()
+	assert.NoError(t, err)
+	assert.NotNil(t, ethclient)
+
 	// test free testcontainers
 	testApps.Free()
 	endpoint, err = testApps.GetDBEndPoint()
@@ -56,4 +88,8 @@ func TestNewTestcontainerApps(t *testing.T) {
 	endpoint, err = testApps.GetL2GethEndPoint()
 	assert.EqualError(t, err, "l2 geth is not running")
 	assert.Empty(t, endpoint)
+
+	endpoint, err = testApps.GetPoSL1EndPoint()
+	assert.EqualError(t, err, "PoS L1 container is not running")
+	assert.NotEmpty(t, endpoint)
 }
