90dcdd8a01a049929c28ac9167491adae26948e8
Andi <36215014+ChengenH@users.noreply.github.com>
2024-04-22 13:22:13 +0800

    chore: remove repetitive words

--- common/version/prover_version.go
+++ common/version/prover_version.go
@@ -9,7 +9,7 @@ import (
 
 // CheckScrollProverVersion check the "scroll-prover" version, if it's different from the local one, return false
 func CheckScrollProverVersion(proverVersion string) bool {
-	// note the the version is in fact in the format of "tag-commit-scroll_prover-halo2",
+	// note the version is in fact in the format of "tag-commit-scroll_prover-halo2",
 	// so split-by-'-' length should be 4
 	remote := strings.Split(proverVersion, "-")
 	if len(remote) != 4 {
