8be70f0c804a87ac93d22c3c79326d68b645044c
JayLiu <38887641+luky116@users.noreply.github.com>
2024-05-02 21:12:20 +0800

    fix start testcontainers fail bug (#1313)

--- common/testcontainers/testcontainers.go
+++ common/testcontainers/testcontainers.go
@@ -31,6 +31,11 @@ type TestcontainerApps struct {
 // NewTestcontainerApps returns new instance of TestcontainerApps struct
 func NewTestcontainerApps() *TestcontainerApps {
 	timestamp := time.Now().Nanosecond()
+	// In order to solve the problem of "creating reaper failed: failed to create container"
+	// refer to https://github.com/testcontainers/testcontainers-go/issues/2172
+	if err := os.Setenv("TESTCONTAINERS_RYUK_DISABLED", "true"); err != nil {
+		panic("set env failed: " + err.Error())
+	}
 	return &TestcontainerApps{
 		Timestamp: timestamp,
 	}
