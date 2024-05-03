2d14457c91a527525591180c8ed1c285b952de1d
Péter Garamvölgyi <peter@scroll.io>
2024-05-03 14:43:20 +0800

    make envs more configurable

--- contracts/docker/scripts/deploy.sh
+++ contracts/docker/scripts/deploy.sh
@@ -3,19 +3,27 @@
 export FOUNDRY_EVM_VERSION="cancun"
 export FOUNDRY_BYTECODE_HASH="none"
 
-if [ "$L1_RPC_ENDPOINT" = "" ]; then
+if [ "${L1_RPC_ENDPOINT}" = "" ]; then
     echo "L1_RPC_ENDPOINT is not set"
-    exit 1
-else
-    echo "using L1_RPC_ENDPOINT = $L1_RPC_ENDPOINT"
+    L1_RPC_ENDPOINT="http://host.docker.internal:8543"
 fi
 
 if [ "$L2_RPC_ENDPOINT" = "" ]; then
     echo "L2_RPC_ENDPOINT is not set"
-    exit 1
-else
-    echo "using L2_RPC_ENDPOINT = $L2_RPC_ENDPOINT"
+    L2_RPC_ENDPOINT="http://host.docker.internal:8545"
 fi
+
+if [ "${L1_RPC_ENDPOINT}" = "" ]; then
+    echo "L1_RPC_ENDPOINT is not set"
+    L1_RPC_ENDPOINT="http://host.docker.internal:8543"
+fi
+
+if [ "${BATCH_SIZE}" = "" ]; then
+    BATCH_SIZE="100"
+fi
+
+echo "using L1_RPC_ENDPOINT = $L1_RPC_ENDPOINT"
+echo "using L2_RPC_ENDPOINT = $L2_RPC_ENDPOINT"
 
 # simulate L1
 echo ""
@@ -30,9 +38,9 @@ forge script scripts/foundry/DeployScroll.s.sol:DeployScroll --rpc-url "$L2_RPC_
 # deploy L1
 echo ""
 echo "deploying on L1"
-forge script scripts/foundry/DeployScroll.s.sol:DeployScroll --rpc-url "$L1_RPC_ENDPOINT" --sig "run(string,string)" "L1" "verify-config" --broadcast || exit 1
+forge script scripts/foundry/DeployScroll.s.sol:DeployScroll --rpc-url "$L1_RPC_ENDPOINT" --batch-size "$BATCH_SIZE" --sig "run(string,string)" "L1" "verify-config" --broadcast || exit 1
 
 # deploy L2
 echo ""
 echo "deploying on L2"
-forge script scripts/foundry/DeployScroll.s.sol:DeployScroll --rpc-url "$L2_RPC_ENDPOINT" --sig "run(string,string)" "L2" "verify-config" --broadcast --legacy || exit 1
+forge script scripts/foundry/DeployScroll.s.sol:DeployScroll --rpc-url "$L2_RPC_ENDPOINT"  --batch-size "$BATCH_SIZE" --sig "run(string,string)" "L2" "verify-config" --broadcast --legacy || exit 1
