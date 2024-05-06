9c69970d240ed94bd8bd61eba970afcc68a218e9
georgehao <haohongfan@gmail.com>
2024-05-06 18:21:01 +0800

    feat: update

--- bridge-history-api/internal/controller/fetcher/l2_fetcher.go
+++ bridge-history-api/internal/controller/fetcher/l2_fetcher.go
@@ -146,7 +146,7 @@ func (c *L2MessageFetcher) fetchAndSaveEvents(confirmation uint64) {
 			return
 		}
 
-		if updateErr := c.eventUpdateLogic.UpdateL1BridgeBatchDepositEvent(c.ctx, l2FetcherResult.BridgeBatchDepositMessage); updateErr != nil {
+		if updateErr := c.eventUpdateLogic.UpdateL2BridgeBatchDepositEvent(c.ctx, l2FetcherResult.BridgeBatchDepositMessage); updateErr != nil {
 			log.Error("failed to update L1 batch index and status", "from", from, "to", to, "err", updateErr)
 			return
 		}
