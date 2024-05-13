5497777c88ba61d12e83e4181268e135d8ad28f9
Péter Garamvölgyi <peter@scroll.io>
2024-05-13 16:18:15 +0100

    update dockerfile

--- contracts/docker/scripts/gen-configs.sh
+++ contracts/docker/scripts/gen-configs.sh
@@ -9,3 +9,8 @@ forge script scripts/foundry/DeployScroll.s.sol:DeployScroll --sig "run(string,s
 echo ""
 echo "generating genesis.json"
 forge script scripts/foundry/DeployScroll.s.sol:GenerateGenesis || exit 1
+
+# generate config files
+echo ""
+echo "generating rollup-config.json and bridge-history-config.json"
+forge script scripts/foundry/DeployScroll.s.sol:GenerateRollupConfig || exit 1
