2b3871f5cf193616bbdf4ac6c6423431f707c84d
colinlyguo <colinlyguo@scroll.io>
2024-05-21 17:49:14 +0800

    add naming

--- contracts/scripts/foundry/DeployTestCurieOpcodesContract.s.sol
+++ contracts/scripts/foundry/DeployTestCurieOpcodesContract.s.sol
@@ -6,14 +6,14 @@ import {console} from "forge-std/console.sol";
 
 import {TestCurieOpcodes} from "../../src/misc/TestCurieOpcodes.sol";
 
-contract DeployEcc is Script {
+contract DeployTestCurieOpcodesContract is Script {
     address L2_TEST_CURIE_OPCODES_ADDR = vm.envAddress("L2_TEST_CURIE_OPCODES_ADDR");
 
     function run() external {
         uint256 L2_DEPLOYER_PRIVATE_KEY = vm.envUint("L2_DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(L2_DEPLOYER_PRIVATE_KEY);
-        Ecc ecc = new Ecc();
-        L2_TEST_CURIE_OPCODES_ADDR = address(ecc);
+        TestCurieOpcodes src = new TestCurieOpcodes();
+        L2_TEST_CURIE_OPCODES_ADDR = address(src);
         vm.stopBroadcast();
 
         logAddress("L2_TEST_CURIE_OPCODES_ADDR", L2_TEST_CURIE_OPCODES_ADDR);
