56cb9d46648cdb725072ebd17223aa26b272b9fe
Zhuo Zhang <mycinbrin@gmail.com>
2024-05-28 13:43:30 +0800

    improve common/libzkp/e2e-test.sh

--- common/libzkp/e2e-test.sh
+++ common/libzkp/e2e-test.sh
@@ -1,4 +1,6 @@
 set -xeu
+set -o pipefail
+
 export CHAIN_ID=534352
 export RUST_BACKTRACE=full
 export RUST_LOG=debug
@@ -6,19 +8,32 @@ export RUST_MIN_STACK=100000000
 export PROVER_OUTPUT_DIR=test_zkp_test
 #export LD_LIBRARY_PATH=/:/usr/local/cuda/lib64
 
-REPO="../.."
+mkdir -p $PROVER_OUTPUT_DIR
+
+REPO=$(realpath ../..)
 
 function build_test_bins() {
+    cd impl
+    cargo build --release
+    ln -f -s $(realpath target/release/libzkp.so) $REPO/prover/core/lib
+    ln -f -s $(realpath target/release/libzkp.so) $REPO/coordinator/internal/logic/verifier/lib
+    cd ..
+}
+
+function build_test_bins_old() {
     cd $REPO
     cd prover
     make libzkp
     go test -tags="gpu ffi" -timeout 0 -c core/prover_test.go
     cd ..
     cd coordinator
+    make libzkp
     go test -tags="gpu ffi" -timeout 0 -c ./internal/logic/verifier
     cd ..
     cd common/libzkp
 }
 
-$REPO/prover/core.test -test.v
-$REPO/coordinator/verifier.test -test.v
+build_test_bins
+rm -rf prover.log verifier.log
+$REPO/prover/core.test -test.v 2>&1 | tee prover.log
+$REPO/coordinator/verifier.test -test.v 2>&1 | tee verifier.log
