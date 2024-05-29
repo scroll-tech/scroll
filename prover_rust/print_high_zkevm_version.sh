282d6e996b822cb13858b64bc8805cb58f19479d
Mengran Lan <lanmengran@qq.com>
2024-05-29 18:23:44 +0800

    update makefile

---
+++ prover_rust/print_high_zkevm_version.sh
@@ -0,0 +1,9 @@
+
+
+higher_zkevm_item=`grep "zkevm-circuits.git" ./Cargo.lock | sort | uniq | awk -F "[#=]" '{print $3" "$4}' | sort -k 1 | tail -n 1`
+
+higher_version=`echo $higher_zkevm_item | awk '{print $1}'`
+
+higher_commit=`echo $higher_zkevm_item | cut -d ' ' -f2 | cut -c-7`
+
+echo "$higher_version $higher_commit"
