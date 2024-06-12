420823234c223682a7d939c567004cd5474a5b05
Sigmund <mingxing.tc@gmail.com>
2024-06-13 02:30:55 +0800

    fix logger test, add asserts

--- common/database/logger_test.go
+++ common/database/logger_test.go
@@ -1,24 +1,30 @@
 package database
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"io"
 	"os"
+	"regexp"
 	"testing"
 	"time"
 
 	"github.com/mattn/go-colorable"
 	"github.com/mattn/go-isatty"
 	"github.com/scroll-tech/go-ethereum/log"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGormLogger(t *testing.T) {
-	output := io.Writer(os.Stderr)
+	var buf bytes.Buffer
+	output := io.MultiWriter(os.Stderr, &buf)
+
 	usecolor := (isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd())) && os.Getenv("TERM") != "dumb"
 	if usecolor {
 		output = colorable.NewColorableStderr()
 	}
+
 	ostream := log.StreamHandler(output, log.TerminalFormat(usecolor))
 	glogger := log.NewGlogHandler(ostream)
 	// Set log level
@@ -28,8 +34,20 @@ func TestGormLogger(t *testing.T) {
 	var gl gormLogger
 	gl.gethLogger = log.Root()
 
-	gl.Error(context.Background(), "test %s error:%v", "testError", errors.New("test error"))
-	gl.Warn(context.Background(), "test %s warn:%v", "testWarn", errors.New("test warn"))
-	gl.Info(context.Background(), "test %s warn:%v", "testInfo", errors.New("test info"))
+	gl.Error(context.Background(), "test error: %s, %v", "testError", errors.New("test error"))
+	gl.Warn(context.Background(), "test warn: %s, %v", "testWarn", errors.New("test warn"))
+	gl.Info(context.Background(), "test info: %s, %v", "testInfo", errors.New("test info"))
 	gl.Trace(context.Background(), time.Now(), func() (string, int64) { return "test trace", 1 }, nil)
+
+	logOutput := buf.String()
+
+	errorPattern := `ERROR\[\d{2}-\d{2}\|\d{2}:\d{2}:\d{2}\.\d{3}\] gorm\s+err message="test error: testError, test error"`
+	warnPattern := `WARN \[\d{2}-\d{2}\|\d{2}:\d{2}:\d{2}\.\d{3}\] gorm\s+warn message="test warn: testWarn, test warn"`
+	infoPattern := `INFO \[\d{2}-\d{2}\|\d{2}:\d{2}:\d{2}\.\d{3}\] gorm\s+info message="test info: testInfo, test info"`
+	tracePattern := `DEBUG\[\d{2}-\d{2}\|\d{2}:\d{2}:\d{2}\.\d{3}\] gorm\s+line=.* cost=.* sql="test trace" rowsAffected=1 err=nil`
+
+	assert.Regexp(t, regexp.MustCompile(errorPattern), logOutput, "Error log does not match expected pattern")
+	assert.Regexp(t, regexp.MustCompile(warnPattern), logOutput, "Warn log does not match expected pattern")
+	assert.Regexp(t, regexp.MustCompile(infoPattern), logOutput, "Info log does not match expected pattern")
+	assert.Regexp(t, regexp.MustCompile(tracePattern), logOutput, "Trace log does not match expected pattern")
 }
