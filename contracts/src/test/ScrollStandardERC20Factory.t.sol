94d9fc3a05e17a1475f6068dc72b4a8c8a11326e
ayenesimo1i <2958807+ayenesimo1i@users.noreply.github.com>
2024-06-19 23:11:49 +0300

    add ScrollStandardERC20Factory coverage

--- contracts/src/test/ScrollStandardERC20Factory.t.sol
+++ contracts/src/test/ScrollStandardERC20Factory.t.sol
@@ -7,6 +7,7 @@ import {WETH} from "solmate/tokens/WETH.sol";
 
 import {ScrollStandardERC20} from "../libraries/token/ScrollStandardERC20.sol";
 import {ScrollStandardERC20Factory} from "../libraries/token/ScrollStandardERC20Factory.sol";
+import {IScrollStandardERC20Factory} from "../libraries/token/IScrollStandardERC20Factory.sol";
 
 contract ScrollStandardERC20FactoryTest is DSTestPlus {
     ScrollStandardERC20 private impl;
@@ -15,6 +16,12 @@ contract ScrollStandardERC20FactoryTest is DSTestPlus {
     function setUp() public {
         impl = new ScrollStandardERC20();
         factory = new ScrollStandardERC20Factory(address(impl));
+    }
+
+    function testDeployFactoryRevert() external {
+        // zero address as ERC20 implementation address, revert
+        hevm.expectRevert("zero implementation address");
+        new ScrollStandardERC20Factory(address(0));
     }
 
     function testDeployL2Token(address _gateway, address _l1Token) external {
@@ -26,6 +33,8 @@ contract ScrollStandardERC20FactoryTest is DSTestPlus {
 
         // call by owner, should succeed
         address computed = factory.computeL2TokenAddress(_gateway, _l1Token);
+        hevm.expectEmit(true, true, false, false);
+        emit IScrollStandardERC20Factory.DeployToken(_l1Token, computed);
         address deployed = factory.deployL2Token(_gateway, _l1Token);
         assertEq(computed, deployed);
     }
