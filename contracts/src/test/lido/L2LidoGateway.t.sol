35477aac4caad70acbb2f2e8ef8da7b03e0bc87e
knotts <knotts1416@protonmail.com>
2024-06-21 22:05:49 +0100

    fix spelling: finialize => finalize

--- contracts/src/test/lido/L2LidoGateway.t.sol
+++ contracts/src/test/lido/L2LidoGateway.t.sol
@@ -401,7 +401,7 @@ contract L2LidoGatewayTest is L2GatewayTestBase {
         hevm.expectRevert(ErrorNonZeroMsgValue.selector);
         mockMessenger.callTarget{value: 1}(address(mockGateway), message);
 
-        // succeed when finialize
+        // succeed when finalize
         bytes memory xDomainCalldata = abi.encodeCall(
             l2Messenger.relayMessage,
             (address(counterpartGateway), address(gateway), 0, 0, message)
