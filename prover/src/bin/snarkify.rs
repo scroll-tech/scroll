f33d97cf79bd716ba724a74eabc91a88fac8f162
Boyu Sun <boyu@snarkify.io>
2024-06-26 11:54:49 +0800

    Snarkify SDK integration

---
+++ prover/src/bin/snarkify.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+use snarkify_sdk::prover::ProofHandler;
+
+struct MyProofHandler;
+
+#[derive(Deserialize)]
+struct MyInput {
+    public_input: String,
+}
+
+#[derive(Serialize)]
+struct MyOutput {
+    proof: String,
+}
+
+#[async_trait]
+impl ProofHandler for MyProofHandler {
+    type Input = MyInput;
+    type Output = MyOutput;
+    type Error = ();
+
+    async fn prove(data: Self::Input) -> Result<Self::Output, Self::Error> {
+        Ok(MyOutput {
+            proof: data.public_input.chars().rev().collect(),
+        })
+    }
+}
+
+fn main() -> Result<(), io::Error> {
+    snarkify_sdk::run::<MyProofHandler>()
+}
