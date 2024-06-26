566c4942b7283606636142bcb2e50fdb82dc425e
Boyu Sun <boyu@snarkify.io>
2024-06-26 20:55:03 +0800

    Running version

--- prover/src/bin/snarkify.rs
+++ prover/src/bin/snarkify.rs
@@ -1,32 +1,43 @@
 use std::io;
+use std::rc::Rc;
 
 use async_trait::async_trait;
-use serde::{Deserialize, Serialize};
 use snarkify_sdk::prover::ProofHandler;
-use prover::prover_core;
+use prover_runner::{
+    prover_core::Prover,
+    types::{Task, ProofDetail},
+    config::{Config, AssetsDirEnvConfig},
+    version,
+};
 
 struct MyProofHandler;
 
-#[derive(Deserialize)]
-struct MyInput {
-    public_input: String,
-}
-
-#[derive(Serialize)]
-struct MyOutput {
-    proof: String,
-}
 
 #[async_trait]
 impl ProofHandler for MyProofHandler {
-    type Input = MyInput;
-    type Output = MyOutput;
-    type Error = ();
+    type Input = Task;
+    type Output = ProofDetail;
+    type Error = String;
 
     async fn prove(data: Self::Input) -> Result<Self::Output, Self::Error> {
-        Ok(MyOutput {
-            proof: data.public_input.chars().rev().collect(),
-        })
+        let config: Config = Config::from_file("config.json".to_string()).map_err(|e| e.to_string())?;
+
+        if let Err(e) = AssetsDirEnvConfig::init() {
+            log::error!("AssetsDirEnvConfig init failed: {:#}", e);
+            std::process::exit(-2);
+        }
+
+        let prover = Prover::new(&config).map_err(|e| e.to_string())?;
+
+        log::info!(
+            "prover start successfully. name: {}, type: {:?}, publickey: {}, version: {}",
+            config.prover_name,
+            config.proof_type,
+            prover.get_public_key(),
+            version::get_version(),
+        );
+
+        prover.prove_task(&data).map_err(|e| e.to_string())
     }
 }
 
--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -1,12 +1,12 @@
-mod config;
+pub mod config;
 mod coordinator_client;
 mod geth_client;
 mod key_signer;
 mod prover;
 mod task_cache;
 mod task_processor;
-mod types;
+pub mod types;
 mod utils;
-mod version;
+pub mod version;
 mod zk_circuits_handler;
 pub mod prover_core;
