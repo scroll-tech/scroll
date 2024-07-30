a22aa8dc24f46ff79aadaf6cac2a78e787c05fbe
colin <102356659+colinlyguo@users.noreply.github.com>
2024-07-30 16:11:33 +0800

    chore: cleanup libzkp interfaces used by coordinator and update da-codec dependency (#1469)

--- common/libzkp/impl/src/batch.rs
+++ common/libzkp/impl/src/batch.rs
@@ -1,46 +1,14 @@
-use crate::{
-    types::{CheckChunkProofsResponse, ProofResult},
-    utils::{
-        c_char_to_str, c_char_to_vec, file_exists, panic_catch, string_to_c_char, vec_to_c_char,
-        OUTPUT_DIR,
-    },
-};
+use crate::utils::{c_char_to_str, c_char_to_vec, panic_catch};
 use libc::c_char;
 use prover_v3::BatchProof as BatchProofLoVersion;
 use prover_v4::{
-    aggregator::{Prover, Verifier as VerifierHiVersion},
-    check_chunk_hashes,
-    consts::BATCH_VK_FILENAME,
-    utils::{chunk_trace_to_witness_block, init_env_and_log},
-    BatchHeader, BatchProof as BatchProofHiVersion, BatchProvingTask, BlockTrace, BundleProof,
-    BundleProvingTask, ChunkInfo, ChunkProof, MAX_AGG_SNARKS,
+    aggregator::Verifier as VerifierHiVersion, utils::init_env_and_log,
+    BatchProof as BatchProofHiVersion, BundleProof,
 };
 use snark_verifier_sdk::verify_evm_calldata;
-use std::{cell::OnceCell, env, ptr::null};
+use std::{cell::OnceCell, env};
 
-static mut PROVER: OnceCell<Prover> = OnceCell::new();
 static mut VERIFIER: OnceCell<VerifierHiVersion> = OnceCell::new();
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn init_batch_prover(params_dir: *const c_char, assets_dir: *const c_char) {
-    init_env_and_log("ffi_batch_prove");
-
-    let params_dir = c_char_to_str(params_dir);
-    let assets_dir = c_char_to_str(assets_dir);
-
-    // TODO: add a settings in scroll-prover.
-    env::set_var("SCROLL_PROVER_ASSETS_DIR", assets_dir);
-
-    // VK file must exist, it is optional and logged as a warning in prover.
-    if !file_exists(assets_dir, &BATCH_VK_FILENAME) {
-        panic!("{} must exist in folder {}", *BATCH_VK_FILENAME, assets_dir);
-    }
-
-    let prover = Prover::from_dirs(params_dir, assets_dir);
-
-    PROVER.set(prover).unwrap();
-}
 
 /// # Safety
 #[no_mangle]
@@ -55,119 +23,6 @@ pub unsafe extern "C" fn init_batch_verifier(params_dir: *const c_char, assets_d
     let verifier_hi = VerifierHiVersion::from_dirs(params_dir, assets_dir);
 
     VERIFIER.set(verifier_hi).unwrap();
-}
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn get_batch_vk() -> *const c_char {
-    let vk_result = panic_catch(|| PROVER.get_mut().unwrap().get_batch_vk());
-
-    vk_result
-        .ok()
-        .flatten()
-        .map_or(null(), |vk| string_to_c_char(base64::encode(vk)))
-}
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn get_bundle_vk() -> *const c_char {
-    let vk_result = panic_catch(|| PROVER.get_mut().unwrap().get_bundle_vk());
-
-    vk_result
-        .ok()
-        .flatten()
-        .map_or(null(), |vk| string_to_c_char(base64::encode(vk)))
-}
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn check_chunk_proofs(chunk_proofs: *const c_char) -> *const c_char {
-    let check_result: Result<bool, String> = panic_catch(|| {
-        let chunk_proofs = c_char_to_vec(chunk_proofs);
-        let chunk_proofs = serde_json::from_slice::<Vec<ChunkProof>>(&chunk_proofs)
-            .map_err(|e| format!("failed to deserialize chunk proofs: {e:?}"))?;
-
-        if chunk_proofs.is_empty() {
-            return Err("provided chunk proofs are empty.".to_string());
-        }
-
-        let prover_ref = PROVER.get().expect("failed to get reference to PROVER.");
-
-        let valid = prover_ref.check_protocol_of_chunks(&chunk_proofs);
-        Ok(valid)
-    })
-    .unwrap_or_else(|e| Err(format!("unwind error: {e:?}")));
-
-    let r = match check_result {
-        Ok(valid) => CheckChunkProofsResponse {
-            ok: valid,
-            error: None,
-        },
-        Err(err) => CheckChunkProofsResponse {
-            ok: false,
-            error: Some(err),
-        },
-    };
-
-    serde_json::to_vec(&r).map_or(null(), vec_to_c_char)
-}
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn gen_batch_proof(
-    chunk_hashes: *const c_char,
-    chunk_proofs: *const c_char,
-    batch_header: *const c_char,
-) -> *const c_char {
-    let proof_result: Result<Vec<u8>, String> = panic_catch(|| {
-        let chunk_hashes = c_char_to_vec(chunk_hashes);
-        let chunk_proofs = c_char_to_vec(chunk_proofs);
-        let batch_header = c_char_to_vec(batch_header);
-
-        let chunk_hashes = serde_json::from_slice::<Vec<ChunkInfo>>(&chunk_hashes)
-            .map_err(|e| format!("failed to deserialize chunk hashes: {e:?}"))?;
-        let chunk_proofs = serde_json::from_slice::<Vec<ChunkProof>>(&chunk_proofs)
-            .map_err(|e| format!("failed to deserialize chunk proofs: {e:?}"))?;
-        let batch_header = serde_json::from_slice::<BatchHeader<MAX_AGG_SNARKS>>(&batch_header)
-            .map_err(|e| format!("failed to deserialize batch header: {e:?}"))?;
-
-        if chunk_hashes.len() != chunk_proofs.len() {
-            return Err(format!("chunk hashes and chunk proofs lengths mismatch: chunk_hashes.len() = {}, chunk_proofs.len() = {}",
-                chunk_hashes.len(), chunk_proofs.len()));
-        }
-
-        let chunk_hashes_proofs: Vec<(_,_)> = chunk_hashes
-            .into_iter()
-            .zip(chunk_proofs.clone())
-            .collect();
-        check_chunk_hashes("", &chunk_hashes_proofs).map_err(|e| format!("failed to check chunk info: {e:?}"))?;
-
-        let batch = BatchProvingTask {
-            chunk_proofs,
-            batch_header,
-        };
-        let proof = PROVER
-            .get_mut()
-            .expect("failed to get mutable reference to PROVER.")
-            .gen_batch_proof(batch, None, OUTPUT_DIR.as_deref())
-            .map_err(|e| format!("failed to generate proof: {e:?}"))?;
-
-        serde_json::to_vec(&proof).map_err(|e| format!("failed to serialize the proof: {e:?}"))
-    })
-    .unwrap_or_else(|e| Err(format!("unwind error: {e:?}")));
-
-    let r = match proof_result {
-        Ok(proof_bytes) => ProofResult {
-            message: Some(proof_bytes),
-            error: None,
-        },
-        Err(err) => ProofResult {
-            message: None,
-            error: Some(err),
-        },
-    };
-
-    serde_json::to_vec(&r).map_or(null(), vec_to_c_char)
 }
 
 /// # Safety
@@ -206,57 +61,9 @@ pub unsafe extern "C" fn verify_batch_proof(
 
 /// # Safety
 #[no_mangle]
-pub unsafe extern "C" fn gen_bundle_proof(batch_proofs: *const c_char) -> *const c_char {
-    let proof_result: Result<Vec<u8>, String> = panic_catch(|| {
-        let batch_proofs = c_char_to_vec(batch_proofs);
-        let batch_proofs = serde_json::from_slice::<Vec<BatchProofHiVersion>>(&batch_proofs)
-            .map_err(|e| format!("failed to deserialize batch proofs: {e:?}"))?;
-
-        let bundle = BundleProvingTask { batch_proofs };
-        let proof = PROVER
-            .get_mut()
-            .expect("failed to get mutable reference to PROVER.")
-            .gen_bundle_proof(bundle, None, OUTPUT_DIR.as_deref())
-            .map_err(|e| format!("failed to generate bundle proof: {e:?}"))?;
-
-        serde_json::to_vec(&proof)
-            .map_err(|e| format!("failed to serialize the bundle proof: {e:?}"))
-    })
-    .unwrap_or_else(|e| Err(format!("unwind error: {e:?}")));
-
-    let r = match proof_result {
-        Ok(proof_bytes) => ProofResult {
-            message: Some(proof_bytes),
-            error: None,
-        },
-        Err(err) => ProofResult {
-            message: None,
-            error: Some(err),
-        },
-    };
-
-    serde_json::to_vec(&r).map_or(null(), vec_to_c_char)
-}
-
-/// # Safety
-#[no_mangle]
 pub unsafe extern "C" fn verify_bundle_proof(proof: *const c_char) -> c_char {
     let proof = c_char_to_vec(proof);
     let proof = serde_json::from_slice::<BundleProof>(proof.as_slice()).unwrap();
     let verified = panic_catch(|| VERIFIER.get().unwrap().verify_bundle_proof(proof));
     verified.unwrap_or(false) as c_char
-}
-
-// This function is only used for debugging on Go side.
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn block_traces_to_chunk_info(block_traces: *const c_char) -> *const c_char {
-    let block_traces = c_char_to_vec(block_traces);
-    let block_traces = serde_json::from_slice::<Vec<BlockTrace>>(&block_traces).unwrap();
-
-    let witness_block = chunk_trace_to_witness_block(block_traces).unwrap();
-    let chunk_info = ChunkInfo::from_witness_block(&witness_block, false);
-
-    let chunk_info_bytes = serde_json::to_vec(&chunk_info).unwrap();
-    vec_to_c_char(chunk_info_bytes)
 }
--- common/libzkp/impl/src/chunk.rs
+++ common/libzkp/impl/src/chunk.rs
@@ -1,44 +1,14 @@
-use crate::{
-    types::ProofResult,
-    utils::{
-        c_char_to_str, c_char_to_vec, file_exists, panic_catch, string_to_c_char, vec_to_c_char,
-        OUTPUT_DIR,
-    },
-};
+use crate::utils::{c_char_to_str, c_char_to_vec, panic_catch};
 use libc::c_char;
 use prover_v3::{zkevm::Verifier as VerifierLoVersion, ChunkProof as ChunkProofLoVersion};
 use prover_v4::{
-    consts::CHUNK_VK_FILENAME,
-    utils::init_env_and_log,
-    zkevm::{Prover, Verifier as VerifierHiVersion},
-    BlockTrace, ChunkProof as ChunkProofHiVersion, ChunkProvingTask,
+    utils::init_env_and_log, zkevm::Verifier as VerifierHiVersion,
+    ChunkProof as ChunkProofHiVersion,
 };
-use std::{cell::OnceCell, env, ptr::null};
+use std::{cell::OnceCell, env};
 
-static mut PROVER: OnceCell<Prover> = OnceCell::new();
 static mut VERIFIER_LO_VERSION: OnceCell<VerifierLoVersion> = OnceCell::new();
 static mut VERIFIER_HI_VERSION: OnceCell<VerifierHiVersion> = OnceCell::new();
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn init_chunk_prover(params_dir: *const c_char, assets_dir: *const c_char) {
-    init_env_and_log("ffi_chunk_prove");
-
-    let params_dir = c_char_to_str(params_dir);
-    let assets_dir = c_char_to_str(assets_dir);
-
-    // TODO: add a settings in scroll-prover.
-    env::set_var("SCROLL_PROVER_ASSETS_DIR", assets_dir);
-
-    // VK file must exist, it is optional and logged as a warning in prover.
-    if !file_exists(assets_dir, &CHUNK_VK_FILENAME) {
-        panic!("{} must exist in folder {}", *CHUNK_VK_FILENAME, assets_dir);
-    }
-
-    let prover = Prover::from_dirs(params_dir, assets_dir);
-
-    PROVER.set(prover).unwrap();
-}
 
 /// # Safety
 #[no_mangle]
@@ -61,50 +31,6 @@ pub unsafe extern "C" fn init_chunk_verifier(
 
     VERIFIER_LO_VERSION.set(verifier_lo).unwrap();
     VERIFIER_HI_VERSION.set(verifier_hi).unwrap();
-}
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn get_chunk_vk() -> *const c_char {
-    let vk_result = panic_catch(|| PROVER.get_mut().unwrap().get_vk());
-
-    vk_result
-        .ok()
-        .flatten()
-        .map_or(null(), |vk| string_to_c_char(base64::encode(vk)))
-}
-
-/// # Safety
-#[no_mangle]
-pub unsafe extern "C" fn gen_chunk_proof(block_traces: *const c_char) -> *const c_char {
-    let proof_result: Result<Vec<u8>, String> = panic_catch(|| {
-        let block_traces = c_char_to_vec(block_traces);
-        let block_traces = serde_json::from_slice::<Vec<BlockTrace>>(&block_traces)
-            .map_err(|e| format!("failed to deserialize block traces: {e:?}"))?;
-        let chunk = ChunkProvingTask::from(block_traces);
-
-        let proof = PROVER
-            .get_mut()
-            .expect("failed to get mutable reference to PROVER.")
-            .gen_chunk_proof(chunk, None, None, OUTPUT_DIR.as_deref())
-            .map_err(|e| format!("failed to generate proof: {e:?}"))?;
-
-        serde_json::to_vec(&proof).map_err(|e| format!("failed to serialize the proof: {e:?}"))
-    })
-    .unwrap_or_else(|e| Err(format!("unwind error: {e:?}")));
-
-    let r = match proof_result {
-        Ok(proof_bytes) => ProofResult {
-            message: Some(proof_bytes),
-            error: None,
-        },
-        Err(err) => ProofResult {
-            message: None,
-            error: Some(err),
-        },
-    };
-
-    serde_json::to_vec(&r).map_or(null(), vec_to_c_char)
 }
 
 /// # Safety
