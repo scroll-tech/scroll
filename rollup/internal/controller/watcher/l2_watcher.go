29ff90f3bab82202001b675d5ae3ca9b1a7cb2aa
colinlyguo <colinlyguo@scroll.io>
2024-08-23 22:37:10 +0800

    remove if

--- rollup/internal/controller/watcher/l2_watcher.go
+++ rollup/internal/controller/watcher/l2_watcher.go
@@ -127,11 +127,9 @@ func (w *L2WatcherClient) getAndStoreBlocks(ctx context.Context, from, to uint64
 			return fmt.Errorf("failed to GetBlockByNumberOrHash: %v. number: %v", err, number)
 		}
 		if block.RowConsumption == nil {
-			if number == 6279234 {
-				block.RowConsumption = &gethTypes.RowConsumption{}
-			} else {
-				return fmt.Errorf("fetched block does not contain RowConsumption. number: %v", number)
-			}
+			block.RowConsumption = &gethTypes.RowConsumption{}
+		} else {
+			return fmt.Errorf("fetched block does not contain RowConsumption. number: %v", number)
 		}
 
 		log.Info("retrieved block", "height", block.Header().Number, "hash", block.Header().Hash().String())
