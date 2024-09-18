736d850be1b17990ac5f5c756bf44c440488983e
colin <102356659+colinlyguo@users.noreply.github.com>
2024-09-18 12:46:20 +0800

    feat(l2-watcher): add nil row consumption block metric (#1518)

--- common/version/version.go
+++ common/version/version.go
@@ -5,7 +5,7 @@ import (
 	"runtime/debug"
 )
 
-var tag = "v4.4.60"
+var tag = "v4.4.61"
 
 var commit = func() string {
 	if info, ok := debug.ReadBuildInfo(); ok {
--- rollup/internal/controller/watcher/l2_watcher_metrics.go
+++ rollup/internal/controller/watcher/l2_watcher_metrics.go
@@ -12,6 +12,7 @@ type l2WatcherMetrics struct {
 	fetchRunningMissingBlocksHeight   prometheus.Gauge
 	rollupL2BlocksFetchedGap          prometheus.Gauge
 	rollupL2BlockL1CommitCalldataSize prometheus.Gauge
+	fetchNilRowConsumptionBlockTotal  prometheus.Counter
 }
 
 var (
@@ -37,6 +38,10 @@ func initL2WatcherMetrics(reg prometheus.Registerer) *l2WatcherMetrics {
 			rollupL2BlockL1CommitCalldataSize: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
 				Name: "rollup_l2_block_l1_commit_calldata_size",
 				Help: "The l1 commitBatch calldata size of the l2 block",
+			}),
+			fetchNilRowConsumptionBlockTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+				Name: "rollup_l2_watcher_fetch_nil_row_consumption_block_total",
+				Help: "The total number of occurrences where a fetched block has nil RowConsumption",
 			}),
 		}
 	})
