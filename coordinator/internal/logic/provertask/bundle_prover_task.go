77d3a75a2ad43b2c45184d97c2121a3ac9b410b6
Morty <70688412+yiweichi@users.noreply.github.com>
2025-01-09 17:22:02 +0800

    Update coordinator/internal/logic/provertask/bundle_prover_task.go
    Co-authored-by: colin <102356659+colinlyguo@users.noreply.github.com>

--- coordinator/internal/logic/provertask/bundle_prover_task.go
+++ coordinator/internal/logic/provertask/bundle_prover_task.go
@@ -67,7 +67,7 @@ func (bp *BundleProverTask) Assign(ctx *gin.Context, getTaskParameter *coordinat
 	if strings.HasPrefix(taskCtx.ProverName, ExternalProverNamePrefix) {
 		unassignedBundleCount, getCountError := bp.bundleOrm.GetUnassignedBundleCount(ctx.Copy(), maxActiveAttempts, maxTotalAttempts)
 		if getCountError != nil {
-			log.Error("failed to get unassigned batch proving tasks count", "height", getTaskParameter.ProverHeight, "err", err)
+			log.Error("failed to get unassigned batch proving tasks count", "height", getTaskParameter.ProverHeight, "err", getCountError)
 			return nil, ErrCoordinatorInternalFailure
 		}
 		// Assign external prover if unassigned task number exceeds threshold
