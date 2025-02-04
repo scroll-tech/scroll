afccc3d8734549f8a635d0933cfcaba50bf7c4b5
noelwei <fan@scroll.io>
2025-02-04 20:03:00 +0900

    fix lint
    Signed-off-by: noelwei <fan@scroll.io>

--- common/types/message/euclid/message_test.go
+++ common/types/message/euclid/message_test.go
@@ -15,10 +15,10 @@ func TestDeserializeProof(t *testing.T) {
 
 	// Decode the JSON data into an BatchTask instance.
 	var batchProof BatchProof
-	if err := json.Unmarshal(data, &batchProof); err != nil {
+	if err = json.Unmarshal(data, &batchProof); err != nil {
 		t.Fatalf("failed to unmarshal JSON into Batch Proof: %v", err)
 	}
-	if err := batchProof.SanityCheck(); err != nil {
+	if err = batchProof.SanityCheck(); err != nil {
 		t.Fatalf("failed to sanity check for Batch Proof: %v", err)
 	}
 
@@ -30,10 +30,10 @@ func TestDeserializeProof(t *testing.T) {
 
 	// Decode the JSON data into an BatchTask instance.
 	var bundleProof BundleProof
-	if err := json.Unmarshal(data, &bundleProof); err != nil {
+	if err = json.Unmarshal(data, &bundleProof); err != nil {
 		t.Fatalf("failed to unmarshal JSON into Bundle Proof: %v", err)
 	}
-	if err := bundleProof.SanityCheck(); err != nil {
+	if err = bundleProof.SanityCheck(); err != nil {
 		t.Fatalf("failed to sanity check for Bundle Proof: %v", err)
 	}
 }
