f77b8aebd5c568a1d5223b8391ecf7cd4080c4c3
Morty <yiweichi1@gmail.com>
2025-03-11 13:18:22 +0800

    fix: to check high version forkname

--- coordinator/internal/logic/auth/login.go
+++ coordinator/internal/logic/auth/login.go
@@ -42,7 +42,7 @@ func NewLoginLogic(db *gorm.DB, cfg *config.Config, vf *verifier.Verifier) *Logi
 
 	var highHardForks []string
 	highHardForks = append(highHardForks, cfg.ProverManager.Verifier.HighVersionCircuit.ForkName)
-	if cfg.ProverManager.Verifier.LowVersionCircuit.ForkName != "euclid" {
+	if cfg.ProverManager.Verifier.HighVersionCircuit.ForkName != "euclid" {
 		highHardForks = append(highHardForks, cfg.ProverManager.Verifier.LowVersionCircuit.ForkName)
 	}
 	proverVersionHardForkMap[cfg.ProverManager.Verifier.HighVersionCircuit.MinProverVersion] = highHardForks
