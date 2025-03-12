459941d942b75079c6a377529aee37841036618f
colinlyguo <colinlyguo@scroll.io>
2025-03-13 03:27:38 +0800

    use hexutil.Big

--- common/types/message/message.go
+++ common/types/message/message.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"math/big"
 
 	"github.com/scroll-tech/go-ethereum/common"
+	"github.com/scroll-tech/go-ethereum/common/hexutil"
 	"github.com/scroll-tech/go-ethereum/log"
 )
 
@@ -88,11 +88,11 @@ type ChunkInfo struct {
 
 // BlockContextV2 is the block context for euclid v2
 type BlockContextV2 struct {
-	Timestamp uint64   `json:"timestamp"`
-	BaseFee   *big.Int `json:"base_fee"`
-	GasLimit  uint64   `json:"gas_limit"`
-	NumTxs    uint16   `json:"num_txs"`
-	NumL1Msgs uint16   `json:"num_l1_msgs"`
+	Timestamp uint64      `json:"timestamp"`
+	BaseFee   hexutil.Big `json:"base_fee"`
+	GasLimit  uint64      `json:"gas_limit"`
+	NumTxs    uint16      `json:"num_txs"`
+	NumL1Msgs uint16      `json:"num_l1_msgs"`
 }
 
 // SubCircuitRowUsage tracing info added in v0.11.0rc8
