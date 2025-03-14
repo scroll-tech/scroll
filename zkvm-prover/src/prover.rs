901693a2c00da3ecd34eca264f6f1fa1e176b15f
colinlyguo <colinlyguo@scroll.io>
2025-03-14 18:42:31 +0800

    remove a log

--- zkvm-prover/src/prover.rs
+++ zkvm-prover/src/prover.rs
@@ -182,10 +182,6 @@ impl LocalProver {
         // if we got assigned a task for an unknown hard fork, there is something wrong in the
         // coordinator
         let config = self.config.circuits.get(hard_fork_name).unwrap();
-        println!(
-            "config workspace path for hard-fork {:?} = {:?}",
-            hard_fork_name, config.workspace_path
-        );
 
         match hard_fork_name {
             "euclid" => Arc::new(Arc::new(Mutex::new(EuclidHandler::new(
