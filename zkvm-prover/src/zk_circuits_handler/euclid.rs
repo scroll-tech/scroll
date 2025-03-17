ad32dd6fb4efaf595d957c67752baa7e85eba581
kunxian xia <xiakunxian130@gmail.com>
2025-03-17 14:05:41 +0800

    bump zkvm-prover from 0.1.0-rc.7 to 0.1.0-rc.8

--- zkvm-prover/src/zk_circuits_handler/euclid.rs
+++ zkvm-prover/src/zk_circuits_handler/euclid.rs
@@ -6,7 +6,7 @@ use async_trait::async_trait;
 use scroll_proving_sdk::prover::{proving_service::ProveRequest, ProofType};
 use scroll_zkvm_prover::{
     task::{batch::BatchProvingTask, bundle::BundleProvingTask, chunk::ChunkProvingTask},
-    BatchProver, BundleProver, ChunkProver,
+    BatchProver, BundleProver, ChunkProver, ProverConfig,
 };
 use tokio::sync::Mutex;
 pub struct EuclidHandler {
@@ -24,18 +24,39 @@ impl EuclidHandler {
         let cache_dir = workspace_path.join("cache");
         let chunk_exe = workspace_path.join("chunk/app.vmexe");
         let chunk_app_config = workspace_path.join("chunk/openvm.toml");
-        let chunk_prover = ChunkProver::setup(chunk_exe, chunk_app_config, Some(cache_dir.clone()))
-            .expect("Failed to setup chunk prover");
+        let chunk_prover = ChunkProver::setup(
+            chunk_exe,
+            chunk_app_config,
+            Some(cache_dir.clone()),
+            ProverConfig {
+                segment_len: Some((1 << 22) - 100),
+            },
+        )
+        .expect("Failed to setup chunk prover");
 
         let batch_exe = workspace_path.join("batch/app.vmexe");
         let batch_app_config = workspace_path.join("batch/openvm.toml");
-        let batch_prover = BatchProver::setup(batch_exe, batch_app_config, Some(cache_dir.clone()))
-            .expect("Failed to setup batch prover");
+        let batch_prover = BatchProver::setup(
+            batch_exe,
+            batch_app_config,
+            Some(cache_dir.clone()),
+            ProverConfig {
+                segment_len: Some((1 << 22) - 100),
+            },
+        )
+        .expect("Failed to setup batch prover");
 
         let bundle_exe = workspace_path.join("bundle/app.vmexe");
         let bundle_app_config = workspace_path.join("bundle/openvm.toml");
-        let bundle_prover = BundleProver::setup(bundle_exe, bundle_app_config, Some(cache_dir))
-            .expect("Failed to setup bundle prover");
+        let bundle_prover = BundleProver::setup(
+            bundle_exe,
+            bundle_app_config,
+            Some(cache_dir),
+            ProverConfig {
+                segment_len: Some((1 << 22) - 100),
+            },
+        )
+        .expect("Failed to setup bundle prover");
 
         Self {
             chunk_prover,
