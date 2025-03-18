b3e46673f64f3642448439770a93856818f65add
colinlyguo <colinlyguo@scroll.io>
2025-03-18 13:50:15 +0800

    remove FIXME

--- coordinator/internal/logic/provertask/batch_prover_task.go
+++ coordinator/internal/logic/provertask/batch_prover_task.go
@@ -293,6 +293,6 @@ func (bp *BatchProverTask) getBatchTaskDetail(dbBatch *orm.Batch, chunkInfos []*
 	// | bytes32 | bytes32 | bytes48        | bytes48   |
 	taskDetail.KzgProof = message.Byte48{Big: hexutil.Big(*new(big.Int).SetBytes(dbBatch.BlobDataProof[112:160]))}
 	taskDetail.KzgCommitment = message.Byte48{Big: hexutil.Big(*new(big.Int).SetBytes(dbBatch.BlobDataProof[64:112]))}
-	taskDetail.ChallengeDigest = common.BytesToHash(dbBatch.BlobDataProof[0:32]) // FIXME: Challenge = ChallengeDigest % BLS_MODULUS, get the original ChallengeDigest.
+	taskDetail.ChallengeDigest = common.BytesToHash(dbBatch.BlobDataProof[0:32])
 	return taskDetail, nil
 }
