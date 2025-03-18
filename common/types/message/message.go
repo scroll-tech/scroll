819ded9389583ffd7b40af3adf7f4337bbdef666
noelwei <fan@scroll.io>
2025-03-19 08:44:56 +0900

    resume dep to rc.8 and skip ChallengeDigest field
    Signed-off-by: noelwei <fan@scroll.io>

--- common/types/message/message.go
+++ common/types/message/message.go
@@ -96,9 +96,9 @@ type BatchTaskDetail struct {
 	ChunkProofs     []ChunkProof `json:"chunk_proofs"`
 	BatchHeader     interface{}  `json:"batch_header"`
 	BlobBytes       []byte       `json:"blob_bytes"`
-	KzgProof        Byte48       `json:"kzg_proof"`
-	KzgCommitment   Byte48       `json:"kzg_commitment"`
-	ChallengeDigest common.Hash  `json:"challenge_digest"`
+	KzgProof        Byte48       `json:"kzg_proof,omitempty"`
+	KzgCommitment   Byte48       `json:"kzg_commitment,omitempty"`
+	ChallengeDigest common.Hash  `json:"challenge_digest,omitempty"`
 }
 
 // BundleTaskDetail consists of all the information required to describe the task to generate a proof for a bundle of batches.
