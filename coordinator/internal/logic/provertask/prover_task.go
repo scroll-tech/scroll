78a458daa56e596bf15a8aa3dd6cc9593dafaaa4
colinlyguo <colinlyguo@scroll.io>
2025-03-27 16:20:16 +0800

    update logs

--- coordinator/internal/logic/provertask/prover_task.go
+++ coordinator/internal/logic/provertask/prover_task.go
@@ -122,7 +122,7 @@ func (b *BaseProverTask) hardForkSanityCheck(ctx *gin.Context, taskCtx *proverTa
 	}
 
 	if _, ok := taskCtx.HardForkNames[hardForkName]; !ok {
-		return "", fmt.Errorf("to be assigned prover task's hard-fork name is not the same as prover, proverName: %s, proverVersion: %s, hardForkName: %s", taskCtx.ProverName, taskCtx.ProverVersion, hardForkName)
+		return "", fmt.Errorf("to be assigned prover task's hard-fork name is not the same as prover, proverName: %s, proverVersion: %s, hardForkName: %s, taskHardForkName: %s", taskCtx.ProverName, taskCtx.ProverVersion, hardForkName, taskCtx.HardForkNames)
 	}
 	return hardForkName, nil
 }
