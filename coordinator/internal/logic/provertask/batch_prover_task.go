4c4cff0ca4a454af5cf8680b713f61fc92e72354
colinlyguo <colinlyguo@scroll.io>
2025-03-31 20:04:22 +0800

    temporarily mask KzgProof

--- coordinator/internal/logic/provertask/batch_prover_task.go
+++ coordinator/internal/logic/provertask/batch_prover_task.go
@@ -293,7 +293,7 @@ func (bp *BatchProverTask) getBatchTaskDetail(dbBatch *orm.Batch, chunkInfos []*
 	// | z       | y       | kzg_commitment | kzg_proof |
 	// |---------|---------|----------------|-----------|
 	// | bytes32 | bytes32 | bytes48        | bytes48   |
-	taskDetail.KzgProof = message.Byte48{Big: hexutil.Big(*new(big.Int).SetBytes(dbBatch.BlobDataProof[112:160]))}
+	// taskDetail.KzgProof = message.Byte48{Big: hexutil.Big(*new(big.Int).SetBytes(dbBatch.BlobDataProof[112:160]))}
 	taskDetail.KzgCommitment = message.Byte48{Big: hexutil.Big(*new(big.Int).SetBytes(dbBatch.BlobDataProof[64:112]))}
 
 	return taskDetail, nil
