3f654dfc0095d1d26df27c6d04a530ef62b44bd8
colinlyguo <colinlyguo@scroll.io>
2025-04-11 02:00:53 +0800

    tweak

--- coordinator/internal/logic/auth/login.go
+++ coordinator/internal/logic/auth/login.go
@@ -77,6 +77,7 @@ func (l *LoginLogic) Check(login *types.LoginParameter) error {
 
 	// FIXME: for backward compatibility, set prover version as darwin prover version,
 	// change it to l.cfg.ProverManager.Verifier.LowVersionCircuit.MinProverVersion after Euclid upgrade.
+	// hardcode the prover version because l.cfg.ProverManager.Verifier.LowVersionCircuit.MinProverVersion is used in another check and should be set as v4.4.89 for darwinV2 provers.
 	if !version.CheckScrollRepoVersion(login.Message.ProverVersion, "v4.4.56") {
 		return fmt.Errorf("incompatible prover version. please upgrade your prover, minimum allowed version: %s, actual version: %s",
 			l.cfg.ProverManager.Verifier.LowVersionCircuit.MinProverVersion, login.Message.ProverVersion)
