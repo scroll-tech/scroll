03eadd0dfe788105e3ee33e8cae0e8a41b801593
colinlyguo <colinlyguo@scroll.io>
2025-04-16 17:07:31 +0800

    feat(gas-oracle): add a warn log when entering fallback mode

--- rollup/internal/controller/relayer/l1_relayer.go
+++ rollup/internal/controller/relayer/l1_relayer.go
@@ -167,6 +167,7 @@ func (r *Layer1Relayer) ProcessGasPriceOracle() {
 				if r.lastBaseFee == r.cfg.GasOracleConfig.L1BaseFeeDefault && r.lastBlobBaseFee == r.cfg.GasOracleConfig.L1BlobBaseFeeDefault {
 					return
 				}
+				log.Warn("The committing batch has been stuck for a long time, it's likely that the L1 gas fee spiked, set fees to default values", "baseFee", baseFee, "blobBaseFee", blobBaseFee)
 				baseFee = r.cfg.GasOracleConfig.L1BaseFeeDefault
 				blobBaseFee = r.cfg.GasOracleConfig.L1BlobBaseFeeDefault
 			} else if err != nil {
