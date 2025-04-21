b80e59aabe252bde6ab369d340c28f021cd62dd6
colinlyguo <colinlyguo@scroll.io>
2025-04-21 21:01:29 +0800

    refactor

--- rollup/cmd/proposer_tool/app/app.go
+++ rollup/cmd/proposer_tool/app/app.go
@@ -3,28 +3,18 @@ package app
 import (
 	"context"
 	"fmt"
-	"math/big"
 	"os"
 	"os/signal"
-	"time"
 
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/scroll-tech/da-codec/encoding"
-	"github.com/scroll-tech/go-ethereum/common"
-	gethTypes "github.com/scroll-tech/go-ethereum/core/types"
-	"github.com/scroll-tech/go-ethereum/ethclient"
 	"github.com/scroll-tech/go-ethereum/log"
 	"github.com/urfave/cli/v2"
 
-	"scroll-tech/common/database"
 	"scroll-tech/common/utils"
 	"scroll-tech/common/version"
-	"scroll-tech/database/migrate"
 
 	"scroll-tech/rollup/internal/config"
 	"scroll-tech/rollup/internal/controller/watcher"
-	"scroll-tech/rollup/internal/orm"
-	rutils "scroll-tech/rollup/internal/utils"
 )
 
 var app *cli.App
@@ -54,105 +44,8 @@ func action(ctx *cli.Context) error {
 	}
 
 	subCtx, cancel := context.WithCancel(ctx.Context)
-	// Init db connection
-	db, err := database.InitDB(cfg.DBConfig)
-	if err != nil {
-		log.Crit("failed to init db connection", "err", err)
-	}
-	sqlDB, err := db.DB()
-	if err != nil {
-		log.Crit("failed to get db connection", "error", err)
-	}
-	if err = migrate.ResetDB(sqlDB); err != nil {
-		log.Crit("failed to reset db", "error", err)
-	}
-	log.Info("successfully reset db")
-	defer func() {
-		cancel()
-		if err = database.CloseDB(db); err != nil {
-			log.Crit("failed to close db connection", "error", err)
-		}
-	}()
-
-	// Init dbForReplay connection
-	dbForReplay, err := database.InitDB(cfg.DBConfigForReplay)
-	if err != nil {
-		log.Crit("failed to init dbForReplay connection", "err", err)
-	}
-	defer func() {
-		cancel()
-		if err = database.CloseDB(dbForReplay); err != nil {
-			log.Crit("failed to close dbForReplay connection", "error", err)
-		}
-	}()
-
-	// Init l2geth connection
-	l2Client, err := ethclient.Dial(cfg.L2Config.Endpoint)
-	if err != nil {
-		log.Crit("failed to connect l2 geth", "config file", cfgFile, "error", err)
-	}
 
 	startL2BlockHeight := ctx.Uint64(utils.StartL2BlockFlag.Name)
-
-	prevChunk, err := orm.NewChunk(dbForReplay).GetParentChunkByBlockNumber(subCtx, startL2BlockHeight)
-	if err != nil {
-		log.Crit("failed to get previous chunk", "error", err)
-	}
-
-	var startQueueIndex uint64
-	if prevChunk != nil {
-		startQueueIndex = prevChunk.TotalL1MessagesPoppedBefore + prevChunk.TotalL1MessagesPoppedInChunk
-	}
-
-	startBlock := uint64(0)
-	if prevChunk != nil {
-		startBlock = prevChunk.EndBlockNumber + 1
-	}
-
-	var chunk *encoding.Chunk
-	for blockNum := startBlock; blockNum <= startL2BlockHeight; blockNum++ {
-		block, err := l2Client.BlockByNumber(context.Background(), big.NewInt(int64(blockNum)))
-		if err != nil {
-			log.Crit("failed to get block", "block number", blockNum, "error", err)
-		}
-
-		for _, tx := range block.Transactions() {
-			if tx.Type() == gethTypes.L1MessageTxType {
-				startQueueIndex++
-			}
-		}
-
-		if blockNum == startL2BlockHeight {
-			chunk = &encoding.Chunk{Blocks: []*encoding.Block{{Header: block.Header()}}}
-		}
-	}
-
-	// Setting empty hash as the post_l1_message_queue_hash of the first chunk,
-	// i.e., treating the first L1 message after this chunk as the first L1 message in message queue v2.
-	// Though this setting is different from mainnet, it's simple yet sufficient for data analysis usage.
-	_, err = orm.NewChunk(db).InsertTestChunkForProposerTool(subCtx, chunk, encoding.CodecV0, startQueueIndex)
-	if err != nil {
-		log.Crit("failed to insert chunk", "error", err)
-	}
-
-	batch := &encoding.Batch{
-		Index:                      0,
-		TotalL1MessagePoppedBefore: 0,
-		ParentBatchHash:            common.Hash{},
-		Chunks:                     []*encoding.Chunk{chunk},
-	}
-
-	var dbBatch *orm.Batch
-	dbBatch, err = orm.NewBatch(db).InsertBatch(subCtx, batch, encoding.CodecV0, rutils.BatchMetrics{})
-	if err != nil {
-		log.Crit("failed to insert batch", "error", err)
-	}
-
-	if err = orm.NewChunk(db).UpdateBatchHashInRange(subCtx, 0, 0, dbBatch.Hash); err != nil {
-		log.Crit("failed to update batch hash for chunks", "error", err)
-	}
-
-	registry := prometheus.DefaultRegisterer
 
 	genesisPath := ctx.String(utils.Genesis.Name)
 	genesis, err := utils.ReadGenesis(genesisPath)
@@ -160,24 +53,13 @@ func action(ctx *cli.Context) error {
 		log.Crit("failed to read genesis", "genesis file", genesisPath, "error", err)
 	}
 
-	// sanity check config
-	if cfg.L2Config.BatchProposerConfig.MaxChunksPerBatch <= 0 {
-		log.Crit("cfg.L2Config.BatchProposerConfig.MaxChunksPerBatch must be greater than 0")
-	}
-	if cfg.L2Config.ChunkProposerConfig.MaxL2GasPerChunk <= 0 {
-		log.Crit("cfg.L2Config.ChunkProposerConfig.MaxL2GasPerChunk must be greater than 0")
-	}
-
 	minCodecVersion := encoding.CodecVersion(ctx.Uint(utils.MinCodecVersionFlag.Name))
-	chunkProposer := watcher.NewChunkProposer(subCtx, cfg.L2Config.ChunkProposerConfig, minCodecVersion, genesis.Config, db, registry)
-	chunkProposer.SetReplayDB(dbForReplay)
-	batchProposer := watcher.NewBatchProposer(subCtx, cfg.L2Config.BatchProposerConfig, minCodecVersion, genesis.Config, db, registry)
-	batchProposer.SetReplayDB(dbForReplay)
-	bundleProposer := watcher.NewBundleProposer(subCtx, cfg.L2Config.BundleProposerConfig, minCodecVersion, genesis.Config, db, registry)
 
-	go utils.Loop(subCtx, 100*time.Millisecond, chunkProposer.TryProposeChunk)
-	go utils.Loop(subCtx, 100*time.Millisecond, batchProposer.TryProposeBatch)
-	go utils.Loop(subCtx, 100*time.Millisecond, bundleProposer.TryProposeBundle)
+	proposerTool, err := watcher.NewProposerTool(subCtx, cancel, cfg, startL2BlockHeight, minCodecVersion, genesis.Config)
+	if err != nil {
+		log.Crit("failed to create proposer tool", "startL2BlockHeight", startL2BlockHeight, "minCodecVersion", minCodecVersion, "error", err)
+	}
+	proposerTool.Start()
 
 	// Finish start all proposer tool functions.
 	log.Info("Start proposer-tool successfully", "version", version.Version)
@@ -188,6 +70,9 @@ func action(ctx *cli.Context) error {
 
 	// Wait until the interrupt signal is received from an OS signal.
 	<-interrupt
+
+	cancel()
+	proposerTool.Stop()
 
 	return nil
 }
