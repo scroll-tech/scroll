dc6d3affd00428128d8e7035e1204d08740ea413
Ho <fan@scroll.io>
2025-06-17 17:21:32 +0900

    fix test failure

--- coordinator/internal/controller/api/controller.go
+++ coordinator/internal/controller/api/controller.go
@@ -1,14 +1,11 @@
 package api
 
 import (
-	"encoding/json"
-
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/scroll-tech/go-ethereum/log"
 	"github.com/scroll-tech/go-ethereum/params"
 	"gorm.io/gorm"
 
-	"scroll-tech/common/libzkp"
 	"scroll-tech/coordinator/internal/config"
 	"scroll-tech/coordinator/internal/logic/verifier"
 )
@@ -31,7 +28,8 @@ func InitController(cfg *config.Config, chainCfg *params.ChainConfig, db *gorm.D
 
 	log.Info("verifier created", "openVmVerifier", vf.OpenVMVkMap)
 
-	l2cfg := cfg.L2.Endpoint
+	// TODO: enable this when the libzkp has been updated
+	/*l2cfg := cfg.L2.Endpoint
 	if l2cfg == nil {
 		panic("l2geth is not specified")
 	}
@@ -39,7 +37,7 @@ func InitController(cfg *config.Config, chainCfg *params.ChainConfig, db *gorm.D
 	if err != nil {
 		panic(err)
 	}
-	libzkp.InitL2geth(string(l2cfgBytes))
+	libzkp.InitL2geth(string(l2cfgBytes))*/
 
 	Auth = NewAuthController(db, cfg, vf)
 	GetTask = NewGetTaskController(cfg, chainCfg, db, reg)
