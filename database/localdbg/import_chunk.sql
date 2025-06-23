e5592052d56cea3224d9a325195a1f087948a396
Ho <fan@scroll.io>
2025-06-23 11:38:56 +0900

    add debug stuff for local running coordinator / prover pair

---
+++ database/localdbg/import_chunk.sql
@@ -0,0 +1,8 @@
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795824', '0x05cab11cc4054dc7c8b559d2c7fd412891f6c94512539db37456cadaca7fceeb', '10405845', '10405944', '0xbf4294a7c70709c02f0b38bd532e3fb8c8485e34b598992049ef14aeac3d6daf', '0x522d7f22665754abccab64dc67f1ae4b2dda9729613893f20117810f5092427d', '1067977', '2', '1749040306', '0xdbc3ec00148cb6f8369ac740006613ca9d0c4502a134eab0b19019c55bcca2ca', '0xbe7752f82ad42463b27e0ac06b2c86e853a2f1fdf36456c8496979fda2998896', '0x86c1756e36e88429f488999207317e04a1c15b52d47c568a149cc973061c74eb', '0xee2216fa66e00af3c00c4f21172202c17560b7bd9dc661321f44988f428e2e7d', '5951574', '98', '0', '0', 'true', '0x588be45906ca9c9f7436a0d092603667e8cff81641115fb2e142714c00000000', '0x3223e4556c429f27f83e309efb061653e1624706f288fc85563bef8100000000');
---
+++ database/localdbg/import_more_chunks.sql
@@ -0,0 +1,72 @@
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795823', '0xdbc3ec00148cb6f8369ac740006613ca9d0c4502a134eab0b19019c55bcca2ca', '10405745', '10405844', '0x9b89275494c743803843bd3df6b7b9ff43bbbef2ec1a67b9680681f73476feac', '0xff1ae0f43684296baf178282a9384365357b92c32053518e9e0979cd3732d632', '1067973', '4', '1749039452', '0x72f0cbeb2de49271ee5f102da89c1783e2baa5b01ce96f8bb1bbcdbb11f72e0e', '0x86c1756e36e88429f488999207317e04a1c15b52d47c568a149cc973061c74eb', '0x6a4bc0f6dcf544d18084595284bd6da0ad82bd3daa10d5924fe6cadc5980b889', '0xee2216fa66e00af3c00c4f21172202c17560b7bd9dc661321f44988f428e2e7d', '9672856', '103', '0', '0', 'true', '0xac1cb1387af33778061a9301c10d4a4481d99452785714667695ebb400000000', '0x588be45906ca9c9f7436a0d092603667e8cff81641115fb2e142714c00000000');
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795822', '0x72f0cbeb2de49271ee5f102da89c1783e2baa5b01ce96f8bb1bbcdbb11f72e0e', '10405645', '10405744', '0xca94e73dcf085bb4cf11e82a69a46a450d8a13b0ab100a36cf27235d08506a25', '0x17e5632b0e15e21b317fa27134fb24bb5097a925cf0b3a1441065474b8616741', '1067972', '1', '1749038511', '0x6d36cd65eddc355c504e28159844b7a671a2b627fabf83b5b1c9b0819aa800a5', '0x6a4bc0f6dcf544d18084595284bd6da0ad82bd3daa10d5924fe6cadc5980b889', '0x13e9d47953f0f6dae1a3625e5706e26f8897da0c86994426610e1d64918dd6dc', '0xee2216fa66e00af3c00c4f21172202c17560b7bd9dc661321f44988f428e2e7d', '6897786', '105', '0', '0', 'true', '0xbf186c8c0b504aa5b01c72219f41cb317a5c774eb6876867bfb5f9cc00000000', '0xac1cb1387af33778061a9301c10d4a4481d99452785714667695ebb400000000');
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795821', '0x6d36cd65eddc355c504e28159844b7a671a2b627fabf83b5b1c9b0819aa800a5', '10405545', '10405644', '0x13167bb858b1c6210c5965e72f55469e5bcc204a3ad94b585c9d4edee7583c32', '0xd0eb92204cbd8a4a44f0aa75b1554ca6d8e95a2da8460eaf2e4f2a85ae89f87a', '1067971', '1', '1749037722', '0x7d3db73a6a4503b6bf0acd269554c97928c08d0d8b0b258e8ab468bf1685d823', '0x13e9d47953f0f6dae1a3625e5706e26f8897da0c86994426610e1d64918dd6dc', '0x1a71a3b84ee819dd5827369cb905d05c66cd70ea0d00e996a0fc3b2a18beab78', '0xee2216fa66e00af3c00c4f21172202c17560b7bd9dc661321f44988f428e2e7d', '9847682', '102', '0', '0', 'true', '0xd350c1d4bccade184fa3e1768b9865bf2ee0bf40108164c16287098b00000000', '0xbf186c8c0b504aa5b01c72219f41cb317a5c774eb6876867bfb5f9cc00000000');
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795820', '0x7d3db73a6a4503b6bf0acd269554c97928c08d0d8b0b258e8ab468bf1685d823', '10405445', '10405544', '0xfab227c444916cc75ae161aed936f838ee1099d386098bfa8dd827e14d2707fb', '0x97f10e03c344086069e8854c3fe8496489978690b30f1f72f25f9d108c52c359', '1067971', '0', '1749036803', '0xe832fd69a7c60b6f7882b095970ff9af1945509b08f8a27c24d09b72f1065610', '0x1a71a3b84ee819dd5827369cb905d05c66cd70ea0d00e996a0fc3b2a18beab78', '0x52d34e124b415b90029d9689a588be66ed84aa3a17ee540efcb7dd179ccc2bf2', '0xee2216fa66e00af3c00c4f21172202c17560b7bd9dc661321f44988f428e2e7d', '9717230', '102', '0', '0', 'true', '0xd350c1d4bccade184fa3e1768b9865bf2ee0bf40108164c16287098b00000000', '0xd350c1d4bccade184fa3e1768b9865bf2ee0bf40108164c16287098b00000000');
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795819', '0xe832fd69a7c60b6f7882b095970ff9af1945509b08f8a27c24d09b72f1065610', '10405345', '10405444', '0x1159e4f1e0d9263451d4fcb350fa3c40ac42e1167e7ee5ccd9f8f3c9bd63f7e2', '0x3cdb32ac5cc1a6ba55f63d7862a8ea111aa6736a54577a37d2a2a7ca5ed27e77', '1067968', '3', '1749035912', '0x0140216f406c4d7c656e138c2a292aed2feecd3b298449f210c3dd8a94b20cbe', '0x52d34e124b415b90029d9689a588be66ed84aa3a17ee540efcb7dd179ccc2bf2', '0xb3be5e60afe9e45f04a3ee5173e099f791fc8b3318b8dc87ffb2fc58f0cf0051', '0xa565f3fdd399dc23aa34602405ff543a7628d36e4c51f232cfa42ad8ead16a5c', '7947615', '99', '0', '0', 'true', '0x701631a081e57a034fc4901a2502ec45233faeb52d4ee95a3c358ccc00000000', '0xd350c1d4bccade184fa3e1768b9865bf2ee0bf40108164c16287098b00000000');
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795818', '0x0140216f406c4d7c656e138c2a292aed2feecd3b298449f210c3dd8a94b20cbe', '10405245', '10405344', '0xcbd0bf8de84d031b9ea27e0cdc35ba1886b65e823f4df361dbbab747bd2445da', '0xb0b40d064a9a6ec6b2234397eea1cd9baa643cabadea7b9c5ebbd8e22e52c350', '1067968', '0', '1749034972', '0x4c900747837da5d70e0933e1482c92731da16c35ce0f6926a4f409c219143fc5', '0xb3be5e60afe9e45f04a3ee5173e099f791fc8b3318b8dc87ffb2fc58f0cf0051', '0xb20143ddfb4277e957555292ea72be3d50e30f3f185bad011b6d10b29faeefd2', '0xa565f3fdd399dc23aa34602405ff543a7628d36e4c51f232cfa42ad8ead16a5c', '5931635', '103', '0', '0', 'true', '0x701631a081e57a034fc4901a2502ec45233faeb52d4ee95a3c358ccc00000000', '0x701631a081e57a034fc4901a2502ec45233faeb52d4ee95a3c358ccc00000000');
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795817', '0x4c900747837da5d70e0933e1482c92731da16c35ce0f6926a4f409c219143fc5', '10405145', '10405244', '0x7571f7779a8078a176cd9458ffdc9c75a6ff4056f88e47ba72ff96a6d165e1bc', '0x083750d7664c98f10ce3042890c26643d68fb7bbf6fe582deb75caaa80618d4d', '1067967', '1', '1749033992', '0x6e6da747415600904d5b29241618de67760b2fbd8cbbd3a58efb27718b549c36', '0xb20143ddfb4277e957555292ea72be3d50e30f3f185bad011b6d10b29faeefd2', '0x131fe9cfc6d92555efc3e1b222c81968e2b6f13595513188140d6da48a5ee0a4', '0xa565f3fdd399dc23aa34602405ff543a7628d36e4c51f232cfa42ad8ead16a5c', '5354644', '104', '0', '0', 'true', '0x421ad11af2c169fff8f376e3838b644032fa00417634bbfba4557ec300000000', '0x701631a081e57a034fc4901a2502ec45233faeb52d4ee95a3c358ccc00000000');
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795816', '0x6e6da747415600904d5b29241618de67760b2fbd8cbbd3a58efb27718b549c36', '10405045', '10405144', '0xa2cc3ea845ffa14c0b392dd391f0215867267bef9c6d68008ba2d430ef69be32', '0x8c6fbd3a543c08d604ed6330c62c635132de0c3431d632f4c26f00da3d36f07f', '1067964', '3', '1749033192', '0x7c8b5eb58109ed7947bc90c1ba36ab7e02fa59512e34c5af1db69e01a7a2f72a', '0x131fe9cfc6d92555efc3e1b222c81968e2b6f13595513188140d6da48a5ee0a4', '0x86590a1c0de788f0cab33135eaf79c59172da157cd175670695074e7aeb75ea3', '0xa565f3fdd399dc23aa34602405ff543a7628d36e4c51f232cfa42ad8ead16a5c', '8354632', '102', '0', '0', 'true', '0x8f7b703da3b65cd87a10314594d8906ef41273a67e855d5fb247bf1800000000', '0x421ad11af2c169fff8f376e3838b644032fa00417634bbfba4557ec300000000');
+INSERT INTO chunk ("index", hash, start_block_number, end_block_number,
+       start_block_hash, end_block_hash, total_l1_messages_popped_before,
+       total_l1_messages_popped_in_chunk, start_block_time,
+       parent_chunk_hash, state_root, parent_chunk_state_root,
+       withdraw_root, total_l2_tx_gas, total_l2_tx_num,
+       total_l1_commit_calldata_size, total_l1_commit_gas,
+       enable_compress, prev_l1_message_queue_hash, post_l1_message_queue_hash
+       ) VALUES ('795815', '0x7c8b5eb58109ed7947bc90c1ba36ab7e02fa59512e34c5af1db69e01a7a2f72a', '10404945', '10405044', '0x806f53f6c2ef7d880b1b422428dfc4b655e66827aa2853443b7061f50807a2e8', '0xff8f1c3f2dd77c24313b5689a72478c3090349103e668065df2c1afc6e67f947', '1067962', '2', '1749032332', '0x627211154cdac0037baf7541dced64ef88454fa27861b57fa3fa501a3b52ee8e', '0x86590a1c0de788f0cab33135eaf79c59172da157cd175670695074e7aeb75ea3', '0x25bf485ee5e3b6d440571521dbd8ee990795a4c1089e39afecb9ea571e15311d', '0xa565f3fdd399dc23aa34602405ff543a7628d36e4c51f232cfa42ad8ead16a5c', '7143030', '102', '0', '0', 'true', '0x1b6df8ca8be3757e5d0415e09963b984f7560cb9b32be9c5e75b702600000000', '0x8f7b703da3b65cd87a10314594d8906ef41273a67e855d5fb247bf1800000000');
