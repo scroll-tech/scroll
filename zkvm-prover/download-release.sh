824b2e9c787022e70178d6d6212cf830c4079ac0
Ho <fan@scroll.io>
2025-07-04 12:00:19 +0900

    upgrade dep

--- zkvm-prover/download-release.sh
+++ zkvm-prover/download-release.sh
@@ -2,19 +2,24 @@
 
 # release version
 if [ -z "${SCROLL_ZKVM_VERSION}" ]; then
-    SCROLL_ZKVM_VERSION=$($SHELL ./print_high_zkvm_version.sh | cut -d' ' -f1|cut -c2-)
+#   before we use version tag for zkvm, we can not acquire the correct version of zkvm from script
+#    SCROLL_ZKVM_VERSION=$($SHELL ./print_high_zkvm_version.sh | cut -d' ' -f1|cut -c2-)
+    SCROLL_ZKVM_VERSION=0.5.0rc0
 fi
 
 echo $SCROLL_ZKVM_VERSION
 
+mkdir -p .work/chunk
+mkdir -p .work/batch
+mkdir -p .work/bundle
+
 # chunk-circuit exe
 wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/chunk/app.vmexe -O .work/chunk/app.vmexe
-
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/chunk/openvm.toml -O .work/chunk/openvm.toml
 # batch-circuit exe
 wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/batch/app.vmexe -O .work/batch/app.vmexe
-
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/batch/openvm.toml -O .work/batch/openvm.toml
 # bundle-circuit exe
 wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/bundle/app.vmexe -O .work/bundle/app.vmexe
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/bundle/openvm.toml -O .work/bundle/openvm.toml
 
-# bundle-circuit exe, legacy version, may not exist
-wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/bundle/app_euclidv1.vmexe -O .work/bundle/app_euclidv1.vmexe || echo "legacy app not exist for $SCROLL_ZKVM_VERSION"
