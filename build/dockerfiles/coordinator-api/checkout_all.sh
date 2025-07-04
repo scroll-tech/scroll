654cd428522501e17a4a5c11c54c2a85e459bf77
Ho <fan@scroll.io>
2025-07-04 15:55:35 +0900

    update gpu dep

--- build/dockerfiles/coordinator-api/checkout_all.sh
+++ build/dockerfiles/coordinator-api/checkout_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -uex
 
-PLONKY3_GPU_COMMIT=261b322        # v0.2.0
-OPENVM_STARK_GPU_COMMIT=3082234   # PR#48
-OPENVM_GPU_COMMIT=8094b4f         # branch: patch-v1.2.0
+PLONKY3_GPU_COMMIT=450ec18        # feynman
+OPENVM_STARK_GPU_COMMIT=e3b2d6    # branch: sync/upstream-250702
+OPENVM_GPU_COMMIT=75df915    # branch: patch-v1.2.1-rc.1-pipe
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)
 
