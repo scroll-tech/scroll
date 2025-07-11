1cb71d58b7d12fdafb3c55d06ff92efa440b11a0
Ho <fan@scroll.io>
2025-07-11 11:28:13 +0900

    made batch begin from 1 record dump sample

--- rollup/tests/integration_tool/imports.go
+++ rollup/tests/integration_tool/imports.go
@@ -165,7 +165,7 @@ func importBatch(ctx context.Context, db *gorm.DB, chks []*orm.Chunk, encChks []
 		}
 	}
 
-	var index uint64
+	index := uint64(1)
 	var parentHash common.Hash
 	if last != nil {
 		index = last.Index + 1
