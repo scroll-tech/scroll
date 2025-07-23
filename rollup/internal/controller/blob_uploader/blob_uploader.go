dd80ee53f60b1898f5765dd9ec72b6b5e8c2a55d
Morty <yiweichi1@gmail.com>
2025-07-24 01:08:16 +0800

    feat(blob-uploader): support codec version 8

--- rollup/internal/controller/blob_uploader/blob_uploader.go
+++ rollup/internal/controller/blob_uploader/blob_uploader.go
@@ -167,7 +167,7 @@ func (b *BlobUploader) constructBlobCodec(dbBatch *orm.Batch) (*kzg4844.Blob, er
 			Chunks:                     chunks,
 		}
 
-	case encoding.CodecV7:
+	case encoding.CodecV7, encoding.CodecV8:
 		encodingBatch = &encoding.Batch{
 			Index:                  dbBatch.Index,
 			ParentBatchHash:        common.HexToHash(dbBatch.ParentBatchHash),
