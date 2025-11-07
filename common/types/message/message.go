e79f0813317c6c9770232cdbdd5769c3f2ce61ac
Ho <fan@scroll.io>
2025-11-07 22:04:53 +0900

    trivial

--- common/types/message/message.go
+++ common/types/message/message.go
@@ -94,8 +94,8 @@ func (e *Byte48) UnmarshalJSON(input []byte) error {
 type BatchTaskDetail struct {
 	Version uint8 `json:"version"`
 	// use one of the string of "euclidv1" / "euclidv2"
-	ForkName      string              `json:"fork_name"`
-	ChunkInfos    []*ChunkInfo        `json:"chunk_infos"`
+	ForkName string `json:"fork_name"`
+	//	ChunkInfos    []*ChunkInfo        `json:"chunk_infos"`
 	ChunkProofs   []*OpenVMChunkProof `json:"chunk_proofs"`
 	BatchHeader   interface{}         `json:"batch_header"`
 	BlobBytes     []byte              `json:"blob_bytes"`
