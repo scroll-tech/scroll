e9470ff7a5fee847ce6b93e7970eb8fa0396319f
Ho <fan@scroll.io>
2025-11-11 15:24:16 +0900

    update config template

--- coordinator/cmd/proxy/app/app.go
+++ coordinator/cmd/proxy/app/app.go
@@ -52,6 +52,7 @@ func action(ctx *cli.Context) error {
 
 	var db *gorm.DB
 	if dbCfg := cfg.ProxyManager.DB; dbCfg != nil {
+		log.Info("Apply persistent storage", "via", cfg.ProxyManager.DB.DSN)
 		db, err = database.InitDB(cfg.ProxyManager.DB)
 		if err != nil {
 			log.Crit("failed to init db connection", "err", err)
