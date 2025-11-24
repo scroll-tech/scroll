494b836d0018291d19454a13dc9b62bdf84ec4b3
Morty <yiweichi1@gmail.com>
2025-11-25 00:24:14 +0800

    feat(blob-uploader): upload blob once it's proposed

--- rollup/internal/controller/blob_uploader/blob_uploader.go
+++ rollup/internal/controller/blob_uploader/blob_uploader.go
@@ -242,10 +242,12 @@ func (b *BlobUploader) GetFirstUnuploadedBatchByPlatform(ctx context.Context, st
 		break
 	}
 
-	if len(batch.CommitTxHash) == 0 {
-		log.Debug("got batch not committed for blob uploading", "batch_index", batchIndex, "platform", platform.String())
-		return nil, nil
-	}
+	// disable this check to upload blobs before it's committed. This is to 
+	// alleviate the case nodes try to fetch the blob from s3 before its uploaded. 
+	// if len(batch.CommitTxHash) == 0 {
+	// 	log.Debug("got batch not committed for blob uploading", "batch_index", batchIndex, "platform", platform.String())
+	// 	return nil, nil
+	// }
 
 	return batch, nil
 }
