07849aa40153077a4c9e998609be1f315ae6020f
georgehao <haohongfan@gmail.com>
2025-11-25 23:04:41 +0800

    remove the check

--- bridge-history-api/internal/logic/history_logic.go
+++ bridge-history-api/internal/logic/history_logic.go
@@ -440,15 +440,11 @@ func (h *HistoryLogic) processAndCacheTxHistoryInfo(ctx context.Context, cacheKe
 		return nil, 0, err
 	}
 
-	pagedTxs, total, isHit, err := h.getCachedTxsInfo(ctx, cacheKey, page, pageSize)
+	pagedTxs, total, _, err := h.getCachedTxsInfo(ctx, cacheKey, page, pageSize)
 	if err != nil {
 		log.Error("failed to get cached tx info", "cached key", cacheKey, "page", page, "page size", pageSize, "error", err)
 		return nil, 0, err
 	}
 
-	if !isHit {
-		log.Error("cache miss after write, expect hit", "cached key", cacheKey, "page", page, "page size", pageSize, "error", err)
-		return nil, 0, err
-	}
 	return pagedTxs, total, nil
 }
